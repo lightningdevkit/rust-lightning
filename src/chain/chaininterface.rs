fbe4f7a442f4e969bf22dfca5d41482c80206c84
Tamas Blummer <tamas.blummer@gmail.com>
2018-03-13 18:51:33 +0100

    Split out BroadcastInterface, ChainWatchInterface monitors re-enter from called listeners

--- src/chain/chaininterface.rs
+++ src/chain/chaininterface.rs
@@ -1,9 +1,9 @@
-use bitcoin::blockdata::block::BlockHeader;
+use bitcoin::blockdata::block::{Block, BlockHeader};
 use bitcoin::blockdata::transaction::Transaction;
 use bitcoin::blockdata::script::Script;
 use bitcoin::util::hash::Sha256dHash;
-
-use std::sync::{Weak,Mutex};
+use std::sync::{Mutex,Weak,MutexGuard};
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 /// An interface to request notification of certain scripts as they appear the
 /// chain.
@@ -21,11 +21,16 @@ pub trait ChainWatchInterface: Sync + Send {
 	/// Indicates that a listener needs to see all transactions.
 	fn watch_all_txn(&self);
 
-	/// Sends a transaction out to (hopefully) be mined
-	fn broadcast_transaction(&self, tx: &Transaction);
-
 	fn register_listener(&self, listener: Weak<ChainListener>);
 	//TODO: unregister
+}
+
+/// An interface to send a transaction to connected Bitcoin peers.
+/// This is for final settlement. An error might indicate that no peers can be reached or
+/// that peers rejected the transaction.
+pub trait BroadcasterInterface: Sync + Send {
+	/// Sends a transaction out to (hopefully) be mined
+	fn broadcast_transaction(&self, tx: &Transaction);
 }
 
 /// A trait indicating a desire to listen for events from the chain
@@ -54,7 +59,7 @@ pub enum ConfirmationTarget {
 /// called from inside the library in response to ChainListener events, P2P events, or timer
 /// events).
 pub trait FeeEstimator: Sync + Send {
-	fn get_est_sat_per_vbyte(&self, ConfirmationTarget) -> u64;
+	fn get_est_sat_per_vbyte(&self, confirmation_target: ConfirmationTarget) -> u64;
 }
 
 /// Utility to capture some common parts of ChainWatchInterface implementors.
@@ -62,6 +67,33 @@ pub trait FeeEstimator: Sync + Send {
 pub struct ChainWatchInterfaceUtil {
 	watched: Mutex<(Vec<Script>, Vec<(Sha256dHash, u32)>, bool)>, //TODO: Something clever to optimize this
 	listeners: Mutex<Vec<Weak<ChainListener>>>,
+	reentered: AtomicUsize
+}
+
+/// Register listener
+impl ChainWatchInterface for ChainWatchInterfaceUtil {
+	fn install_watch_script(&self, script_pub_key: Script) {
+		let mut watched = self.watched.lock().unwrap();
+		watched.0.push(Script::from(script_pub_key));
+		self.reentered.fetch_add(1, Ordering::Relaxed);
+	}
+
+	fn install_watch_outpoint(&self, outpoint: (Sha256dHash, u32)) {
+		let mut watched = self.watched.lock().unwrap();
+		watched.1.push(outpoint);
+		self.reentered.fetch_add(1, Ordering::Relaxed);
+	}
+
+	fn watch_all_txn(&self) {
+		let mut watched = self.watched.lock().unwrap();
+		watched.2 = true;
+		self.reentered.fetch_add(1, Ordering::Relaxed);
+	}
+
+	fn register_listener(&self, listener: Weak<ChainListener>) {
+		let mut vec = self.listeners.lock().unwrap();
+		vec.push(listener);
+	}
 }
 
 impl ChainWatchInterfaceUtil {
@@ -69,40 +101,32 @@ impl ChainWatchInterfaceUtil {
 		ChainWatchInterfaceUtil {
 			watched: Mutex::new((Vec::new(), Vec::new(), false)),
 			listeners: Mutex::new(Vec::new()),
+			reentered: AtomicUsize::new(1)
 		}
 	}
 
-	pub fn install_watch_script(&self, spk: Script) {
-		let mut watched = self.watched.lock().unwrap();
-		watched.0.push(Script::from(spk));
-	}
-
-	pub fn install_watch_outpoint(&self, outpoint: (Sha256dHash, u32)) {
-		let mut watched = self.watched.lock().unwrap();
-		watched.1.push(outpoint);
-	}
-
-	pub fn watch_all_txn(&self) { //TODO: refcnt this?
-		let mut watched = self.watched.lock().unwrap();
-		watched.2 = true;
-	}
-
-	pub fn register_listener(&self, listener: Weak<ChainListener>) {
-		let mut vec = self.listeners.lock().unwrap();
-		vec.push(listener);
-	}
-
-	pub fn do_call_block_connected(&self, header: &BlockHeader, height: u32, txn_matched: &[&Transaction], indexes_of_txn_matched: &[u32]) {
-		let listeners = self.listeners.lock().unwrap().clone();
-		for listener in listeners.iter() {
-			match listener.upgrade() {
-				Some(arc) => arc.block_connected(header, height, txn_matched, indexes_of_txn_matched),
-				None => ()
+	/// notify listener that a block was connected
+	/// notification will repeat if notified listener register new listeners
+	pub fn block_connected_with_filtering(&self, block: &Block, height: u32) {
+		let mut reentered = true;
+		while reentered {
+			let mut matched = Vec::new();
+			let mut matched_index = Vec::new();
+			{
+				let watched = self.watched.lock().unwrap();
+				for (index, transaction) in block.txdata.iter().enumerate() {
+					if self.does_match_tx_unguarded(transaction, &watched) {
+						matched.push(transaction);
+						matched_index.push(index as u32);
+					}
+				}
 			}
+			reentered = self.block_connected_checked(&block.header, height, matched.as_slice(), matched_index.as_slice());
 		}
 	}
 
-	pub fn do_call_block_disconnected(&self, header: &BlockHeader) {
+	/// notify listener that a block was disconnected
+	pub fn block_disconnected(&self, header: &BlockHeader) {
 		let listeners = self.listeners.lock().unwrap().clone();
 		for listener in listeners.iter() {
 			match listener.upgrade() {
@@ -112,9 +136,28 @@ impl ChainWatchInterfaceUtil {
 		}
 	}
 
+	/// call listeners for connected blocks if they are still around.
+	/// returns true if notified listeners registered additional listener
+	pub fn block_connected_checked(&self, header: &BlockHeader, height: u32, txn_matched: &[&Transaction], indexes_of_txn_matched: &[u32]) -> bool {
+		let last_seen = self.reentered.load(Ordering::Relaxed);
+
+		let listeners = self.listeners.lock().unwrap().clone();
+		for listener in listeners.iter() {
+			match listener.upgrade() {
+				Some(arc) => arc.block_connected(header, height, txn_matched, indexes_of_txn_matched),
+				None => ()
+			}
+		}
+		return last_seen != self.reentered.load(Ordering::Relaxed);
+	}
+
 	/// Checks if a given transaction matches the current filter
 	pub fn does_match_tx(&self, tx: &Transaction) -> bool {
 		let watched = self.watched.lock().unwrap();
+		self.does_match_tx_unguarded (tx, &watched)
+	}
+
+	fn does_match_tx_unguarded (&self, tx: &Transaction, watched: &MutexGuard<(Vec<Script>, Vec<(Sha256dHash, u32)>, bool)>) -> bool {
 		if watched.2 {
 			return true;
 		}
