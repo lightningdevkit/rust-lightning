1ec9c3aa4bf99022df969de20acc84ff49314910
Matt Corallo <git@bluematt.me>
2018-04-16 16:55:03 -0400

    Provide fallback for crypto's fixed_time_eq on non-x86/arm targets

---
+++ build.rs
@@ -0,0 +1,10 @@
+extern crate gcc;
+
+fn main() {
+	#[cfg(not(any(target_arch = "x86", target_arch = "x86_64", target_arch = "arm")))]
+	{
+		let mut cfg = gcc::Build::new();
+		cfg.file("src/util/rust_crypto_nonstd_arch.c");
+		cfg.compile("lib_rust_crypto_nonstd_arch.a");
+	}
+}
