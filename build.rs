84cb40f859fba8d8f2c8e6562f310f4b832a05b0
Matt Corallo <git@bluematt.me>
2018-07-19 22:45:34 -0400

    Bump version for API overhauls, update deps and docs

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
-extern crate gcc;
+extern crate cc;
 
 fn main() {
 	#[cfg(not(any(target_arch = "x86", target_arch = "x86_64", target_arch = "arm")))]
 	{
-		let mut cfg = gcc::Build::new();
+		let mut cfg = cc::Build::new();
 		cfg.file("src/util/rust_crypto_nonstd_arch.c");
 		cfg.compile("lib_rust_crypto_nonstd_arch.a");
 	}
