87aabff8abb5928373e42c966326b6b4d0993372
Matt Corallo <git@bluematt.me>
2018-08-17 17:36:22 -0400

    Disable push_msat in full_stack_target temporarily

--- fuzz/fuzz_targets/full_stack_target.rs
+++ fuzz/fuzz_targets/full_stack_target.rs
@@ -315,7 +315,9 @@ pub fn do_test(data: &[u8]) {
 				if !peers.borrow()[peer_id as usize] { return; }
 				let their_key = get_pubkey!();
 				let chan_value = slice_to_be24(get_slice!(3)) as u64;
-				let push_msat_value = slice_to_be24(get_slice!(3)) as u64;
+				//let push_msat_value = slice_to_be24(get_slice!(3)) as u64;
+				// Temporarily don't read extra bytes to avoid breaking existing test-cases
+				let push_msat_value = 0;
 				if channelmanager.create_channel(their_key, chan_value, push_msat_value, 0).is_err() { return; }
 			},
 			6 => {
