7743cbdf14412bfbe37fbce787e7b1f9a9a44d53
Matt Corallo <git@bluematt.me>
2018-08-17 14:29:16 -0400

    Add APIError docs

--- src/util/errors.rs
+++ src/util/errors.rs
@@ -1,15 +1,22 @@
 use std::fmt;
 
+/// Indicates an error on the client's part (usually some variant of attempting to use too-low or
+/// too-high values)
 pub enum APIError {
+	/// Indicates the API was wholly misused (see err for more). Cases where these can be returned
+	/// are documented, but generally indicates some precondition of a function was violated.
 	APIMisuseError {err: &'static str},
+	/// Due to a high feerate, we were unable to complete the request.
+	/// For example, this may be returned if the feerate implies we cannot open a channel at the
+	/// requested value, but opening a larger channel would succeed.
 	FeeRateTooHigh {err: String, feerate: u64},
 }
 
 impl fmt::Debug for APIError {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
 		match *self {
 			APIError::APIMisuseError {ref err} => f.write_str(err),
 			APIError::FeeRateTooHigh {ref err, ref feerate} => write!(f, "{} feerate: {}", err, feerate)
 		}
-  }
+	}
 }
