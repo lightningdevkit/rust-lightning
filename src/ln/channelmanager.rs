3fbbd9e8d21da9e3d469a5c17cdf81e9a1ff0720
Matt Corallo <git@bluematt.me>
2018-08-30 15:10:23 -0400

    Remove unused feeest field in ChannelManager tests

--- src/ln/channelmanager.rs
+++ src/ln/channelmanager.rs
@@ -2274,7 +2274,6 @@ mod tests {
 	}
 
 	struct Node {
-		feeest: Arc<test_utils::TestFeeEstimator>,
 		chain_monitor: Arc<chaininterface::ChainWatchInterfaceUtil>,
 		tx_broadcaster: Arc<test_utils::TestBroadcaster>,
 		chan_monitor: Arc<test_utils::TestChannelMonitor>,
@@ -2827,7 +2826,7 @@ mod tests {
 			};
 			let node = ChannelManager::new(node_id.clone(), 0, true, Network::Testnet, feeest.clone(), chan_monitor.clone(), chain_monitor.clone(), tx_broadcaster.clone(), Arc::clone(&logger)).unwrap();
 			let router = Router::new(PublicKey::from_secret_key(&secp_ctx, &node_id), Arc::clone(&logger));
-			nodes.push(Node { feeest, chain_monitor, tx_broadcaster, chan_monitor, node, router });
+			nodes.push(Node { chain_monitor, tx_broadcaster, chan_monitor, node, router });
 		}
 
 		nodes
