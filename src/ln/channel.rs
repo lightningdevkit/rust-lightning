29d3533696818985389ce9bd82de453ad8ba857c
Yuntai Kyong <yuntai.kyong@gmail.com>
2018-09-21 00:42:03 +0900

    Update comments

--- src/ln/channel.rs
+++ src/ln/channel.rs
@@ -286,15 +286,22 @@ pub(super) struct Channel {
 	pending_outbound_htlcs: Vec<OutboundHTLCOutput>,
 	holding_cell_htlc_updates: Vec<HTLCUpdateAwaitingACK>,
 
-	// pending_update_fee is filled when sending and receiving update_fee 
-	// For outbound channel, feerate_per_kw is upadated with the value from
+	// pending_update_fee is filled when sending and receiving update_fee
+	// For outbound channel, feerate_per_kw is updated with the value from
 	// pending_update_fee when revoke_and_ack is received
+	//
 	// For inbound channel, feerate_per_kw is updated when it receives
-	// commitment_signed when pending_update_fee.is_some().
+	// commitment_signed and revoke_and_ack is generated
+	// The pending value is kept when another pair of update_fee and commitment_signed
+	// is received during AwaitingRemoteRevoke and relieved when the expected
+	// revoke_and_ack is received and new commitment_signed is generated to be
+	// sent to the funder. Otherwise, the pending value is removed when receiving
+	// commitment_signed.
 	pending_update_fee: Option<u64>,
 	// update_fee() during ChannelState::AwaitingRemoteRevoke is hold in
 	// holdina_cell_update_fee then moved to pending_udpate_fee when revoke_and_ack
-	// is received.
+	// is received. holding_cell_update_fee is updated when there are additional
+	// update_fee() during ChannelState::AwaitingRemoteRevoke.
 	holding_cell_update_fee: Option<u64>,
 	next_local_htlc_id: u64,
 	next_remote_htlc_id: u64,
