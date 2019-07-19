084ef28708765c0b1ecc041e64a125a7090eeed1
Matt Corallo <git@bluematt.me>
2019-07-18 22:17:36 -0400

    Stop using rng in peer_channel_encryptor to generate ephemeral keys
    This removes the bulk of our reliance on the rand crate in non-test
    envs, paving a way towards a syscall-less rust-lightning and WASM.
    Since this is a breaking change for full_stack_target (and several
    fuzz targets), go ahead and make other changes to make things more
    distinct.

--- fuzz/fuzz_targets/peer_crypt_target.rs
+++ fuzz/fuzz_targets/peer_crypt_target.rs
@@ -34,13 +34,17 @@ pub fn do_test(data: &[u8]) {
 		Ok(key) => key,
 		Err(_) => return,
 	};
+	let ephemeral_key = match SecretKey::from_slice(get_slice!(32)) {
+		Ok(key) => key,
+		Err(_) => return,
+	};
 
 	let mut crypter = if get_slice!(1)[0] != 0 {
 		let their_pubkey = match PublicKey::from_slice(get_slice!(33)) {
 			Ok(key) => key,
 			Err(_) => return,
 		};
-		let mut crypter = PeerChannelEncryptor::new_outbound(their_pubkey);
+		let mut crypter = PeerChannelEncryptor::new_outbound(their_pubkey, ephemeral_key);
 		crypter.get_act_one();
 		match crypter.process_act_two(get_slice!(50), &our_network_key) {
 			Ok(_) => {},
@@ -50,7 +54,7 @@ pub fn do_test(data: &[u8]) {
 		crypter
 	} else {
 		let mut crypter = PeerChannelEncryptor::new_inbound(&our_network_key);
-		match crypter.process_act_one_with_key(get_slice!(50), &our_network_key) {
+		match crypter.process_act_one_with_keys(get_slice!(50), &our_network_key, ephemeral_key) {
 			Ok(_) => {},
 			Err(_) => return,
 		}
