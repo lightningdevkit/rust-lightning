cd31cdb6b174b1e8d6abcf46b33b4f39c96a7254
Steven Roose <steven@stevenroose.org>
2019-12-05 20:29:16 +0000

    Document the default values of configs

--- lightning/src/util/config.rs
+++ lightning/src/util/config.rs
@@ -4,6 +4,9 @@
 use ln::channelmanager::{BREAKDOWN_TIMEOUT, MAX_LOCAL_BREAKDOWN_TIMEOUT};
 
 /// Top-level config which holds ChannelHandshakeLimits and ChannelConfig.
+///
+/// Default::default() provides sane defaults for most configurations
+/// (but currently with 0 relay fees!)
 #[derive(Clone, Debug)]
 pub struct UserConfig {
 	/// Channel config that we propose to our counterparty.
@@ -15,7 +18,6 @@ pub struct UserConfig {
 }
 
 impl Default for UserConfig {
-	/// Provides sane defaults for most configurations (but with 0 relay fees!)
 	fn default() -> Self {
 		UserConfig {
 			own_channel_config: ChannelHandshakeConfig::default(),
@@ -26,28 +28,32 @@ impl Default for UserConfig {
 }
 
 /// Configuration we set when applicable.
+///
+/// Default::default() provides sane defaults.
 #[derive(Clone, Debug)]
 pub struct ChannelHandshakeConfig {
 	/// Confirmations we will wait for before considering the channel locked in.
 	/// Applied only for inbound channels (see ChannelHandshakeLimits::max_minimum_depth for the
 	/// equivalent limit applied to outbound channels).
+	///
+	/// Default value: 6.
 	pub minimum_depth: u32,
 	/// Set to the amount of time we require our counterparty to wait to claim their money.
 	///
 	/// It's one of the main parameter of our security model. We (or one of our watchtowers) MUST
 	/// be online to check for peer having broadcast a revoked transaction to steal our funds
 	/// at least once every our_to_self_delay blocks.
-	/// Default is BREAKDOWN_TIMEOUT, we enforce it as a minimum at channel opening so you can
-	/// tweak config to ask for more security, not less.
 	///
 	/// Meanwhile, asking for a too high delay, we bother peer to freeze funds for nothing in
 	/// case of an honest unilateral channel close, which implicitly decrease the economic value of
 	/// our channel.
+	///
+	/// Default value: BREAKDOWN_TIMEOUT (currently 144), we enforce it as a minimum at channel
+	/// opening so you can tweak config to ask for more security, not less.
 	pub our_to_self_delay: u16,
 }
 
 impl Default for ChannelHandshakeConfig {
-	/// Provides sane defaults for `ChannelHandshakeConfig`
 	fn default() -> ChannelHandshakeConfig {
 		ChannelHandshakeConfig {
 			minimum_depth: 6,
@@ -61,23 +67,39 @@ impl Default for ChannelHandshakeConfig {
 /// These limits are only applied to our counterparty's limits, not our own.
 ///
 /// Use 0/<type>::max_value() as appropriate to skip checking.
+///
+/// Provides sane defaults for most configurations.
+///
+/// Most additional limits are disabled except those with which specify a default in individual
+/// field documentation. Note that this may result in barely-usable channels, but since they
+/// are applied mostly only to incoming channels that's not much of a problem.
 #[derive(Copy, Clone, Debug)]
 pub struct ChannelHandshakeLimits {
 	/// Minimum allowed satoshis when a channel is funded, this is supplied by the sender and so
 	/// only applies to inbound channels.
+	///
+	/// Default value: 0.
 	pub min_funding_satoshis: u64,
 	/// The remote node sets a limit on the minimum size of HTLCs we can send to them. This allows
 	/// you to limit the maximum minimum-size they can require.
+	///
+	/// Default value: u64::max_value.
 	pub max_htlc_minimum_msat: u64,
 	/// The remote node sets a limit on the maximum value of pending HTLCs to them at any given
 	/// time to limit their funds exposure to HTLCs. This allows you to set a minimum such value.
+	///
+	/// Default value: 0.
 	pub min_max_htlc_value_in_flight_msat: u64,
 	/// The remote node will require we keep a certain amount in direct payment to ourselves at all
 	/// time, ensuring that we are able to be punished if we broadcast an old state. This allows to
 	/// you limit the amount which we will have to keep to ourselves (and cannot use for HTLCs).
+	///
+	/// Default value: u64::max_value.
 	pub max_channel_reserve_satoshis: u64,
 	/// The remote node sets a limit on the maximum number of pending HTLCs to them at any given
 	/// time. This allows you to set a minimum such value.
+	///
+	/// Default value: 0.
 	pub min_max_accepted_htlcs: u16,
 	/// Outputs below a certain value will not be added to on-chain transactions. The dust value is
 	/// required to always be higher than this value so this only applies to HTLC outputs (and
@@ -86,38 +108,39 @@ pub struct ChannelHandshakeLimits {
 	/// This setting allows you to set a minimum dust limit for their commitment transactions,
 	/// reflecting the reality that tiny outputs are not considered standard transactions and will
 	/// not propagate through the Bitcoin network.
-	/// Defaults to 546, or the current dust limit on the Bitcoin network.
+	///
+	/// Default value: 546, the current dust limit on the Bitcoin network.
 	pub min_dust_limit_satoshis: u64,
 	/// Maximum allowed threshold above which outputs will not be generated in their commitment
 	/// transactions.
 	/// HTLCs below this amount plus HTLC transaction fees are not enforceable on-chain.
+	///
+	/// Default value: u64::max_value.
 	pub max_dust_limit_satoshis: u64,
 	/// Before a channel is usable the funding transaction will need to be confirmed by at least a
 	/// certain number of blocks, specified by the node which is not the funder (as the funder can
 	/// assume they aren't going to double-spend themselves).
-	/// This config allows you to set a limit on the maximum amount of time to wait. Defaults to
-	/// 144 blocks or roughly one day and only applies to outbound channels.
+	/// This config allows you to set a limit on the maximum amount of time to wait.
+	///
+	/// Default value: 144, or roughly one day and only applies to outbound channels.
 	pub max_minimum_depth: u32,
 	/// Set to force the incoming channel to match our announced channel preference in
 	/// ChannelConfig.
-	/// Defaults to true to make the default that no announced channels are possible (which is
+	///
+	/// Default value: true, to make the default that no announced channels are possible (which is
 	/// appropriate for any nodes which are not online very reliably).
 	pub force_announced_channel_preference: bool,
 	/// Set to the amount of time we're willing to wait to claim money back to us.
 	///
 	/// Not checking this value would be a security issue, as our peer would be able to set it to
 	/// max relative lock-time (a year) and we would "lose" money as it would be locked for a long time.
-	/// Default is MAX_LOCAL_BREAKDOWN_TIMEOUT, which we also enforce as a maximum value
+	///
+	/// Default value: MAX_LOCAL_BREAKDOWN_TIMEOUT (1008), which we also enforce as a maximum value
 	/// so you can tweak config to reduce the loss of having useless locked funds (if your peer accepts)
 	pub their_to_self_delay: u16
 }
 
 impl Default for ChannelHandshakeLimits {
-	/// Provides sane defaults for most configurations.
-	///
-	/// Most additional limits are disabled except those with which specify a default in individual
-	/// field documentation. Note that this may result in barely-usable channels, but since they
-	/// are applied mostly only to incoming channels that's not much of a problem.
 	fn default() -> Self {
 		ChannelHandshakeLimits {
 			min_funding_satoshis: 0,
@@ -141,6 +164,8 @@ pub struct ChannelConfig {
 	/// Amount (in millionths of a satoshi) the channel will charge per transferred satoshi.
 	/// This may be allowed to change at runtime in a later update, however doing so must result in
 	/// update messages sent to notify all nodes of our updated relay fee.
+	///
+	/// Default value: 0.
 	pub fee_proportional_millionths: u32,
 	/// Set to announce the channel publicly and notify all nodes that they can route via this
 	/// channel.
@@ -151,6 +176,8 @@ pub struct ChannelConfig {
 	/// channels unless ChannelHandshakeLimits::force_announced_channel_preferences is set.
 	///
 	/// This cannot be changed after the initial channel handshake.
+	///
+	/// Default value: false.
 	pub announced_channel: bool,
 	/// When set, we commit to an upfront shutdown_pubkey at channel open. If our counterparty
 	/// supports it, they will then enforce the mutual-close output to us matches what we provided
@@ -161,6 +188,8 @@ pub struct ChannelConfig {
 	/// lightning payments, so we never require that our counterparties support this option.
 	///
 	/// This cannot be changed after a channel has been initialized.
+	///
+	/// Default value: true.
 	pub commit_upfront_shutdown_pubkey: bool
 }
 
