ccd4f12078655e5df14ad6c6ae2f36a0fffcd152
Matt Corallo <git@bluematt.me>
2019-12-11 18:17:03 -0500

    Pre-build fuzz targets at once in travis instead of as we need them

--- fuzz/travis-fuzz.sh
+++ fuzz/travis-fuzz.sh
@@ -13,6 +13,7 @@ rm *_target.rs
 popd
 
 cargo install --force honggfuzz
+HFUZZ_BUILD_ARGS="--features honggfuzz_fuzz" cargo hfuzz build
 for TARGET in src/bin/*.rs; do
 	FILENAME=$(basename $TARGET)
 	FILE="${FILENAME%.*}"
