lightningdevkit/rust-lightning

commit 912f8774822892d6e371f9a4b1ba7a2553dbb5e7

Pass node features through to RouteHops
This exposes the latest Init-context features in the ChannelDetails
passed to the Router during route calculation, which combines those
with the Node-context features tracked from node_announcements to
provide the latest Node-context features in RouteHop structs.

Fields are also added for Channel-context features, though those are
only partially used since no such features are defined today anyway.

These will be useful when determining whether to use new
TLV-formatted onion hop datas when generating onions for peers.

lightning/src/ln/features.rs
@@ -118,6 +118,13 @@ impl ChannelFeatures {
 			mark: PhantomData,
 		}
 	}
+
+	/// Takes the flags that we know how to interpret in an init-context features that are also
+	/// relevant in a channel-context features and creates a channel-context features from them.
+	pub(crate) fn with_known_relevant_init_flags(_init_ctx: &InitFeatures) -> Self {
+		// There are currently no channel flags defined that we understand.
+		Self { flags: Vec::new(), mark: PhantomData, }
+	}
 }
 
 impl NodeFeatures {
@@ -135,6 +142,17 @@ impl NodeFeatures {
 			flags: vec![2 | 1 << 5],
 			mark: PhantomData,
 		}
+	}
+
+	/// Takes the flags that we know how to interpret in an init-context features that are also
+	/// relevant in a node-context features and creates a node-context features from them.
+	pub(crate) fn with_known_relevant_init_flags(init_ctx: &InitFeatures) -> Self {
+		let mut flags = Vec::new();
+		if init_ctx.flags.len() > 0 {
+			// Pull out data_loss_protect and upfront_shutdown_script (bits 0, 1, 4, and 5)
+			flags.push(init_ctx.flags.last().unwrap() & 0b00110011);
+		}
+		Self { flags, mark: PhantomData, }
 	}
 }
 

fuzz/src/chanmon_consistency.rs
@@ -29,7 +29,7 @@ use lightning::ln::channelmonitor;
 use lightning::ln::channelmonitor::{ChannelMonitor, ChannelMonitorUpdateErr, HTLCUpdate};
 use lightning::ln::channelmanager::{ChannelManager, PaymentHash, PaymentPreimage, ChannelManagerReadArgs};
 use lightning::ln::router::{Route, RouteHop};
-use lightning::ln::features::InitFeatures;
+use lightning::ln::features::{ChannelFeatures, InitFeatures, NodeFeatures};
 use lightning::ln::msgs::{CommitmentUpdate, ChannelMessageHandler, ErrorAction, UpdateAddHTLC, Init};
 use lightning::util::enforcing_trait_impls::EnforcingChannelKeys;
 use lightning::util::events;
@@ -414,7 +414,9 @@ pub fn do_test(data: &[u8]) {
 				if let Err(_) = $source.send_payment(Route {
 					hops: vec![RouteHop {
 						pubkey: $dest.0.get_our_node_id(),
+						node_features: NodeFeatures::empty(),
 						short_channel_id: $dest.1,
+						channel_features: ChannelFeatures::empty(),
 						fee_msat: 5000000,
 						cltv_expiry_delta: 200,
 					}],
@@ -429,12 +431,16 @@ pub fn do_test(data: &[u8]) {
 				if let Err(_) = $source.send_payment(Route {
 					hops: vec![RouteHop {
 						pubkey: $middle.0.get_our_node_id(),
+						node_features: NodeFeatures::empty(),
 						short_channel_id: $middle.1,
+						channel_features: ChannelFeatures::empty(),
 						fee_msat: 50000,
 						cltv_expiry_delta: 100,
 					},RouteHop {
 						pubkey: $dest.0.get_our_node_id(),
+						node_features: NodeFeatures::empty(),
 						short_channel_id: $dest.1,
+						channel_features: ChannelFeatures::empty(),
 						fee_msat: 5000000,
 						cltv_expiry_delta: 200,
 					}],

fuzz/src/router.rs
@@ -5,8 +5,9 @@ use bitcoin::blockdata::transaction::Transaction;
 
 use lightning::chain::chaininterface::{ChainError,ChainWatchInterface};
 use lightning::ln::channelmanager::ChannelDetails;
+use lightning::ln::features::InitFeatures;
 use lightning::ln::msgs;
-use lightning::ln::msgs::{RoutingMessageHandler};
+use lightning::ln::msgs::RoutingMessageHandler;
 use lightning::ln::router::{Router, RouteHint};
 use lightning::util::logger::Logger;
 use lightning::util::ser::Readable;
@@ -198,6 +199,7 @@ pub fn do_test(data: &[u8]) {
 								channel_id: [0; 32],
 								short_channel_id: Some(slice_to_be64(get_slice!(8))),
 								remote_network_id: get_pubkey!(),
+								counterparty_features: InitFeatures::empty(),
 								channel_value_satoshis: slice_to_be64(get_slice!(8)),
 								user_id: 0,
 								inbound_capacity_msat: 0,

lightning/src/ln/channelmanager.rs
@@ -404,6 +404,10 @@ pub struct ChannelDetails {
 	pub short_channel_id: Option<u64>,
 	/// The node_id of our counterparty
 	pub remote_network_id: PublicKey,
+	/// The Features the channel counterparty provided upon last connection.
+	/// Useful for routing as it is the most up-to-date copy of the counterparty's features and
+	/// many routing-relevant features are present in the init context.
+	pub counterparty_features: InitFeatures,
 	/// The value, in satoshis, of this channel as appears in the funding output
 	pub channel_value_satoshis: u64,
 	/// The user_id passed in to create_channel, or 0 if the channel was inbound.
@@ -679,20 +683,30 @@ impl<ChanSigner: ChannelKeys> ChannelManager<ChanSigner> {
 	/// Gets the list of open channels, in random order. See ChannelDetail field documentation for
 	/// more information.
 	pub fn list_channels(&self) -> Vec<ChannelDetails> {
-		let channel_state = self.channel_state.lock().unwrap();
-		let mut res = Vec::with_capacity(channel_state.by_id.len());
-		for (channel_id, channel) in channel_state.by_id.iter() {
-			let (inbound_capacity_msat, outbound_capacity_msat) = channel.get_inbound_outbound_available_balance_msat();
-			res.push(ChannelDetails {
-				channel_id: (*channel_id).clone(),
-				short_channel_id: channel.get_short_channel_id(),
-				remote_network_id: channel.get_their_node_id(),
-				channel_value_satoshis: channel.get_value_satoshis(),
-				inbound_capacity_msat,
-				outbound_capacity_msat,
-				user_id: channel.get_user_id(),
-				is_live: channel.is_live(),
-			});
+		let mut res = Vec::new();
+		{
+			let channel_state = self.channel_state.lock().unwrap();
+			res.reserve(channel_state.by_id.len());
+			for (channel_id, channel) in channel_state.by_id.iter() {
+				let (inbound_capacity_msat, outbound_capacity_msat) = channel.get_inbound_outbound_available_balance_msat();
+				res.push(ChannelDetails {
+					channel_id: (*channel_id).clone(),
+					short_channel_id: channel.get_short_channel_id(),
+					remote_network_id: channel.get_their_node_id(),
+					counterparty_features: InitFeatures::empty(),
+					channel_value_satoshis: channel.get_value_satoshis(),
+					inbound_capacity_msat,
+					outbound_capacity_msat,
+					user_id: channel.get_user_id(),
+					is_live: channel.is_live(),
+				});
+			}
+		}
+		let per_peer_state = self.per_peer_state.read().unwrap();
+		for chan in res.iter_mut() {
+			if let Some(peer_state) = per_peer_state.get(&chan.remote_network_id) {
+				chan.counterparty_features = peer_state.lock().unwrap().latest_features.clone();
+			}
 		}
 		res
 	}
@@ -703,24 +717,34 @@ impl<ChanSigner: ChannelKeys> ChannelManager<ChanSigner> {
 	/// These are guaranteed to have their is_live value set to true, see the documentation for
 	/// ChannelDetails::is_live for more info on exactly what the criteria are.
 	pub fn list_usable_channels(&self) -> Vec<ChannelDetails> {
-		let channel_state = self.channel_state.lock().unwrap();
-		let mut res = Vec::with_capacity(channel_state.by_id.len());
-		for (channel_id, channel) in channel_state.by_id.iter() {
-			// Note we use is_live here instead of usable which leads to somewhat confused
-			// internal/external nomenclature, but that's ok cause that's probably what the user
-			// really wanted anyway.
-			if channel.is_live() {
-				let (inbound_capacity_msat, outbound_capacity_msat) = channel.get_inbound_outbound_available_balance_msat();
-				res.push(ChannelDetails {
-					channel_id: (*channel_id).clone(),
-					short_channel_id: channel.get_short_channel_id(),
-					remote_network_id: channel.get_their_node_id(),
-					channel_value_satoshis: channel.get_value_satoshis(),
-					inbound_capacity_msat,
-					outbound_capacity_msat,
-					user_id: channel.get_user_id(),
-					is_live: true,
-				});
+		let mut res = Vec::new();
+		{
+			let channel_state = self.channel_state.lock().unwrap();
+			res.reserve(channel_state.by_id.len());
+			for (channel_id, channel) in channel_state.by_id.iter() {
+				// Note we use is_live here instead of usable which leads to somewhat confused
+				// internal/external nomenclature, but that's ok cause that's probably what the user
+				// really wanted anyway.
+				if channel.is_live() {
+					let (inbound_capacity_msat, outbound_capacity_msat) = channel.get_inbound_outbound_available_balance_msat();
+					res.push(ChannelDetails {
+						channel_id: (*channel_id).clone(),
+						short_channel_id: channel.get_short_channel_id(),
+						remote_network_id: channel.get_their_node_id(),
+						counterparty_features: InitFeatures::empty(),
+						channel_value_satoshis: channel.get_value_satoshis(),
+						inbound_capacity_msat,
+						outbound_capacity_msat,
+						user_id: channel.get_user_id(),
+						is_live: true,
+					});
+				}
+			}
+		}
+		let per_peer_state = self.per_peer_state.read().unwrap();
+		for chan in res.iter_mut() {
+			if let Some(peer_state) = per_peer_state.get(&chan.remote_network_id) {
+				chan.counterparty_features = peer_state.lock().unwrap().latest_features.clone();
 			}
 		}
 		res

lightning/src/ln/functional_tests.rs
@@ -11,7 +11,7 @@ use ln::channelmonitor::{ChannelMonitor, CLTV_CLAIM_BUFFER, LATENCY_GRACE_PERIOD
 use ln::channel::{Channel, ChannelError};
 use ln::onion_utils;
 use ln::router::{Route, RouteHop};
-use ln::features::{ChannelFeatures, InitFeatures};
+use ln::features::{ChannelFeatures, InitFeatures, NodeFeatures};
 use ln::msgs;
 use ln::msgs::{ChannelMessageHandler,RoutingMessageHandler,HTLCFailChannelUpdate, ErrorAction};
 use util::enforcing_trait_impls::EnforcingChannelKeys;
@@ -1029,19 +1029,25 @@ fn fake_network_test() {
 	let mut hops = Vec::with_capacity(3);
 	hops.push(RouteHop {
 		pubkey: nodes[2].node.get_our_node_id(),
+		node_features: NodeFeatures::empty(),
 		short_channel_id: chan_2.0.contents.short_channel_id,
+		channel_features: ChannelFeatures::empty(),
 		fee_msat: 0,
 		cltv_expiry_delta: chan_3.0.contents.cltv_expiry_delta as u32
 	});
 	hops.push(RouteHop {
 		pubkey: nodes[3].node.get_our_node_id(),
+		node_features: NodeFeatures::empty(),
 		short_channel_id: chan_3.0.contents.short_channel_id,
+		channel_features: ChannelFeatures::empty(),
 		fee_msat: 0,
 		cltv_expiry_delta: chan_4.1.contents.cltv_expiry_delta as u32
 	});
 	hops.push(RouteHop {
 		pubkey: nodes[1].node.get_our_node_id(),
+		node_features: NodeFeatures::empty(),
 		short_channel_id: chan_4.0.contents.short_channel_id,
+		channel_features: ChannelFeatures::empty(),
 		fee_msat: 1000000,
 		cltv_expiry_delta: TEST_FINAL_CLTV,
 	});
@@ -1052,19 +1058,25 @@ fn fake_network_test() {
 	let mut hops = Vec::with_capacity(3);
 	hops.push(RouteHop {
 		pubkey: nodes[3].node.get_our_node_id(),
+		node_features: NodeFeatures::empty(),
 		short_channel_id: chan_4.0.contents.short_channel_id,
+		channel_features: ChannelFeatures::empty(),
 		fee_msat: 0,
 		cltv_expiry_delta: chan_3.1.contents.cltv_expiry_delta as u32
 	});
 	hops.push(RouteHop {
 		pubkey: nodes[2].node.get_our_node_id(),
+		node_features: NodeFeatures::empty(),
 		short_channel_id: chan_3.0.contents.short_channel_id,
+		channel_features: ChannelFeatures::empty(),
 		fee_msat: 0,
 		cltv_expiry_delta: chan_2.1.contents.cltv_expiry_delta as u32
 	});
 	hops.push(RouteHop {
 		pubkey: nodes[1].node.get_our_node_id(),
+		node_features: NodeFeatures::empty(),
 		short_channel_id: chan_2.0.contents.short_channel_id,
+		channel_features: ChannelFeatures::empty(),
 		fee_msat: 1000000,
 		cltv_expiry_delta: TEST_FINAL_CLTV,
 	});

lightning/src/ln/onion_utils.rs
@@ -425,6 +425,7 @@ pub(super) fn process_onion_failure<T: secp256k1::Signing>(secp_ctx: &Secp256k1<
 #[cfg(test)]
 mod tests {
 	use ln::channelmanager::PaymentHash;
+	use ln::features::{ChannelFeatures, NodeFeatures};
 	use ln::router::{Route, RouteHop};
 	use ln::msgs;
 	use util::ser::Writeable;
@@ -444,22 +445,27 @@ mod tests {
 			hops: vec!(
 					RouteHop {
 						pubkey: PublicKey::from_slice(&hex::decode("02eec7245d6b7d2ccb30380bfbe2a3648cd7a942653f5aa340edcea1f283686619").unwrap()[..]).unwrap(),
+						channel_features: ChannelFeatures::empty(), node_features: NodeFeatures::empty(),
 						short_channel_id: 0, fee_msat: 0, cltv_expiry_delta: 0 // Test vectors are garbage and not generateble from a RouteHop, we fill in payloads manually
 					},
 					RouteHop {
 						pubkey: PublicKey::from_slice(&hex::decode("0324653eac434488002cc06bbfb7f10fe18991e35f9fe4302dbea6d2353dc0ab1c").unwrap()[..]).unwrap(),
+						channel_features: ChannelFeatures::empty(), node_features: NodeFeatures::empty(),
 						short_channel_id: 0, fee_msat: 0, cltv_expiry_delta: 0 // Test vectors are garbage and not generateble from a RouteHop, we fill in payloads manually
 					},
 					RouteHop {
 						pubkey: PublicKey::from_slice(&hex::decode("027f31ebc5462c1fdce1b737ecff52d37d75dea43ce11c74d25aa297165faa2007").unwrap()[..]).unwrap(),
+						channel_features: ChannelFeatures::empty(), node_features: NodeFeatures::empty(),
 						short_channel_id: 0, fee_msat: 0, cltv_expiry_delta: 0 // Test vectors are garbage and not generateble from a RouteHop, we fill in payloads manually
 					},
 					RouteHop {
 						pubkey: PublicKey::from_slice(&hex::decode("032c0b7cf95324a07d05398b240174dc0c2be444d96b159aa6c7f7b1e668680991").unwrap()[..]).unwrap(),
+						channel_features: ChannelFeatures::empty(), node_features: NodeFeatures::empty(),
 						short_channel_id: 0, fee_msat: 0, cltv_expiry_delta: 0 // Test vectors are garbage and not generateble from a RouteHop, we fill in payloads manually
 					},
 					RouteHop {
 						pubkey: PublicKey::from_slice(&hex::decode("02edabbd16b41c8371b92ef2f04c1185b4f03b6dcd52ba9b78d9d7c89c8f221145").unwrap()[..]).unwrap(),
+						channel_features: ChannelFeatures::empty(), node_features: NodeFeatures::empty(),
 						short_channel_id: 0, fee_msat: 0, cltv_expiry_delta: 0 // Test vectors are garbage and not generateble from a RouteHop, we fill in payloads manually
 					},
 			),

commit 658b681772cbffdfbf8331caa1517ed20d1f5343

Fix blanking out non-node-context feature flags when pulling features from init context.
Fixes bug introduced in 912f877

lightning/src/ln/features.rs
@@ -152,9 +152,15 @@ impl NodeFeatures {
 	/// relevant in a node-context features and creates a node-context features from them.
 	pub(crate) fn with_known_relevant_init_flags(init_ctx: &InitFeatures) -> Self {
 		let mut flags = Vec::new();
-		if init_ctx.flags.len() > 0 {
-			// Pull out data_loss_protect and upfront_shutdown_script (bits 0, 1, 4, and 5)
-			flags.push(init_ctx.flags.last().unwrap() & 0b00110011);
+		for (i, feature_byte)in init_ctx.flags.iter().enumerate() {
+			match i {
+				// Blank out initial_routing_sync (feature bits 2/3), gossip_queries (6/7),
+				// gossip_queries_ex (10/11), option_static_remotekey (12/13), and
+				// payment_secret (14/15)
+				0 => flags.push(feature_byte & 0b00110011),
+				1 => flags.push(feature_byte & 0b00000011),
+				_ => (),
+			}
 		}
 		Self { flags, mark: PhantomData, }
 	}
@@ -296,7 +302,7 @@ impl<T: sealed::Context> Readable for Features<T> {
 
 #[cfg(test)]
 mod tests {
-	use super::{ChannelFeatures, InitFeatures, NodeFeatures};
+	use super::{ChannelFeatures, InitFeatures, NodeFeatures, Features};
 
 	#[test]
 	fn sanity_test_our_features() {
@@ -329,5 +335,26 @@ mod tests {
 		assert!(features.requires_unknown_bits());
 		features.clear_require_unknown_bits();
 		assert!(!features.requires_unknown_bits());
+	}
+
+	#[test]
+	fn test_node_with_known_relevant_init_flags() {
+		// Create an InitFeatures with initial_routing_sync supported.
+		let mut init_features = InitFeatures::supported();
+		init_features.set_initial_routing_sync();
+
+		// Attempt to pull out non-node-context feature flags from these InitFeatures.
+		let res = NodeFeatures::with_known_relevant_init_flags(&init_features);
+
+		{
+			// Check that the flags are as expected.
+			assert_eq!(res.flags[0], 0b00100010);
+			assert_eq!(res.flags[1], 0b00000010);
+			assert_eq!(res.flags.len(), 2);
+		}
+
+		// Check that the initial_routing_sync feature was correctly blanked out.
+		let new_features: InitFeatures = Features::from_le_bytes(res.flags);
+		assert!(!new_features.initial_routing_sync());
 	}
 }