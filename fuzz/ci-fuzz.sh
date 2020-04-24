ecb94689bfdc3a2d5e82e04967dbf0622ef71dae
Matt Corallo <git@bluematt.me>
2020-04-24 17:35:18 -0400

    Include color when building fuzz targets in CI

--- fuzz/ci-fuzz.sh
+++ fuzz/ci-fuzz.sh
@@ -12,9 +12,9 @@ rm *_target.rs
 [ "$(git diff)" != "" ] && exit 1
 popd
 
-cargo install --force honggfuzz
+cargo install --color always --force honggfuzz
 sed -i 's/lto = true//' Cargo.toml
-HFUZZ_BUILD_ARGS="--features honggfuzz_fuzz" cargo hfuzz build
+HFUZZ_BUILD_ARGS="--features honggfuzz_fuzz" cargo --color always hfuzz build
 for TARGET in src/bin/*.rs; do
 	FILENAME=$(basename $TARGET)
 	FILE="${FILENAME%.*}"
@@ -27,7 +27,7 @@ for TARGET in src/bin/*.rs; do
 		HFUZZ_RUN_ARGS="$HFUZZ_RUN_ARGS -N1000000"
 	fi
 	export HFUZZ_RUN_ARGS
-	HFUZZ_BUILD_ARGS="--features honggfuzz_fuzz" cargo hfuzz run $FILE
+	HFUZZ_BUILD_ARGS="--features honggfuzz_fuzz" cargo --color always hfuzz run $FILE
 	if [ -f hfuzz_workspace/$FILE/HONGGFUZZ.REPORT.TXT ]; then
 		cat hfuzz_workspace/$FILE/HONGGFUZZ.REPORT.TXT
 		for CASE in hfuzz_workspace/$FILE/SIG*; do
