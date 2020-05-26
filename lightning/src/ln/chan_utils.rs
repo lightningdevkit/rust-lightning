2c112a690b978e9ceeca0940a57c7eebb1dc5b05
Antoine Riard <dev@ariard.me>
2020-05-26 03:55:03 -0400

    Add get_anchor_script
    This is a script builder to generate anchor output ones. They can be
    satisfied either by a signature for the committed funding pubkey or anyone
    after CSV delay expiration.
    
    This is used at anchor output addition while generating commitment transaction.

--- lightning/src/ln/chan_utils.rs
+++ lightning/src/ln/chan_utils.rs
@@ -407,6 +407,23 @@ pub fn get_revokeable_redeemscript(revocation_key: &PublicKey, contest_delay: u1
 	res
 }
 
+/// Gets the witnessScript for an anchor output from the funding public key.
+/// The witness in the spending input must be:
+/// <BIP 143 funding_signature>
+/// After 10 blocks of confirmation, an alternative satisfying witness could be:
+/// <>
+#[inline]
+pub(crate) fn get_anchor_redeemscript(funding_pubkey: &PublicKey) -> Script {
+	Builder::new().push_slice(&funding_pubkey.serialize()[..])
+	              .push_opcode(opcodes::all::OP_CHECKSIG)
+		      .push_opcode(opcodes::all::OP_IFDUP)
+		      .push_opcode(opcodes::all::OP_NOTIF)
+		      .push_int(16)
+		      .push_opcode(opcodes::all::OP_CSV)
+		      .push_opcode(opcodes::all::OP_ENDIF)
+		      .into_script()
+}
+
 #[derive(Clone, PartialEq)]
 /// Information about an HTLC as it appears in a commitment transaction
 pub struct HTLCOutputInCommitment {
