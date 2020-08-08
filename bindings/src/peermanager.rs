f7b893bf85c5f103d98ccd50ba93c34b73ab65a1
joe.miyamoto <joe.miyamoto@bitcoinbank.co.jp>
2020-08-08 17:52:23 +0900

    add FFI entrypoint for constructing PeerManager from NetworkGraph

--- bindings/src/peermanager.rs
+++ bindings/src/peermanager.rs
@@ -10,6 +10,7 @@ use lightning::{
     ln::peer_handler::{PeerManager, MessageHandler},
     routing::network_graph::NetGraphMsgHandler,
     util::config::UserConfig,
+    util::ser::Readable
 };
 
 use crate::{
@@ -25,6 +26,7 @@ use crate::{
         FFIArcChannelManager
     }
 };
+use lightning::routing::network_graph::NetworkGraph;
 
 type FFISimpleArcPeerManager = PeerManager<FFISocketDescriptor, &'static FFIArcChannelManager, Arc<NetGraphMsgHandler<Arc<FFIChainWatchInterface>, Arc<FFILogger>>>, Arc<FFILogger>>;
 type FFIArcPeerManagerHandle = HandleShared<'static, FFISimpleArcPeerManager>;
@@ -42,6 +44,62 @@ fn construct_socket_desc (
     let disconnect_socket_ref = unsafe_block!("" =>  disconnect_socket_ptr.as_ref());
     let socket = FFISocketDescriptor { index, send_data_ptr: *send_data_ref, disconnect_socket_ptr: *disconnect_socket_ref };
     socket
+}
+
+fn create_peer_manager_inner(
+    seed: Ref<Bytes32>,
+    cfg: Ref<UserConfig>,
+    chan_man: FFIArcChannelManagerHandle,
+    install_watch_tx_ptr: Ref<chain_watch_interface_fn::InstallWatchTxPtr>,
+    install_watch_outpoint_ptr: Ref<chain_watch_interface_fn::InstallWatchOutpointPtr>,
+    watch_all_txn_ptr: Ref<chain_watch_interface_fn::WatchAllTxnPtr>,
+    get_chain_utxo_ptr: Ref<chain_watch_interface_fn::GetChainUtxoPtr>,
+    filter_block_ptr: Ref<chain_watch_interface_fn::FilterBlock>,
+    reentered_ptr: Ref<chain_watch_interface_fn::ReEntered>,
+    log_ptr: Ref<ffilogger_fn::LogExtern>,
+    our_node_secret_ptr: Ref<Bytes32>,
+    maybe_network_graph: Option<NetworkGraph>,
+) -> FFISimpleArcPeerManager {
+    let log_ref = unsafe_block!("" => log_ptr.as_ref());
+
+    let our_node_secret: secp256k1::SecretKey =  {
+        let o = unsafe_block!("" => our_node_secret_ptr.as_ref());
+        o.clone().into()
+    };
+    let install_watch_tx_ref = unsafe_block!("function pointer lives as long as ChainWatchInterface and it points to valid data"  => install_watch_tx_ptr.as_ref());
+    let install_watch_outpoint_ref = unsafe_block!("function pointer lives as long as ChainWatchInterface and it points to valid data"  => install_watch_outpoint_ptr.as_ref());
+    let watch_all_txn_ref = unsafe_block!("function pointer lives as long as ChainWatchInterface and it points to valid data"  => watch_all_txn_ptr.as_ref());
+    let get_chain_utxo_ref = unsafe_block!("function pointer lives as long as ChainWatchInterface and it points to valid data"  => get_chain_utxo_ptr.as_ref());
+    let filter_block_ref = unsafe_block!("function pointer lives as long as ChainWatchInterface and it points to valid data"  => filter_block_ptr.as_ref());
+    let reentered_ref = unsafe_block!("function pointer lives as long as ChainWatchInterface and it points to valid data"  => reentered_ptr.as_ref());
+
+    let logger_arc = Arc::new(FFILogger { log_ptr: *log_ref });
+    let chain_watch_interface_arc =
+        Arc::new(FFIChainWatchInterface::new(
+            *install_watch_tx_ref,
+            *install_watch_outpoint_ref,
+            *watch_all_txn_ref,
+            *get_chain_utxo_ref,
+            *filter_block_ref,
+            *reentered_ref
+        ));
+    let route_handler =
+        match maybe_network_graph {
+            Some(network) => {
+                NetGraphMsgHandler::from_net_graph(chain_watch_interface_arc, logger_arc.clone(), network)
+            },
+            None => {
+                NetGraphMsgHandler::new(chain_watch_interface_arc, logger_arc.clone())
+            },
+        };
+    let chan_man = chan_man.as_static_ref();
+    let msg_handler =
+        MessageHandler { chan_handler: chan_man, route_handler: Arc::new(route_handler) };
+
+    let seed = unsafe_block!("It points to valid length buffer" => seed.as_ref());
+    let peer_man =
+        FFISimpleArcPeerManager::new(msg_handler, our_node_secret.clone(), &seed.bytes, logger_arc);
+    peer_man
 }
 
 ffi! {
@@ -62,41 +120,64 @@ ffi! {
         our_node_secret_ptr: Ref<Bytes32>,
         handle: Out<FFIArcPeerManagerHandle>
     ) -> FFIResult {
-        let log_ref = unsafe_block!("" => log_ptr.as_ref());
-
-        let our_node_secret: secp256k1::SecretKey =  {
-            let o = unsafe_block!("" => our_node_secret_ptr.as_ref());
-            o.clone().into()
-        };
-        let install_watch_tx_ref = unsafe_block!("function pointer lives as long as ChainWatchInterface and it points to valid data"  => install_watch_tx_ptr.as_ref());
-        let install_watch_outpoint_ref = unsafe_block!("function pointer lives as long as ChainWatchInterface and it points to valid data"  => install_watch_outpoint_ptr.as_ref());
-        let watch_all_txn_ref = unsafe_block!("function pointer lives as long as ChainWatchInterface and it points to valid data"  => watch_all_txn_ptr.as_ref());
-        let get_chain_utxo_ref = unsafe_block!("function pointer lives as long as ChainWatchInterface and it points to valid data"  => get_chain_utxo_ptr.as_ref());
-        let filter_block_ref = unsafe_block!("function pointer lives as long as ChainWatchInterface and it points to valid data"  => filter_block_ptr.as_ref());
-        let reentered_ref = unsafe_block!("function pointer lives as long as ChainWatchInterface and it points to valid data"  => reentered_ptr.as_ref());
-
-        let logger_arc = Arc::new(FFILogger { log_ptr: *log_ref });
-        let chain_watch_interface_arc =
-            Arc::new(FFIChainWatchInterface::new(
-                *install_watch_tx_ref,
-                *install_watch_outpoint_ref,
-                *watch_all_txn_ref,
-                *get_chain_utxo_ref,
-                *filter_block_ref,
-                *reentered_ref
-            ));
-        let route_handler = NetGraphMsgHandler::new(chain_watch_interface_arc, logger_arc.clone());
-        let chan_man = chan_man.as_static_ref();
-        let msg_handler =
-            MessageHandler { chan_handler: chan_man, route_handler: Arc::new(route_handler) };
-
-        let seed = unsafe_block!("It points to valid length buffer" => seed.as_ref());
         let peer_man =
-            FFISimpleArcPeerManager::new(msg_handler, our_node_secret.clone(), &seed.bytes, logger_arc);
+            create_peer_manager_inner(
+                seed,
+                cfg,
+                chan_man,
+                install_watch_tx_ptr,
+                install_watch_outpoint_ptr,
+                watch_all_txn_ptr,
+                get_chain_utxo_ptr,
+                filter_block_ptr,
+                reentered_ptr,
+                log_ptr,
+                our_node_secret_ptr,
+                None
+            );
         unsafe_block!("" => handle.init(FFIArcPeerManagerHandle::alloc(peer_man)));
         FFIResult::ok()
     }
 
+    fn create_peer_manager_from_net_graph(
+        seed: Ref<Bytes32>,
+        cfg: Ref<UserConfig>,
+
+        chan_man: FFIArcChannelManagerHandle,
+        install_watch_tx_ptr: Ref<chain_watch_interface_fn::InstallWatchTxPtr>,
+        install_watch_outpoint_ptr: Ref<chain_watch_interface_fn::InstallWatchOutpointPtr>,
+        watch_all_txn_ptr: Ref<chain_watch_interface_fn::WatchAllTxnPtr>,
+        get_chain_utxo_ptr: Ref<chain_watch_interface_fn::GetChainUtxoPtr>,
+        filter_block_ptr: Ref<chain_watch_interface_fn::FilterBlock>,
+        reentered_ptr: Ref<chain_watch_interface_fn::ReEntered>,
+
+        log_ptr: Ref<ffilogger_fn::LogExtern>,
+
+        our_node_secret_ptr: Ref<Bytes32>,
+        network_graph_buf_ptr: Ref<u8>,
+        network_graph_buf_len: usize,
+        handle: Out<FFIArcPeerManagerHandle>
+    ) -> FFIResult {
+        let mut net_graph_buf = unsafe_block!("" => network_graph_buf_ptr.as_bytes(network_graph_buf_len));
+        let net_graph: NetworkGraph = Readable::read(&mut net_graph_buf).unwrap();
+        let peer_man =
+            create_peer_manager_inner(
+                seed,
+                cfg,
+                chan_man,
+                install_watch_tx_ptr,
+                install_watch_outpoint_ptr,
+                watch_all_txn_ptr,
+                get_chain_utxo_ptr,
+                filter_block_ptr,
+                reentered_ptr,
+                log_ptr,
+                our_node_secret_ptr,
+                Some(net_graph),
+            );
+        unsafe_block!("" => handle.init(FFIArcPeerManagerHandle::alloc(peer_man)));
+        FFIResult::ok()
+    }
     fn new_inbound_connection(
         index: usize,
         send_data_ptr: Ref<socket_descriptor_fn::SendData>,
