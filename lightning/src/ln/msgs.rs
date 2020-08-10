4395b92cc8bfe0cc803e70bba11f4db58d5d0dbf
Matt Corallo <git@bluematt.me>
2020-08-10 15:00:09 -0400

    Relicense as dual Apache-2.0 + MIT
    This changes the LICENSE file and adds license headers to most files
    to relicense under dual Apache-2.0 and MIT. This is helpful in that
    we retain the patent grant issued under Apache-2.0-licensed work,
    avoiding some sticky patent issues, while still allowing users who
    are more comfortable with the simpler MIT license to use that.
    
    See https://github.com/rust-bitcoin/rust-lightning/issues/659 for
    relicensing statements from code authors.

--- lightning/src/ln/msgs.rs
+++ lightning/src/ln/msgs.rs
@@ -1,3 +1,12 @@
+// This file is Copyright its original authors, visible in version control
+// history.
+//
+// This file is licensed under the Apache License, Version 2.0 <LICENSE-APACHE
+// or http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
+// You may not use this file except in accordance with one or both of these
+// licenses.
+
 //! Wire messages, traits representing wire message handlers, and a few error types live here.
 //!
 //! For a normal node you probably don't need to use anything here, however, if you wish to split a
