6d79c6ff024fa1d0c7431f4ceb78a20cb76dd536
Jeffrey Czyz <jkczyz@gmail.com>
2020-10-06 14:46:44 -0700

    Add unit tests for utils::hex_to_uint256

--- lightning-block-sync/src/utils.rs
+++ lightning-block-sync/src/utils.rs
@@ -43,3 +43,44 @@ pub fn hex_to_vec(hex: &str) -> Option<Vec<u8>> {
 
 	Some(out)
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use bitcoin::util::uint::Uint256;
+
+	#[test]
+	fn hex_to_uint256_empty_str() {
+		assert!(hex_to_uint256("").is_none());
+	}
+
+	#[test]
+	fn hex_to_uint256_too_short_str() {
+		let hex = String::from_utf8(vec![b'0'; 63]).unwrap();
+		assert!(hex_to_uint256(&hex).is_none());
+	}
+
+	#[test]
+	fn hex_to_uint256_too_long_str() {
+		let hex = String::from_utf8(vec![b'0'; 65]).unwrap();
+		assert!(hex_to_uint256(&hex).is_none());
+	}
+
+	#[test]
+	fn hex_to_uint256_invalid_char() {
+		let hex = String::from_utf8(vec![b'G'; 64]).unwrap();
+		assert!(hex_to_uint256(&hex).is_none());
+	}
+
+	#[test]
+	fn hex_to_uint256_lowercase_str() {
+		let hex: String = std::iter::repeat("0123456789abcdef").take(4).collect();
+		assert_eq!(hex_to_uint256(&hex).unwrap(), Uint256([0x0123456789abcdefu64; 4]));
+	}
+
+	#[test]
+	fn hex_to_uint256_uppercase_str() {
+		let hex: String = std::iter::repeat("0123456789ABCDEF").take(4).collect();
+		assert_eq!(hex_to_uint256(&hex).unwrap(), Uint256([0x0123456789abcdefu64; 4]));
+	}
+}
