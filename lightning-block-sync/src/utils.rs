a7ac3c3280681fc380fa91dc514476ed0a5fc916
Jeffrey Czyz <jkczyz@gmail.com>
2020-10-06 16:06:15 -0700

    Add unit tests for utils::hex_to_vec

--- lightning-block-sync/src/utils.rs
+++ lightning-block-sync/src/utils.rs
@@ -83,4 +83,39 @@ mod tests {
 		let hex: String = std::iter::repeat("0123456789ABCDEF").take(4).collect();
 		assert_eq!(hex_to_uint256(&hex).unwrap(), Uint256([0x0123456789abcdefu64; 4]));
 	}
+
+	#[test]
+	fn hex_to_vec_empty_str() {
+		assert_eq!(hex_to_vec("").unwrap(), Vec::<u8>::new());
+	}
+
+	#[test]
+	fn hex_to_vec_odd_length_str() {
+		let hex = "123456789";
+		assert!(hex_to_vec(&hex).is_none());
+	}
+
+	#[test]
+	fn hex_to_vec_even_length_str() {
+		let hex = "0123456789";
+		assert_eq!(hex_to_vec(&hex).unwrap(), vec![0x01u8, 0x23u8, 0x45u8, 0x67u8, 0x89u8]);
+	}
+
+	#[test]
+	fn hex_to_vec_invalid_char() {
+		let hex = String::from_utf8(vec![b'G'; 64]).unwrap();
+		assert!(hex_to_vec(&hex).is_none());
+	}
+
+	#[test]
+	fn hex_to_vec_lowercase_str() {
+		let hex: String = std::iter::repeat("ef").take(32).collect();
+		assert_eq!(hex_to_vec(&hex).unwrap(), vec![0xefu8; 32]);
+	}
+
+	#[test]
+	fn hex_to_vec_uppercase_str() {
+		let hex: String = std::iter::repeat("EF").take(32).collect();
+		assert_eq!(hex_to_vec(&hex).unwrap(), vec![0xefu8; 32]);
+	}
 }
