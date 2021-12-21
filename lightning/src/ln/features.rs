cb823059fec283851f67cf53455ccdeafb29acba
Matt Corallo <git@bluematt.me>
2021-12-21 05:23:51 +0000

    Unset the optional bit for a feature when setting the required bit
    There is no reason to set both, and this currently makes testing
    the new BOLT invoice tests slightly harder, so we just unset it.

--- lightning/src/ln/features.rs
+++ lightning/src/ln/features.rs
@@ -308,6 +308,7 @@ mod sealed {
 					}
 
 					flags[Self::BYTE_OFFSET] |= Self::REQUIRED_MASK;
+					flags[Self::BYTE_OFFSET] &= !Self::OPTIONAL_MASK;
 				}
 
 				/// Sets the feature's optional (odd) bit in the given flags.
