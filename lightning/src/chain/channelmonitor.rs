214c222b79ea9ac78fab0d7126085ec3cdc4ecb5
QPotato <federicobadaloni@hotmail.com>
2022-07-21 21:14:54 -0300

    Add documentation on public method

--- lightning/src/chain/channelmonitor.rs
+++ lightning/src/chain/channelmonitor.rs
@@ -1187,6 +1187,7 @@ impl<Signer: Sign> ChannelMonitor<Signer> {
 			payment_hash, payment_preimage, broadcaster, fee_estimator, logger)
 	}
 
+	// Broadcasts the latest commitment transaction only if it's safe to do so.
 	pub(crate) fn maybe_broadcast_latest_holder_commitment_txn<B: Deref, L: Deref>(
 		&self,
 		broadcaster: &B,
@@ -1198,6 +1199,8 @@ impl<Signer: Sign> ChannelMonitor<Signer> {
 		self.inner.lock().unwrap().maybe_broadcast_latest_holder_commitment_txn(broadcaster, logger)
 	}
 
+	// Broadcasts the latest commitment transaction, even if we can't ensure it's safe to do so
+	// due to missing information.
 	pub fn force_broadcast_latest_holder_commitment_txn_unsafe<B: Deref, L: Deref>(
 		&self,
 		broadcaster: &B,
