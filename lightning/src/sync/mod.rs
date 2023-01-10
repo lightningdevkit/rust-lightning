f66f720fa54cdd28ac998dcc39c859567b2455e0
Matt Corallo <git@bluematt.me>
2023-01-10 06:26:46 +0000

    Move `no-std` sync implementations to a folder to clean up

---
+++ lightning/src/sync/mod.rs
@@ -0,0 +1,11 @@
+#[cfg(all(feature = "std", not(feature = "_bench_unstable"), test))]
+pub use crate::debug_sync::*;
+#[cfg(all(feature = "std", any(feature = "_bench_unstable", not(test))))]
+pub use ::std::sync::{Arc, Mutex, Condvar, MutexGuard, RwLock, RwLockReadGuard, RwLockWriteGuard};
+#[cfg(all(feature = "std", any(feature = "_bench_unstable", not(test))))]
+pub use crate::util::fairrwlock::FairRwLock;
+
+#[cfg(not(feature = "std"))]
+mod nostd_sync;
+#[cfg(not(feature = "std"))]
+pub use nostd_sync::*;
