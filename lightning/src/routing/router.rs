6da0e0683fec994e9d79860125306c08c1893916
Lalitmohansharma1 <lalit_ms@ar.iitr.ac.in>
2023-02-20 15:33:56 +0530

    access hashmap

--- lightning/src/routing/router.rs
+++ lightning/src/routing/router.rs
@@ -1244,21 +1244,24 @@ where L::Target: Logger {
 					if !contributes_sufficient_value || exceeds_max_path_length ||
 						exceeds_cltv_delta_limit || payment_failed_on_this_channel {
 						let mut is_first_hop = true;
-						if let Some(target) = first_hop_targets.get(&NodeId::from_pubkey(&our_node_pubkey)){
-							for channel_details in target {
-								if let Some(short_channel_id_target) = channel_details.short_channel_id{
-									if short_channel_id_target == short_channel_id { // short_channel_id is hop id of candidate.
-										if is_first_hop && !contributes_sufficient_value {
-        										log_trace!(logger, "First Hop {short_channel_id} is excluded due to insufficient value");
-    										} else if is_first_hop && exceeds_max_path_length {
-    											log_trace!(logger, "First Hop {short_channel_id} is excluded due to candidate hop excluded max path length");
-    										} else if is_first_hop && exceeds_cltv_delta_limit {
-    											log_trace!(logger, "First Hop {short_channel_id} is excluded beacause it exceed the maximum total cltv expiry limit");
-    										} else if is_first_hop && payment_failed_on_this_channel {
-    											log_trace!(logger, "First Hop {short_channel_id} is excluded beacause it was failed previously");
+						// if let Some(target) = first_hop_targets.get(&NodeId::from_pubkey(&our_node_pubkey)) {
+						for (key ,value) in &first_hop_targets {
+							if (key == &NodeId::from_pubkey(&our_node_pubkey)) { 
+								for channel_details in value {
+									if let Some(short_channel_id_target) = channel_details.short_channel_id{
+										if short_channel_id_target == short_channel_id { // short_channel_id is hop id of candidate.
+											if is_first_hop && !contributes_sufficient_value {
+        											log_trace!(logger, "First Hop {short_channel_id} is excluded due to insufficient value");
+    											} else if is_first_hop && exceeds_max_path_length {
+    												log_trace!(logger, "First Hop {short_channel_id} is excluded due to candidate hop excluded max path length");	
+    											} else if is_first_hop && exceeds_cltv_delta_limit {
+    												log_trace!(logger, "First Hop {short_channel_id} is excluded beacause it exceed the maximum total cltv expiry limit");		
+    											} else if is_first_hop && payment_failed_on_this_channel {
+    												log_trace!(logger, "First Hop {short_channel_id} is excluded beacause it was failed previously");
+    											}
+    											is_first_hop = false;			
     										}
-    										is_first_hop = false;			
-    									}
+									}	
 								}
 							}
 						}
