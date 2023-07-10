lightningdevkit/rust-lightning

commit f2453b7fffd3ca79cac92803c8c6c99cea970e51

Allow events processing without holding `total_consistency_lock`
Unfortunately, the RAII types used by `RwLock` are not `Send`, which is
why they can't be held over `await` boundaries. In order to allow
asynchronous events processing in multi-threaded environments, we here
allow to process events without holding the `total_consistency_lock`.

lightning/src/ln/channelmanager.rs
@@ -72,7 +72,7 @@ use core::{cmp, mem};
 use core::cell::RefCell;
 use crate::io::Read;
 use crate::sync::{Arc, Mutex, RwLock, RwLockReadGuard, FairRwLock, LockTestExt, LockHeldState};
-use core::sync::atomic::{AtomicUsize, Ordering};
+use core::sync::atomic::{AtomicUsize, AtomicBool, Ordering};
 use core::time::Duration;
 use core::ops::Deref;
 
@@ -926,6 +926,8 @@ where
 
 	/// See `ChannelManager` struct-level documentation for lock order requirements.
 	pending_events: Mutex<Vec<events::Event>>,
+	/// A simple atomic flag to ensure only one task at a time can be processing events asynchronously.
+	pending_events_processor: AtomicBool,
 	/// See `ChannelManager` struct-level documentation for lock order requirements.
 	pending_background_events: Mutex<Vec<BackgroundEvent>>,
 	/// Used when we have to take a BIG lock to make sure everything is self-consistent.
@@ -1680,30 +1682,47 @@ macro_rules! handle_new_monitor_update {
 
 macro_rules! process_events_body {
 	($self: expr, $event_to_handle: expr, $handle_event: expr) => {
-		// We'll acquire our total consistency lock until the returned future completes so that
-		// we can be sure no other persists happen while processing events.
-		let _read_guard = $self.total_consistency_lock.read().unwrap();
+		let mut processed_all_events = false;
+		while !processed_all_events {
+			if $self.pending_events_processor.compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed).is_err() {
+				return;
+			}
 
-		let mut result = NotifyOption::SkipPersist;
+			let mut result = NotifyOption::SkipPersist;
 
-		// TODO: This behavior should be documented. It's unintuitive that we query
-		// ChannelMonitors when clearing other events.
-		if $self.process_pending_monitor_events() {
-			result = NotifyOption::DoPersist;
-		}
+			{
+				// We'll acquire our total consistency lock so that we can be sure no other
+				// persists happen while processing monitor events.
+				let _read_guard = $self.total_consistency_lock.read().unwrap();
 
-		let pending_events = mem::replace(&mut *$self.pending_events.lock().unwrap(), vec![]);
-		if !pending_events.is_empty() {
-			result = NotifyOption::DoPersist;
-		}
+				// TODO: This behavior should be documented. It's unintuitive that we query
+				// ChannelMonitors when clearing other events.
+				if $self.process_pending_monitor_events() {
+					result = NotifyOption::DoPersist;
+				}
+			}
 
-		for event in pending_events {
-			$event_to_handle = event;
-			$handle_event;
-		}
+			let pending_events = $self.pending_events.lock().unwrap().clone();
+			let num_events = pending_events.len();
+			if !pending_events.is_empty() {
+				result = NotifyOption::DoPersist;
+			}
 
-		if result == NotifyOption::DoPersist {
-			$self.persistence_notifier.notify();
+			for event in pending_events {
+				$event_to_handle = event;
+				$handle_event;
+			}
+
+			{
+				let mut pending_events = $self.pending_events.lock().unwrap();
+				pending_events.drain(..num_events);
+				processed_all_events = pending_events.is_empty();
+				$self.pending_events_processor.store(false, Ordering::Release);
+			}
+
+			if result == NotifyOption::DoPersist {
+				$self.persistence_notifier.notify();
+			}
 		}
 	}
 }
@@ -1771,6 +1790,7 @@ where
 			per_peer_state: FairRwLock::new(HashMap::new()),
 
 			pending_events: Mutex::new(Vec::new()),
+			pending_events_processor: AtomicBool::new(false),
 			pending_background_events: Mutex::new(Vec::new()),
 			total_consistency_lock: RwLock::new(()),
 			persistence_notifier: Notifier::new(),
@@ -7916,6 +7936,7 @@ where
 			per_peer_state: FairRwLock::new(per_peer_state),
 
 			pending_events: Mutex::new(pending_events_read),
+			pending_events_processor: AtomicBool::new(false),
 			pending_background_events: Mutex::new(pending_background_events),
 			total_consistency_lock: RwLock::new(()),
 			persistence_notifier: Notifier::new(),

lightning-background-processor/src/lib.rs
@@ -1485,10 +1485,9 @@ mod tests {
 				})
 			}, false,
 		);
-		// TODO: Drop _local and simply spawn after #2003
-		let local_set = tokio::task::LocalSet::new();
-		local_set.spawn_local(bp_future);
-		local_set.spawn_local(async move {
+
+		let t1 = tokio::spawn(bp_future);
+		let t2 = tokio::spawn(async move {
 			do_test_not_pruning_network_graph_until_graph_sync_completion!(nodes, {
 				let mut i = 0;
 				loop {
@@ -1500,7 +1499,9 @@ mod tests {
 			}, tokio::time::sleep(Duration::from_millis(1)).await);
 			exit_sender.send(()).unwrap();
 		});
-		local_set.await;
+		let (r1, r2) = tokio::join!(t1, t2);
+		r1.unwrap().unwrap();
+		r2.unwrap()
 	}
 
 	macro_rules! do_test_payment_path_scoring {
@@ -1654,13 +1655,14 @@ mod tests {
 				})
 			}, false,
 		);
-		// TODO: Drop _local and simply spawn after #2003
-		let local_set = tokio::task::LocalSet::new();
-		local_set.spawn_local(bp_future);
-		local_set.spawn_local(async move {
+		let t1 = tokio::spawn(bp_future);
+		let t2 = tokio::spawn(async move {
 			do_test_payment_path_scoring!(nodes, receiver.recv().await);
 			exit_sender.send(()).unwrap();
 		});
-		local_set.await;
+
+		let (r1, r2) = tokio::join!(t1, t2);
+		r1.unwrap().unwrap();
+		r2.unwrap()
 	}
 }

commit 81722ca8338e200551750c3b22df8312105a9f7b

Handle new event processing logic when enqueuing forward event
This was a regression resulting from f2453b7 since we now process events
in a loop until there aren't any left. Processing events is done in
batches and they are not removed until we're done processing each batch.
Since handling a `PendingHTLCsForwardable` event will call back into the
`ChannelManager`, we'll still see the original forwarding event not
removed. Phantom payments will need an additional forwarding event
before being claimed to make them look real by taking more time.

lightning-invoice/src/utils.rs
@@ -792,12 +792,13 @@ fn prefer_current_channel(min_inbound_capacity_msat: Option<u64>, current_channe
 
 #[cfg(test)]
 mod test {
+	use core::cell::RefCell;
 	use core::time::Duration;
 	use crate::{Currency, Description, InvoiceDescription, SignOrCreationError, CreationError};
 	use bitcoin_hashes::{Hash, sha256};
 	use bitcoin_hashes::sha256::Hash as Sha256;
 	use lightning::sign::PhantomKeysManager;
-	use lightning::events::{MessageSendEvent, MessageSendEventsProvider, Event};
+	use lightning::events::{MessageSendEvent, MessageSendEventsProvider, Event, EventsProvider};
 	use lightning::ln::{PaymentPreimage, PaymentHash};
 	use lightning::ln::channelmanager::{PhantomRouteHints, MIN_FINAL_CLTV_EXPIRY_DELTA, PaymentId, RecipientOnionFields, Retry};
 	use lightning::ln::functional_test_utils::*;
@@ -1357,13 +1358,20 @@ mod test {
 		// Note that we have to "forward pending HTLCs" twice before we see the PaymentClaimable as
 		// this "emulates" the payment taking two hops, providing some privacy to make phantom node
 		// payments "look real" by taking more time.
-		expect_pending_htlcs_forwardable_ignore!(nodes[fwd_idx]);
-		nodes[fwd_idx].node.process_pending_htlc_forwards();
-		expect_pending_htlcs_forwardable_ignore!(nodes[fwd_idx]);
-		nodes[fwd_idx].node.process_pending_htlc_forwards();
+		let other_events = RefCell::new(Vec::new());
+		let forward_event_handler = |event: Event| {
+			if let Event::PendingHTLCsForwardable { .. } = event {
+				nodes[fwd_idx].node.process_pending_htlc_forwards();
+			} else {
+				other_events.borrow_mut().push(event);
+			}
+		};
+		nodes[fwd_idx].node.process_pending_events(&forward_event_handler);
+		nodes[fwd_idx].node.process_pending_events(&forward_event_handler);
 
 		let payment_preimage_opt = if user_generated_pmt_hash { None } else { Some(payment_preimage) };
-		expect_payment_claimable!(&nodes[fwd_idx], payment_hash, payment_secret, payment_amt, payment_preimage_opt, invoice.recover_payee_pub_key());
+		assert_eq!(other_events.borrow().len(), 1);
+		check_payment_claimable(&other_events.borrow()[0], payment_hash, payment_secret, payment_amt, payment_preimage_opt, invoice.recover_payee_pub_key());
 		do_claim_payment_along_route(&nodes[0], &[&vec!(&nodes[fwd_idx])[..]], false, payment_preimage);
 		let events = nodes[0].node.get_and_clear_pending_events();
 		assert_eq!(events.len(), 2);

lightning/src/ln/channelmanager.rs
@@ -1978,6 +1978,8 @@ macro_rules! process_events_body {
 				let mut pending_events = $self.pending_events.lock().unwrap();
 				pending_events.drain(..num_events);
 				processed_all_events = pending_events.is_empty();
+				// Note that `push_pending_forwards_ev` relies on `pending_events_processor` being
+				// updated here with the `pending_events` lock acquired.
 				$self.pending_events_processor.store(false, Ordering::Release);
 			}
 
@@ -5686,16 +5688,21 @@ where
 		}
 	}
 
-	// We only want to push a PendingHTLCsForwardable event if no others are queued.
 	fn push_pending_forwards_ev(&self) {
 		let mut pending_events = self.pending_events.lock().unwrap();
-		let forward_ev_exists = pending_events.iter()
-			.find(|(ev, _)| if let events::Event::PendingHTLCsForwardable { .. } = ev { true } else { false })
-			.is_some();
-		if !forward_ev_exists {
-			pending_events.push_back((events::Event::PendingHTLCsForwardable {
-				time_forwardable:
-					Duration::from_millis(MIN_HTLC_RELAY_HOLDING_CELL_MILLIS),
+		let is_processing_events = self.pending_events_processor.load(Ordering::Acquire);
+		let num_forward_events = pending_events.iter().filter(|(ev, _)|
+			if let events::Event::PendingHTLCsForwardable { .. } = ev { true } else { false }
+		).count();
+		// We only want to push a PendingHTLCsForwardable event if no others are queued. Processing
+		// events is done in batches and they are not removed until we're done processing each
+		// batch. Since handling a `PendingHTLCsForwardable` event will call back into the
+		// `ChannelManager`, we'll still see the original forwarding event not removed. Phantom
+		// payments will need an additional forwarding event before being claimed to make them look
+		// real by taking more time.
+		if (is_processing_events && num_forward_events <= 1) || num_forward_events < 1 {
+			pending_events.push_back((Event::PendingHTLCsForwardable {
+				time_forwardable: Duration::from_millis(MIN_HTLC_RELAY_HOLDING_CELL_MILLIS),
 			}, None));
 		}
 	}

lightning/src/ln/functional_test_utils.rs
@@ -1806,6 +1806,28 @@ macro_rules! get_route_and_payment_hash {
 	}}
 }
 
+pub fn check_payment_claimable(
+	event: &Event, expected_payment_hash: PaymentHash, expected_payment_secret: PaymentSecret,
+	expected_recv_value: u64, expected_payment_preimage: Option<PaymentPreimage>,
+	expected_receiver_node_id: PublicKey,
+) {
+	match event {
+		Event::PaymentClaimable { ref payment_hash, ref purpose, amount_msat, receiver_node_id, .. } => {
+			assert_eq!(expected_payment_hash, *payment_hash);
+			assert_eq!(expected_recv_value, *amount_msat);
+			assert_eq!(expected_receiver_node_id, receiver_node_id.unwrap());
+			match purpose {
+				PaymentPurpose::InvoicePayment { payment_preimage, payment_secret, .. } => {
+					assert_eq!(&expected_payment_preimage, payment_preimage);
+					assert_eq!(expected_payment_secret, *payment_secret);
+				},
+				_ => {},
+			}
+		},
+		_ => panic!("Unexpected event"),
+	}
+}
+
 #[macro_export]
 #[cfg(any(test, ldk_bench, feature = "_test_utils"))]
 macro_rules! expect_payment_claimable {
@@ -1815,22 +1837,8 @@ macro_rules! expect_payment_claimable {
 	($node: expr, $expected_payment_hash: expr, $expected_payment_secret: expr, $expected_recv_value: expr, $expected_payment_preimage: expr, $expected_receiver_node_id: expr) => {
 		let events = $node.node.get_and_clear_pending_events();
 		assert_eq!(events.len(), 1);
-		match events[0] {
-			$crate::events::Event::PaymentClaimable { ref payment_hash, ref purpose, amount_msat, receiver_node_id, .. } => {
-				assert_eq!($expected_payment_hash, *payment_hash);
-				assert_eq!($expected_recv_value, amount_msat);
-				assert_eq!($expected_receiver_node_id, receiver_node_id.unwrap());
-				match purpose {
-					$crate::events::PaymentPurpose::InvoicePayment { payment_preimage, payment_secret, .. } => {
-						assert_eq!(&$expected_payment_preimage, payment_preimage);
-						assert_eq!($expected_payment_secret, *payment_secret);
-					},
-					_ => {},
-				}
-			},
-			_ => panic!("Unexpected event"),
-		}
-	}
+		$crate::ln::functional_test_utils::check_payment_claimable(&events[0], $expected_payment_hash, $expected_payment_secret, $expected_recv_value, $expected_payment_preimage, $expected_receiver_node_id)
+	};
 }
 
 #[macro_export]