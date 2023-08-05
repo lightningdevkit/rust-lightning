67e788eaa7b12e102c33d568017be3b97080f198
Willem Van Lint <noreply@wvanlint.dev>
2023-08-04 17:07:04 -0700

    Include pending HTLCs in ChannelDetails
    This exposes details around pending HTLCs in ChannelDetails.  The state
    of the HTLC in the state machine is also included, so it can be
    determined which protocol message the HTLC is waiting for to advance.

--- lightning/src/routing/router.rs
+++ lightning/src/routing/router.rs
@@ -3294,6 +3294,8 @@ mod tests {
 			config: None,
 			feerate_sat_per_1000_weight: None,
 			channel_shutdown_state: Some(channelmanager::ChannelShutdownState::NotShuttingDown),
+			pending_inbound_htlcs: Vec::new(),
+			pending_outbound_htlcs: Vec::new(),
 		}
 	}
 
@@ -8435,6 +8437,8 @@ pub(crate) mod bench_utils {
 			config: None,
 			feerate_sat_per_1000_weight: None,
 			channel_shutdown_state: Some(channelmanager::ChannelShutdownState::NotShuttingDown),
+			pending_inbound_htlcs: Vec::new(),
+			pending_outbound_htlcs: Vec::new(),
 		}
 	}
 
