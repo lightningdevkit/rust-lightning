90888948ed6b554e9afe5819f928d5c1ec2d386f
Duncan Dean <git@dunxen.dev>
2023-09-13 23:42:05 +0200

    Add `option_dual_fund` feature

--- lightning/src/ln/channelmanager.rs
+++ lightning/src/ln/channelmanager.rs
@@ -11354,6 +11354,8 @@ pub fn provided_init_features(config: &UserConfig) -> InitFeatures {
 	if config.channel_handshake_config.negotiate_anchors_zero_fee_htlc_tx {
 		features.set_anchors_zero_fee_htlc_tx_optional();
 	}
+	#[cfg(any(dual_funding, splicing))]
+	features.set_dual_fund_optional();
 	features
 }
 
