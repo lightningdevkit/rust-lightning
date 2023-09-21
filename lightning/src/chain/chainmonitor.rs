974ba314a29fa35f2ed662b9357fa9cd701e444d
Matt Corallo <git@bluematt.me>
2023-09-21 20:21:13 +0000

    Refer to an "outage" over a "timeout", for when failure happens
    Timeouts may be worth retrying, but an outage is a more general
    term which obviously cannot be retried.

--- lightning/src/chain/chainmonitor.rs
+++ lightning/src/chain/chainmonitor.rs
@@ -98,7 +98,7 @@ impl MonitorUpdateId {
 ///    If at some point no further progress can be made towards persisting the pending updates, the
 ///    node should simply shut down.
 ///
-///  * If the persistence has failed and cannot be retried further (e.g. because of some timeout),
+///  * If the persistence has failed and cannot be retried further (e.g. because of an outage),
 ///    [`ChannelMonitorUpdateStatus::UnrecoverableError`] can be used, though this will result in
 ///    an immediate panic and future operations in LDK generally failing.
 ///
