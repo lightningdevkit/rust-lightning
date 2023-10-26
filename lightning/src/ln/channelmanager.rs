df2d0a47d528b00ee8da80917946beab746a7685
Valentine Wallace <vwallace@protonmail.com>
2023-10-26 16:56:43 -0400

    Add variant for non-intro-nodes to BlindedFailure enum
    For use in supporting receiving to multi-hop blinded paths.

--- lightning/src/ln/channelmanager.rs
+++ lightning/src/ln/channelmanager.rs
@@ -292,7 +292,7 @@ pub(super) enum HTLCForwardInfo {
 #[derive(Clone, Debug, Hash, PartialEq, Eq)]
 enum BlindedFailure {
 	FromIntroductionNode,
-	// Another variant will be added here for non-intro nodes.
+	FromBlindedNode,
 }
 
 /// Tracks the inbound corresponding to an outbound HTLC
@@ -5243,6 +5243,7 @@ where
 							incoming_packet_shared_secret, phantom_shared_secret
 						)
 					},
+					Some(BlindedFailure::FromBlindedNode) => todo!(),
 					None => {
 						onion_error.get_encrypted_failure_packet(incoming_packet_shared_secret, phantom_shared_secret)
 					}
@@ -9467,7 +9468,8 @@ impl_writeable_tlv_based_enum!(PendingHTLCStatus, ;
 );
 
 impl_writeable_tlv_based_enum!(BlindedFailure,
-	(0, FromIntroductionNode) => {}, ;
+	(0, FromIntroductionNode) => {},
+	(2, FromBlindedNode) => {}, ;
 );
 
 impl_writeable_tlv_based!(HTLCPreviousHopData, {
