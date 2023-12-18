f09ac1931f77ee8ccefff50c060878aca3a9eb8b
Valentine Wallace <vwallace@protonmail.com>
2023-12-18 14:27:12 -0500

    Make BlindedFailure enum pub.
    Necessary to include it in the public PendingHTLCInfo struct in the next commit.

--- lightning/src/ln/channelmanager.rs
+++ lightning/src/ln/channelmanager.rs
@@ -305,10 +305,15 @@ pub(super) enum HTLCForwardInfo {
 	},
 }
 
-// Used for failing blinded HTLCs backwards correctly.
+/// Whether this blinded HTLC is being failed backwards by the introduction node or a blinded node,
+/// which determines the failure message that should be used.
 #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
-enum BlindedFailure {
+pub enum BlindedFailure {
+	/// This HTLC is being failed backwards by the introduction node, and thus should be failed with
+	/// [`msgs::UpdateFailHTLC`] and error code `0x8000|0x4000|24`.
 	FromIntroductionNode,
+	/// This HTLC is being failed backwards by a blinded node within the path, and thus should be
+	/// failed with [`msgs::UpdateFailMalformedHTLC`] and error code `0x8000|0x4000|24`.
 	FromBlindedNode,
 }
 
