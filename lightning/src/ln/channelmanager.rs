aae39b4090f6b4a62e2deea874be59d9f4c3e217
Valentine Wallace <vwallace@protonmail.com>
2024-01-11 13:47:17 -0500

    Advertise route blinding feature as supported.
    Now that we fully support forwarding blinded payments, we should advertise
    support so nodes on the network can include us in their blinded paths.

--- lightning/src/ln/channelmanager.rs
+++ lightning/src/ln/channelmanager.rs
@@ -9357,6 +9357,7 @@ pub fn provided_init_features(config: &UserConfig) -> InitFeatures {
 	features.set_channel_type_optional();
 	features.set_scid_privacy_optional();
 	features.set_zero_conf_optional();
+	features.set_route_blinding_optional();
 	if config.channel_handshake_config.negotiate_anchors_zero_fee_htlc_tx {
 		features.set_anchors_zero_fee_htlc_tx_optional();
 	}
