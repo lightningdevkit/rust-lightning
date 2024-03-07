edb22e1ab9818c4f9efe85c7752c6048589489c9
Elias Rohrer <dev@tnull.de>
2024-03-07 17:35:38 +0100

    Implement `Hash` for `BestBlock`

--- lightning/src/chain/mod.rs
+++ lightning/src/chain/mod.rs
@@ -31,7 +31,7 @@ pub(crate) mod onchaintx;
 pub(crate) mod package;
 
 /// The best known block as identified by its hash and height.
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
 pub struct BestBlock {
 	/// The block's hash
 	pub block_hash: BlockHash,
