91592db5d3bcebcdcc529656feb14428af23158a
Elias Rohrer <dev@tnull.de>
2024-03-27 11:50:07 +0100

    Move low-R comments into blocks

--- lightning/src/sign/mod.rs
+++ lightning/src/sign/mod.rs
@@ -369,7 +369,9 @@ impl SpendableOutputDescriptor {
 					});
 					witness_weight += descriptor.max_witness_length();
 					#[cfg(feature = "grind_signatures")]
-					{ witness_weight -= 1; } // Guarantees a low R signature
+					{ // Guarantees a low R signature
+						witness_weight -= 1;
+					}
 					input_value += descriptor.output.value;
 				},
 				SpendableOutputDescriptor::DelayedPaymentOutput(descriptor) => {
@@ -382,7 +384,9 @@ impl SpendableOutputDescriptor {
 					});
 					witness_weight += DelayedPaymentOutputDescriptor::MAX_WITNESS_LENGTH;
 					#[cfg(feature = "grind_signatures")]
-					{ witness_weight -= 1; } // Guarantees a low R signature
+					{ // Guarantees a low R signature
+						witness_weight -= 1;
+					}
 					input_value += descriptor.output.value;
 				},
 				SpendableOutputDescriptor::StaticOutput { ref outpoint, ref output, .. } => {
@@ -395,7 +399,9 @@ impl SpendableOutputDescriptor {
 					});
 					witness_weight += 1 + 73 + 34;
 					#[cfg(feature = "grind_signatures")]
-					{ witness_weight -= 1; } // Guarantees a low R signature
+					{ // Guarantees a low R signature
+						witness_weight -= 1;
+					}
 					input_value += output.value;
 				}
 			}
