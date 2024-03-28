d6770d4a74348c91c91dfe734a1c38b7fa26d81e
Matt Corallo <git@bluematt.me>
2024-03-28 22:02:09 +0000

    Ensure we read the full TLV stream length when maybe-reading `None`
    If we are reading an object that is `MaybeReadable` in a TLV stream
    using `upgradable_required`, it may return early with `Ok(None)`.
    In this case, it will not read any further TLVs from the TLV
    stream. This is fine, except that we generally expect
    `MaybeReadable` always consume the correct number of bytes for the
    full object, even if it doesn't understand it.
    
    This could pose a problem, for example, in cases where we're
    reading a TLV-stream `MaybeReadable` object inside another
    TLV-stream object. In that case, the `MaybeReadable` object may
    return `Ok(None)` and not consume all the available bytes, causing
    the outer TLV read to fail as the TLV length does not match.

--- lightning/src/lib.rs
+++ lightning/src/lib.rs
@@ -94,7 +94,9 @@ pub use std::io;
 pub use core2::io;
 
 #[cfg(not(feature = "std"))]
-mod io_extras {
+#[doc(hidden)]
+/// IO utilities public only for use by in-crate macros. These should not be used externally
+pub mod io_extras {
 	use core2::io::{self, Read, Write};
 
 	/// A writer which will move data into the void.
@@ -154,6 +156,8 @@ mod io_extras {
 }
 
 #[cfg(feature = "std")]
+#[doc(hidden)]
+/// IO utilities public only for use by in-crate macros. These should not be used externally
 mod io_extras {
 	pub fn read_to_end<D: ::std::io::Read>(mut d: D) -> Result<Vec<u8>, ::std::io::Error> {
 		let mut buf = Vec::new();
