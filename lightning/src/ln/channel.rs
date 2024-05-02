8e4879f8acb131750af2c66a344d323ceb9836fc
Duncan Dean <git@dunxen.dev>
2024-05-02 10:05:43 +0200

    Handle re-establishment next_funding_txid

--- lightning/src/ln/channel.rs
+++ lightning/src/ln/channel.rs
@@ -1450,6 +1450,10 @@ pub(super) struct ChannelContext<SP: Deref> where SP::Target: SignerProvider {
 	/// If we can't release a [`ChannelMonitorUpdate`] until some external action completes, we
 	/// store it here and only release it to the `ChannelManager` once it asks for it.
 	blocked_monitor_updates: Vec<PendingChannelMonitorUpdate>,
+	// If we've sent `commtiment_signed` for an interactive transaction construction,
+	// but have not received `tx_signatures` we MUST set `next_funding_txid` to the
+	// txid of that interactive transaction, else we MUST NOT set it.
+	next_funding_txid: Option<Txid>,
 }
 
 #[cfg(any(dual_funding, splicing))]
@@ -2000,6 +2004,7 @@ impl<SP: Deref> ChannelContext<SP> where SP::Target: SignerProvider  {
 			local_initiated_shutdown: None,
 
 			blocked_monitor_updates: Vec::new(),
+			next_funding_txid: None,
 		};
 
 		Ok(channel_context)
@@ -2223,6 +2228,7 @@ impl<SP: Deref> ChannelContext<SP> where SP::Target: SignerProvider  {
 
 			blocked_monitor_updates: Vec::new(),
 			local_initiated_shutdown: None,
+			next_funding_txid: None,
 		})
 	}
 
@@ -4398,6 +4404,16 @@ impl<SP: Deref> Channel<SP> where
 	pub fn set_batch_ready(&mut self) {
 		self.context.is_batch_funding = None;
 		self.context.channel_state.clear_waiting_for_batch();
+	}
+
+	#[cfg(any(dual_funding, splicing))]
+	pub fn set_next_funding_txid(&mut self, txid: &Txid) {
+		self.context.next_funding_txid = Some(*txid);
+	}
+
+	#[cfg(any(dual_funding, splicing))]
+	pub fn clear_next_funding_txid(&mut self) {
+		self.context.next_funding_txid = None;
 	}
 
 	/// Unsets the existing funding information.
@@ -7440,10 +7456,7 @@ impl<SP: Deref> Channel<SP> where
 			next_remote_commitment_number: INITIAL_COMMITMENT_NUMBER - self.context.cur_counterparty_commitment_transaction_number - 1,
 			your_last_per_commitment_secret: remote_last_secret,
 			my_current_per_commitment_point: dummy_pubkey,
-			// TODO(dual_funding): If we've sent `commtiment_signed` for an interactive transaction
-			// construction but have not received `tx_signatures` we MUST set `next_funding_txid` to the
-			// txid of that interactive transaction, else we MUST NOT set it.
-			next_funding_txid: None,
+			next_funding_txid: self.context.next_funding_txid,
 		}
 	}
 
@@ -9425,6 +9438,7 @@ impl<SP: Deref> Writeable for Channel<SP> where SP::Target: SignerProvider {
 			(45, cur_holder_commitment_point, option),
 			(47, next_holder_commitment_point, option),
 			(49, self.context.local_initiated_shutdown, option), // Added in 0.0.122
+			(51, self.context.next_funding_txid, option), // Added in 0.0.124
 		});
 
 		Ok(())
@@ -10022,6 +10036,10 @@ impl<'a, 'b, 'c, ES: Deref, SP: Deref> ReadableArgs<(&'a ES, &'b SP, u32, &'c Ch
 				local_initiated_shutdown,
 
 				blocked_monitor_updates: blocked_monitor_updates.unwrap(),
+				// If we've sent `commtiment_signed` for an interactive transaction construction,
+				// but have not received `tx_signatures` we MUST set `next_funding_txid` to the
+				// txid of that interactive transaction, else we MUST NOT set it.
+				next_funding_txid: None,
 			},
 			#[cfg(any(dual_funding, splicing))]
 			dual_funding_channel_context: None,
--- lightning/src/ln/channelmanager.rs
+++ lightning/src/ln/channelmanager.rs
@@ -7791,6 +7791,7 @@ where
 					peer_state.pending_msg_events.push(msg_send_event);
 				}
 				if let Some(signing_session) = signing_session_opt {
+					let funding_txid = signing_session.unsigned_tx.txid();
 					let (channel_id, channel_phase) = chan_phase_entry.remove_entry();
 					let res = match channel_phase {
 						ChannelPhase::UnfundedOutboundV2(chan) => {
@@ -7812,7 +7813,7 @@ where
 							.into()))),
 					};
 					match res {
-						Ok((channel, commitment_signed, funding_ready_for_sig_event_opt)) => {
+						Ok((mut channel, commitment_signed, funding_ready_for_sig_event_opt)) => {
 							if let Some(funding_ready_for_sig_event) = funding_ready_for_sig_event_opt {
 								let mut pending_events = self.pending_events.lock().unwrap();
 								pending_events.push_back((funding_ready_for_sig_event, None));
@@ -7828,6 +7829,7 @@ where
 									update_fee: None,
 								},
 							});
+							channel.set_next_funding_txid(&funding_txid);
 							peer_state.channel_by_id.insert(channel_id.clone(), ChannelPhase::Funded(channel));
 						},
 						Err((channel_phase, err)) => {
@@ -7863,6 +7865,7 @@ where
 				match channel_phase {
 					ChannelPhase::Funded(chan) => {
 						let (tx_signatures_opt, funding_tx_opt) = try_chan_phase_entry!(self, chan.tx_signatures(&msg), chan_phase_entry);
+						chan.clear_next_funding_txid();
 						if let Some(tx_signatures) = tx_signatures_opt {
 							peer_state.pending_msg_events.push(events::MessageSendEvent::SendTxSignatures {
 								node_id: *counterparty_node_id,
