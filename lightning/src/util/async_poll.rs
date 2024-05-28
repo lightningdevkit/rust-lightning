c1ea761e70aaa33c9b07d2468160488a0f4e05cc
Matt Corallo <git@bluematt.me>
2024-05-28 15:21:38 +0000

    f docs

--- lightning/src/util/async_poll.rs
+++ lightning/src/util/async_poll.rs
@@ -1,4 +1,13 @@
-//! Somse utilities to make working with std Futures easier
+// This file is Copyright its original authors, visible in version control
+// history.
+//
+// This file is licensed under the Apache License, Version 2.0 <LICENSE-APACHE
+// or http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
+// You may not use this file except in accordance with one or both of these
+// licenses.
+
+//! Some utilities to make working with the standard library's [`Future`]s easier
 
 use crate::prelude::*;
 use core::future::Future;
