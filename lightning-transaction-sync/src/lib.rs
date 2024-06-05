9cf08260efb26192063dfc3d465975a48e2d2052
Matt Corallo <git@bluematt.me>
2024-06-05 14:41:03 +0000

    Minimal updates to `lightning-transaction-sync` for bindings
    Bindings don't accept dyn traits, but instead map any traits to a
    single dynamic struct. Thus, we can always take a specific trait
    to accept any implementation, which we do here.

--- lightning-transaction-sync/src/lib.rs
+++ lightning-transaction-sync/src/lib.rs
@@ -72,15 +72,15 @@
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
 
 #[cfg(any(feature = "esplora-blocking", feature = "esplora-async"))]
-mod esplora;
+pub mod esplora;
 
 #[cfg(any(feature = "electrum"))]
-mod electrum;
+pub mod electrum;
 
 #[cfg(any(feature = "esplora-blocking", feature = "esplora-async", feature = "electrum"))]
 mod common;
 #[cfg(any(feature = "esplora-blocking", feature = "esplora-async", feature = "electrum"))]
-mod error;
+pub mod error;
 #[cfg(any(feature = "esplora-blocking", feature = "esplora-async", feature = "electrum"))]
 pub use error::TxSyncError;
 
