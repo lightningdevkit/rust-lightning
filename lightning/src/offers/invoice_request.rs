b2c3ddb1527976ba5639bdc344043aad058a256f
Ian Slane <slaneian@gmail.com>
2024-07-18 13:05:13 -0600

    Refactor amount validation and relocate currency check
    Update `InvoiceRequest` parsing to handle currency amounts in
    offers. The currency check has been moved from
    `OfferContents::check_amount_msats_for_quantity` to the
    `InvoiceRequest` parsing code. This makes sure that an error is
    returned early if an offer specifies an unsupported currency,
    preventing issues with parsing a `Bolt12Invoice`.
    
    This change also simplifies
    `OfferContents::check_amount_msats_for_quantity` by removing
    redundant currency validation, allowing it to trust the
    sender-provided amount.

--- lightning/src/offers/invoice_request.rs
+++ lightning/src/offers/invoice_request.rs
@@ -71,7 +71,7 @@ use crate::ln::inbound_payment::{ExpandedKey, IV_LEN};
 use crate::ln::msgs::DecodeError;
 use crate::offers::merkle::{SignError, SignFn, SignatureTlvStream, SignatureTlvStreamRef, TaggedHash, self};
 use crate::offers::nonce::Nonce;
-use crate::offers::offer::{Offer, OfferContents, OfferId, OfferTlvStream, OfferTlvStreamRef};
+use crate::offers::offer::{Amount, Offer, OfferContents, OfferId, OfferTlvStream, OfferTlvStreamRef};
 use crate::offers::parse::{Bolt12ParseError, ParsedMessage, Bolt12SemanticError};
 use crate::offers::payer::{PayerContents, PayerTlvStream, PayerTlvStreamRef};
 use crate::offers::signer::{Metadata, MetadataMaterial};
@@ -1163,6 +1163,14 @@ impl TryFrom<PartialInvoiceRequestTlvStream> for InvoiceRequestContents {
 		};
 		let offer = OfferContents::try_from(offer_tlv_stream)?;
 
+		match offer.amount() {
+			Some(Amount::Currency {..}) => return Err(Bolt12SemanticError::UnsupportedCurrency),
+			_ => {
+				offer.check_amount_msats_for_quantity(amount, quantity)?;
+				offer.check_quantity(quantity)?;
+			},
+		};
+
 		if !offer.supports_chain(chain.unwrap_or_else(|| offer.implied_chain())) {
 			return Err(Bolt12SemanticError::UnsupportedChain);
 		}
@@ -1170,9 +1178,6 @@ impl TryFrom<PartialInvoiceRequestTlvStream> for InvoiceRequestContents {
 		if offer.amount().is_none() && amount.is_none() {
 			return Err(Bolt12SemanticError::MissingAmount);
 		}
-
-		offer.check_quantity(quantity)?;
-		offer.check_amount_msats_for_quantity(amount, quantity)?;
 
 		let features = features.unwrap_or_else(InvoiceRequestFeatures::empty);
 
