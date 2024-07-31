dae4057fa7af9c80fe45074a1d8cbeb4aec834e9
Ian Slane <slaneian@gmail.com>
2024-07-31 07:13:50 -0600

    f tests for handling Offers with fiat amount
    Refactor tests to ensure that an InvoiceRequest can be created
    successfully when both a fiat amount and amount_msat are
    provided.

--- lightning/src/offers/invoice_request.rs
+++ lightning/src/offers/invoice_request.rs
@@ -1739,15 +1739,17 @@ mod tests {
 			.sign(payer_sign).unwrap();
 		assert_eq!(invoice_request.amount(), Some(Amount::Currency {iso4217_code: *b"USD", amount: 1000}));
 
-		match OfferBuilder::new(recipient_pubkey())
+		let invoice_request = OfferBuilder::new(recipient_pubkey())
 			.amount(Amount::Currency {iso4217_code: *b"USD", amount: 100})
 			.build_unchecked()
-			.request_invoice(vec![1; 32], payer_pubkey())
-			.unwrap().amount_msats(150_000_000)
-		{
-			Ok(_) => panic!("expected error"),
-			Err(e) => assert_eq!(e, Bolt12SemanticError::UnsupportedCurrency),
-		}
+			.request_invoice(vec![1; 32], payer_pubkey()).unwrap()
+			.amount_msats(150_000_000)
+			.unwrap()
+			.build().unwrap()
+			.sign(payer_sign).unwrap();
+		let (_, _, tlv_stream, _) = invoice_request.as_tlv_stream();
+		assert_eq!(invoice_request.amount_msats(), Some(150_000_000));
+		assert_eq!(tlv_stream.amount, Some(150_000_000));
 	}
 
 	#[test]
