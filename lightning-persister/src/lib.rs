f87eaccae88c7033d1b228284c37cbad3cd42819
Elias Rohrer <dev@tnull.de>
2024-08-02 16:31:13 -0500

    `rustfmt`: Run on `lightning-persister`

--- lightning-persister/src/lib.rs
+++ lightning-persister/src/lib.rs
@@ -2,12 +2,11 @@
 
 #![deny(rustdoc::broken_intra_doc_links)]
 #![deny(rustdoc::private_intra_doc_links)]
-
 #![deny(missing_docs)]
-
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
 
-#[cfg(ldk_bench)] extern crate criterion;
+#[cfg(ldk_bench)]
+extern crate criterion;
 
 pub mod fs_store;
 
