c8279fe3dedcb7832136bdd2f77fb752aa71e3a9
optout <13562139+optout21@users.noreply.github.com>
2024-08-02 07:25:57 +0200

    Remove copyright

--- lightning/src/util/bech32.rs
+++ lightning/src/util/bech32.rs
@@ -1,12 +1,3 @@
-// This file is Copyright its original authors, visible in version control
-// history.
-//
-// This file is licensed under the Apache License, Version 2.0 <LICENSE-APACHE
-// or http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
-// You may not use this file except in accordance with one or both of these
-// licenses.
-
 //! Bech32 encoding/decoding for lightning invoices.
 
 use core::fmt;
