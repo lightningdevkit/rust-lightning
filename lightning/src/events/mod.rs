bb94320e4095b23e4bdc025ade7a59d181f47a90
Jeffrey Czyz <jkczyz@gmail.com>
2024-08-07 17:16:54 -0500

    Make PaymentFailureReason upgradable
    This allows downgrading to version 0.0.124 or later and using None for a
    PaymentFailureReason that was added after.

--- lightning/src/events/mod.rs
+++ lightning/src/events/mod.rs
@@ -538,7 +538,7 @@ pub enum PaymentFailureReason {
 	InvoiceRequestRejected,
 }
 
-impl_writeable_tlv_based_enum!(PaymentFailureReason,
+impl_writeable_tlv_based_enum_upgradable!(PaymentFailureReason,
 	(0, RecipientRejected) => {},
 	(1, UnknownRequiredFeatures) => {},
 	(2, UserAbandoned) => {},
@@ -878,7 +878,8 @@ pub enum Event {
 		/// [`Offer`]: crate::offers::offer::Offer
 		payment_hash: Option<PaymentHash>,
 		/// The reason the payment failed. This is only `None` for events generated or serialized
-		/// by versions prior to 0.0.115.
+		/// by versions prior to 0.0.115, or when downgrading to 0.0.124 or later with a reason that
+		/// was added after.
 		reason: Option<PaymentFailureReason>,
 	},
 	/// Indicates that a path for an outbound payment was successful.
@@ -1939,7 +1940,7 @@ impl MaybeReadable for Event {
 					Ok(Some(Event::PaymentFailed {
 						payment_id,
 						payment_hash,
-						reason,
+						reason: _init_tlv_based_struct_field!(reason, upgradable_option),
 					}))
 				};
 				f()
