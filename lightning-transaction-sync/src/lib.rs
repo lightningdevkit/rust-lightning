c15968c5f4969991244b0462048ca8017362209c
Elias Rohrer <dev@tnull.de>
2024-09-04 13:02:45 +0200

    Add copyright notices to `lightning-transaction-sync` modules

--- lightning-transaction-sync/src/lib.rs
+++ lightning-transaction-sync/src/lib.rs
@@ -1,3 +1,10 @@
+// This file is Copyright its original authors, visible in version control history.
+//
+// This file is licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. You may not use this file except in
+// accordance with one or both of these licenses.
+
 //! Provides utilities for syncing LDK via the transaction-based [`Confirm`] interface.
 //!
 //! The provided synchronization clients need to be registered with a [`ChainMonitor`] via the
