802f5a65b46024902550d57e0dafbf1c0cfd0d76
Elias Rohrer <dev@tnull.de>
2024-09-16 12:29:35 +0200

    `rustfmt`: Run on `crypto/mod.rs`

--- lightning/src/crypto/mod.rs
+++ lightning/src/crypto/mod.rs
@@ -2,8 +2,8 @@
 use bitcoin::hashes::cmp::fixed_time_eq;
 
 pub(crate) mod chacha20;
+pub(crate) mod chacha20poly1305rfc;
 #[cfg(not(fuzzing))]
 pub(crate) mod poly1305;
-pub(crate) mod chacha20poly1305rfc;
 pub(crate) mod streams;
 pub(crate) mod utils;
