50356381c5a95949dd9851f441a8ee8018e82351
Elias Rohrer <dev@tnull.de>
2024-09-18 09:42:27 +0200

    `rustfmt`: Run on `util/mod.rs`

--- lightning/src/util/mod.rs
+++ lightning/src/util/mod.rs
@@ -15,23 +15,23 @@ pub(crate) mod fuzz_wrappers;
 #[macro_use]
 pub mod ser_macros;
 
-pub mod errors;
-pub mod ser;
-pub mod message_signing;
-pub mod persist;
-pub mod scid_utils;
-pub mod sweep;
-pub mod wakers;
 #[cfg(fuzzing)]
 pub mod base32;
 #[cfg(not(fuzzing))]
 pub(crate) mod base32;
+pub mod errors;
+pub mod message_signing;
+pub mod persist;
+pub mod scid_utils;
+pub mod ser;
+pub mod sweep;
+pub mod wakers;
 
-pub(crate) mod atomic_counter;
 pub(crate) mod async_poll;
+pub(crate) mod atomic_counter;
 pub(crate) mod byte_utils;
-pub(crate) mod transaction_utils;
 pub mod hash_tables;
+pub(crate) mod transaction_utils;
 
 #[cfg(feature = "std")]
 pub(crate) mod time;
@@ -43,8 +43,8 @@ pub mod indexed_map;
 pub(crate) mod macro_logger;
 
 // These have to come after macro_logger to build
-pub mod logger;
 pub mod config;
+pub mod logger;
 
 #[cfg(any(test, feature = "_test_utils"))]
 pub mod test_utils;
