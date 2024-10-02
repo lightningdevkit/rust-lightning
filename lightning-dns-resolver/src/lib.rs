ae5d5841cc9d6046ae175e33d621e5d9d8daff4d
Matt Corallo <git@bluematt.me>
2024-10-02 18:23:31 +0000

    Set the `dns_resolution` feature in `OMDomainResolver`
    `OMDomainResolver` actually does support building DNSSECProofs, so
    should be setting the `dns_resolution` `NodeFeature` flag.

--- lightning-dns-resolver/src/lib.rs
+++ lightning-dns-resolver/src/lib.rs
@@ -21,6 +21,8 @@ use lightning::onion_message::messenger::{
 	MessageSendInstructions, Responder, ResponseInstruction,
 };
 
+use lightning_types::features::NodeFeatures;
+
 #[cfg(not(any(target_pointer_width = "32", target_pointer_width = "64")))]
 const WE_REQUIRE_32_OR_64_BIT_USIZE: u8 = 424242;
 
@@ -105,6 +107,12 @@ where
 			}
 		});
 		None
+	}
+
+	fn provided_node_features(&self) -> NodeFeatures {
+		let mut features = NodeFeatures::empty();
+		features.set_dns_resolution_optional();
+		features
 	}
 
 	fn release_pending_messages(&self) -> Vec<(DNSResolverMessage, MessageSendInstructions)> {
