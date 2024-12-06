d80f237a106ccec168e114d98dccc6afff7310fd
optout <13562139+optout21@users.noreply.github.com>
2024-12-06 16:42:52 +0100

    Add begin_interactive_funding_tx_construction()

--- lightning/src/ln/interactivetxs.rs
+++ lightning/src/ln/interactivetxs.rs
@@ -1152,13 +1152,13 @@ pub(crate) enum InteractiveTxInput {
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub struct SharedOwnedOutput {
+pub(super) struct SharedOwnedOutput {
 	tx_out: TxOut,
 	local_owned: u64,
 }
 
 impl SharedOwnedOutput {
-	fn new(tx_out: TxOut, local_owned: u64) -> SharedOwnedOutput {
+	pub fn new(tx_out: TxOut, local_owned: u64) -> SharedOwnedOutput {
 		debug_assert!(
 			local_owned <= tx_out.value.to_sat(),
 			"SharedOwnedOutput: Inconsistent local_owned value {}, larger than output value {}",
@@ -1177,7 +1177,7 @@ impl SharedOwnedOutput {
 /// its control -- exclusive by the adder or shared --, and
 /// its ownership -- value fully owned by the adder or jointly
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub enum OutputOwned {
+pub(super) enum OutputOwned {
 	/// Belongs to a single party -- controlled exclusively and fully belonging to a single party
 	Single(TxOut),
 	/// Output with shared control, but fully belonging to local node
@@ -1187,7 +1187,7 @@ pub enum OutputOwned {
 }
 
 impl OutputOwned {
-	fn tx_out(&self) -> &TxOut {
+	pub fn tx_out(&self) -> &TxOut {
 		match self {
 			OutputOwned::Single(tx_out) | OutputOwned::SharedControlFullyOwned(tx_out) => tx_out,
 			OutputOwned::Shared(output) => &output.tx_out,
@@ -1663,14 +1663,57 @@ impl InteractiveTxConstructor {
 	}
 }
 
+/// Determine whether a change output should be added or not, and if so, of what size,
+/// considering our given inputs, outputs, and intended contribution.
+/// Computes and takes into account fees.
+/// Return value is the value computed for the change output (in satoshis),
+/// or None if a change is not needed/possible.
+#[allow(dead_code)] // TODO(dual_funding): Remove once begin_interactive_funding_tx_construction() is used
+pub(super) fn calculate_change_output_value(
+	is_initiator: bool, our_contribution: u64, funding_inputs_prev_outputs: &Vec<&TxOut>,
+	funding_outputs: &Vec<OutputOwned>, funding_feerate_sat_per_1000_weight: u32,
+	holder_dust_limit_satoshis: u64,
+) -> Option<u64> {
+	let our_funding_inputs_weight =
+		funding_inputs_prev_outputs.iter().fold(0u64, |weight, prev_output| {
+			weight.saturating_add(estimate_input_weight(prev_output).to_wu())
+		});
+	let our_funding_outputs_weight = funding_outputs.iter().fold(0u64, |weight, out| {
+		weight.saturating_add(get_output_weight(&out.tx_out().script_pubkey).to_wu())
+	});
+	let our_contributed_weight =
+		our_funding_outputs_weight.saturating_add(our_funding_inputs_weight);
+	let mut fees_sats = fee_for_weight(funding_feerate_sat_per_1000_weight, our_contributed_weight);
+
+	// If we are the initiator, we must pay for weight of all common fields in the funding transaction.
+	if is_initiator {
+		let common_fees =
+			fee_for_weight(funding_feerate_sat_per_1000_weight, TX_COMMON_FIELDS_WEIGHT);
+		fees_sats = fees_sats.saturating_add(common_fees);
+	}
+
+	let total_input_satoshis: u64 =
+		funding_inputs_prev_outputs.iter().map(|out| out.value.to_sat()).sum();
+
+	let remaining_value =
+		total_input_satoshis.saturating_sub(our_contribution).saturating_sub(fees_sats);
+
+	if remaining_value <= holder_dust_limit_satoshis {
+		None
+	} else {
+		Some(remaining_value)
+	}
+}
+
 #[cfg(test)]
 mod tests {
 	use crate::chain::chaininterface::{fee_for_weight, FEERATE_FLOOR_SATS_PER_KW};
 	use crate::ln::channel::TOTAL_BITCOIN_SUPPLY_SATOSHIS;
 	use crate::ln::interactivetxs::{
-		generate_holder_serial_id, AbortReason, HandleTxCompleteValue, InteractiveTxConstructor,
-		InteractiveTxConstructorArgs, InteractiveTxMessageSend, MAX_INPUTS_OUTPUTS_COUNT,
-		MAX_RECEIVED_TX_ADD_INPUT_COUNT, MAX_RECEIVED_TX_ADD_OUTPUT_COUNT,
+		calculate_change_output_value, generate_holder_serial_id, AbortReason,
+		HandleTxCompleteValue, InteractiveTxConstructor, InteractiveTxConstructorArgs,
+		InteractiveTxMessageSend, MAX_INPUTS_OUTPUTS_COUNT, MAX_RECEIVED_TX_ADD_INPUT_COUNT,
+		MAX_RECEIVED_TX_ADD_OUTPUT_COUNT,
 	};
 	use crate::ln::types::ChannelId;
 	use crate::sign::EntropySource;
@@ -2594,5 +2637,149 @@ mod tests {
 		// Initiators should have even serial id, non-initiators should have odd serial id.
 		assert_eq!(generate_holder_serial_id(&&entropy_source, true) % 2, 0);
 		assert_eq!(generate_holder_serial_id(&&entropy_source, false) % 2, 1)
+	}
+
+	#[test]
+	fn test_calculate_change_output_value_open() {
+		let input_prevouts_owned = vec![
+			TxOut { value: Amount::from_sat(70_000), script_pubkey: ScriptBuf::new() },
+			TxOut { value: Amount::from_sat(60_000), script_pubkey: ScriptBuf::new() },
+		];
+		let input_prevouts: Vec<&TxOut> = input_prevouts_owned.iter().collect();
+		let our_contributed = 110_000;
+		let txout = TxOut { value: Amount::from_sat(128_000), script_pubkey: ScriptBuf::new() };
+		let outputs = vec![OutputOwned::SharedControlFullyOwned(txout)];
+		let funding_feerate_sat_per_1000_weight = 3000;
+
+		let total_inputs: u64 = input_prevouts.iter().map(|o| o.value.to_sat()).sum();
+		let gross_change = total_inputs - our_contributed;
+		let fees = 1746;
+		let common_fees = 126;
+		{
+			// There is leftover for change
+			let res = calculate_change_output_value(
+				true,
+				our_contributed,
+				&input_prevouts,
+				&outputs,
+				funding_feerate_sat_per_1000_weight,
+				300,
+			);
+			assert_eq!(res.unwrap(), gross_change - fees - common_fees);
+		}
+		{
+			// There is leftover for change, without common fees
+			let res = calculate_change_output_value(
+				false,
+				our_contributed,
+				&input_prevouts,
+				&outputs,
+				funding_feerate_sat_per_1000_weight,
+				300,
+			);
+			assert_eq!(res.unwrap(), gross_change - fees);
+		}
+		{
+			// Larger fee, smaller change
+			let res = calculate_change_output_value(
+				true,
+				our_contributed,
+				&input_prevouts,
+				&outputs,
+				9000,
+				300,
+			);
+			assert_eq!(res.unwrap(), 14384);
+		}
+		{
+			// Insufficient inputs, no leftover
+			let res = calculate_change_output_value(
+				false,
+				130_000,
+				&input_prevouts,
+				&outputs,
+				funding_feerate_sat_per_1000_weight,
+				300,
+			);
+			assert!(res.is_none());
+		}
+		{
+			// Very small leftover
+			let res = calculate_change_output_value(
+				false,
+				128_100,
+				&input_prevouts,
+				&outputs,
+				funding_feerate_sat_per_1000_weight,
+				300,
+			);
+			assert!(res.is_none());
+		}
+		{
+			// Small leftover, but not dust
+			let res = calculate_change_output_value(
+				false,
+				128_100,
+				&input_prevouts,
+				&outputs,
+				funding_feerate_sat_per_1000_weight,
+				100,
+			);
+			assert_eq!(res.unwrap(), 154);
+		}
+	}
+
+	#[test]
+	fn test_calculate_change_output_value_splice() {
+		let input_prevouts_owned = vec![
+			TxOut { value: Amount::from_sat(70_000), script_pubkey: ScriptBuf::new() },
+			TxOut { value: Amount::from_sat(60_000), script_pubkey: ScriptBuf::new() },
+		];
+		let input_prevouts: Vec<&TxOut> = input_prevouts_owned.iter().collect();
+		let our_contributed = 110_000;
+		let txout = TxOut { value: Amount::from_sat(148_000), script_pubkey: ScriptBuf::new() };
+		let outputs = vec![OutputOwned::Shared(SharedOwnedOutput::new(txout, our_contributed))];
+		let funding_feerate_sat_per_1000_weight = 3000;
+
+		let total_inputs: u64 = input_prevouts.iter().map(|o| o.value.to_sat()).sum();
+		let gross_change = total_inputs - our_contributed;
+		let fees = 1746;
+		let common_fees = 126;
+		{
+			// There is leftover for change
+			let res = calculate_change_output_value(
+				true,
+				our_contributed,
+				&input_prevouts,
+				&outputs,
+				funding_feerate_sat_per_1000_weight,
+				300,
+			);
+			assert_eq!(res.unwrap(), gross_change - fees - common_fees);
+		}
+		{
+			// Very small leftover
+			let res = calculate_change_output_value(
+				false,
+				128_100,
+				&input_prevouts,
+				&outputs,
+				funding_feerate_sat_per_1000_weight,
+				300,
+			);
+			assert!(res.is_none());
+		}
+		{
+			// Small leftover, but not dust
+			let res = calculate_change_output_value(
+				false,
+				128_100,
+				&input_prevouts,
+				&outputs,
+				funding_feerate_sat_per_1000_weight,
+				100,
+			);
+			assert_eq!(res.unwrap(), 154);
+		}
 	}
 }
