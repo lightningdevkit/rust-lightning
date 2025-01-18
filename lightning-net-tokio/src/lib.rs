28d60ec831db73e25ed7e864db8b5979dba364e7
Aditya Sharma <adityasharma@Adityas-MacBook-Pro.local>
2025-01-18 18:17:19 +0530

    Enable ChainMonitor to distribute PeerStorage
    To enable ChainMonitor sending peer storage to channel partners whenever a new block is added,
    We implement BaseMessageHandler for ChainMonitor.
    This allows the `ChainMonitor` to handle the peer storage distribution.
    
    Key changes:
     - Add BaseMessageHandler into the MessageHandler.
     - Implement BaseMessageHandler for ChainMonitor.
     - Process BaseMessageHandler events inside process_events().

--- lightning-net-tokio/src/lib.rs
+++ lightning-net-tokio/src/lib.rs
@@ -832,6 +832,7 @@ mod tests {
 			route_handler: Arc::clone(&a_handler),
 			onion_message_handler: Arc::new(IgnoringMessageHandler {}),
 			custom_message_handler: Arc::new(IgnoringMessageHandler {}),
+			send_only_message_handler: Arc::new(IgnoringMessageHandler {}),
 		};
 		let a_manager = Arc::new(PeerManager::new(
 			a_msg_handler,
@@ -855,6 +856,7 @@ mod tests {
 			route_handler: Arc::clone(&b_handler),
 			onion_message_handler: Arc::new(IgnoringMessageHandler {}),
 			custom_message_handler: Arc::new(IgnoringMessageHandler {}),
+			send_only_message_handler: Arc::new(IgnoringMessageHandler {}),
 		};
 		let b_manager = Arc::new(PeerManager::new(
 			b_msg_handler,
@@ -917,6 +919,7 @@ mod tests {
 			onion_message_handler: Arc::new(IgnoringMessageHandler {}),
 			route_handler: Arc::new(lightning::ln::peer_handler::IgnoringMessageHandler {}),
 			custom_message_handler: Arc::new(IgnoringMessageHandler {}),
+			send_only_message_handler: Arc::new(IgnoringMessageHandler {}),
 		};
 		let a_manager = Arc::new(PeerManager::new(
 			a_msg_handler,
