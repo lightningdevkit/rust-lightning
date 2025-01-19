d5a996ab001d1475251c9adaaccaff284b86c7c4
Aditya Sharma <adityasharma@Adityas-MacBook-Pro.local>
2025-01-19 16:10:04 +0530

    Distribute PeerStorage from ChainMonitor
    Everytime a new block is added we send PeerStorage to all of our channel partner.
    
     - Add our_peer_storage and our_peerstorage_encryption_key to ChainMonitor
     - Write send_peer_storage() and send it to all channel partners whenever a new block is added

--- lightning/src/chain/chainmonitor.rs
+++ lightning/src/chain/chainmonitor.rs
@@ -33,6 +33,7 @@ use crate::chain::channelmonitor::{ChannelMonitor, ChannelMonitorUpdate, Balance
 use crate::chain::transaction::{OutPoint, TransactionData};
 use crate::ln::types::ChannelId;
 use crate::ln::msgs::{self, BaseMessageHandler, Init, MessageSendEvent};
+use crate::ln::our_peer_storage::OurPeerStorage;
 use crate::sign::ecdsa::EcdsaChannelSigner;
 use crate::sign::PeerStorageKey;
 use crate::events::{self, Event, EventHandler, ReplayEvent};
@@ -256,6 +257,8 @@ pub struct ChainMonitor<ChannelSigner: EcdsaChannelSigner, C: Deref, T: Deref, F
 	/// it to give to users (or [`MonitorEvent`]s for `ChannelManager` to process).
 	event_notifier: Notifier,
 	pending_send_only_events: Mutex<Vec<MessageSendEvent>>,
+
+	our_peerstorage_encryption_key: PeerStorageKey,
 }
 
 impl<ChannelSigner: EcdsaChannelSigner, C: Deref, T: Deref, F: Deref, L: Deref, P: Deref> ChainMonitor<ChannelSigner, C, T, F, L, P>
@@ -409,6 +412,7 @@ where C::Target: chain::Filter,
 			highest_chain_height: AtomicUsize::new(0),
 			event_notifier: Notifier::new(),
 			pending_send_only_events: Mutex::new(Vec::new()),
+			our_peerstorage_encryption_key
 		}
 	}
 
@@ -691,7 +695,12 @@ where C::Target: chain::Filter,
 	}
 
 	fn send_peer_storage(&self, their_node_id: PublicKey) {
-		// TODO: Serialize `ChannelMonitor`s inside `our_peer_storage` and update [`OurPeerStorage::block_height`] accordingly.
+		// TODO: Serialize `ChannelMonitor`s inside `our_peer_storage`.
+
+		let our_peer_storage = OurPeerStorage::create_from_data(self.our_peerstorage_encryption_key.clone(), Vec::new());
+		log_debug!(self.logger, "Sending Peer Storage from chainmonitor");
+		self.pending_send_only_events.lock().unwrap().push(MessageSendEvent::SendPeerStorage { node_id: their_node_id,
+			msg: msgs::PeerStorage { data: our_peer_storage.encrypted_data() } })
 	}
 }
 
@@ -735,6 +744,12 @@ where
 			monitor.block_connected(
 				header, txdata, height, &*self.broadcaster, &*self.fee_estimator, &self.logger)
 		});
+
+		// Send peer storage everytime a new block arrives.
+		for node_id in self.all_counterparty_node_ids() {
+			self.send_peer_storage(node_id);
+		}
+
 		// Assume we may have some new events and wake the event processor
 		self.event_notifier.notify();
 	}
@@ -786,6 +801,12 @@ where
 				header, height, &*self.broadcaster, &*self.fee_estimator, &self.logger
 			)
 		});
+
+		// Send peer storage everytime a new block arrives.
+		for node_id in self.all_counterparty_node_ids() {
+			self.send_peer_storage(node_id);
+		}
+
 		// Assume we may have some new events and wake the event processor
 		self.event_notifier.notify();
 	}
