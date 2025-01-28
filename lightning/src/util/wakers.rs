15038d4c2173b95c0f8a07f4359fb5300f194bfb
Elias Rohrer <dev@tnull.de>
2025-01-28 14:01:54 +0100

    Make `Notifier` `pub` to allow it being used outside of `lightning`

--- lightning/src/util/wakers.rs
+++ lightning/src/util/wakers.rs
@@ -30,17 +30,29 @@ use core::pin::Pin;
 use core::task::{Context, Poll};
 
 /// Used to signal to one of many waiters that the condition they're waiting on has happened.
-pub(crate) struct Notifier {
+///
+/// This is usually used by LDK objects such as [`ChannelManager`] or [`PeerManager`] to signal to
+/// the background processor that it should wake up and process pending events.
+///
+/// [`ChannelManager`]: crate::ln::channelmanager::ChannelManager
+/// [`PeerManager`]: crate::ln::peer_handler::PeerManager
+pub struct Notifier {
 	notify_pending: Mutex<(bool, Option<Arc<Mutex<FutureState>>>)>,
 }
 
 impl Notifier {
-	pub(crate) fn new() -> Self {
+	/// Constructs a new notifier.
+	pub fn new() -> Self {
 		Self { notify_pending: Mutex::new((false, None)) }
 	}
 
 	/// Wake waiters, tracking that wake needs to occur even if there are currently no waiters.
-	pub(crate) fn notify(&self) {
+	///
+	/// We deem the notification successful either directly after any callbacks were made, or after
+	/// the user [`poll`]ed a previously-completed future.
+	///
+	/// [`poll`]: core::future::Future::poll
+	pub fn notify(&self) {
 		let mut lock = self.notify_pending.lock().unwrap();
 		if let Some(future_state) = &lock.1 {
 			if complete_future(future_state) {
@@ -52,7 +64,7 @@ impl Notifier {
 	}
 
 	/// Gets a [`Future`] that will get woken up with any waiters
-	pub(crate) fn get_future(&self) -> Future {
+	pub fn get_future(&self) -> Future {
 		let mut lock = self.notify_pending.lock().unwrap();
 		let mut self_idx = 0;
 		if let Some(existing_state) = &lock.1 {
