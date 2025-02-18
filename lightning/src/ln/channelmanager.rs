7fb9ba1154814a62ca4e7e0852f57c547f93cbda
Matt Corallo <git@bluematt.me>
2025-02-18 13:27:32 +0000

    Increase min CLTV delta and delta before CLTV at which chans close
    In the previous commit it was observed that we actually have to run
    the whole "get a transaction confirmed" process from start to
    finish twice to claim an HTLC on an anchor channel. This leaves us
    with only 9 blocks to get each transaction confirmed, which is
    quite aggressive.
    
    Here we double this threshold, force-closing channels which have an
    expiring HTLC 36 blocks before expiry instead of 18. We also
    increase the minimum CLTV expiry delta to 48 to ensure we have at
    least a few blocks after the transactions get confirmed before we
    need to fail the inbound edge of a forwarded HTLC back.
    
    We do not change the default CLTV expiry delta of 72 blocks.

--- lightning/src/ln/channelmanager.rs
+++ lightning/src/ln/channelmanager.rs
@@ -2823,7 +2823,7 @@ pub(crate) const MAX_LOCAL_BREAKDOWN_TIMEOUT: u16 = 2 * 6 * 24 * 7;
 // i.e. the node we forwarded the payment on to should always have enough room to reliably time out
 // the HTLC via a full update_fail_htlc/commitment_signed dance before we hit the
 // CLTV_CLAIM_BUFFER point (we static assert that it's at least 3 blocks more).
-pub const MIN_CLTV_EXPIRY_DELTA: u16 = 6*7;
+pub const MIN_CLTV_EXPIRY_DELTA: u16 = 6*8;
 // This should be long enough to allow a payment path drawn across multiple routing hops with substantial
 // `cltv_expiry_delta`. Indeed, the length of those values is the reaction delay offered to a routing node
 // in case of HTLC on-chain settlement. While appearing less competitive, a node operator could decide to
