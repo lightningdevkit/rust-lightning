6db1c42e307d743408094c0b683bb7778aed806a
Jeffrey Czyz <jkczyz@gmail.com>
2025-03-07 16:12:46 -0600

    Consider funding scopes in get_available_balances
    A FundedChannel may have more than one pending FundingScope during
    splicing, one for the splice attempt and one or more for any RBF
    attempts. When calling get_available_balances, consider all funding
    scopes and take the minimum by next_outbound_htlc_limit_msat. This is
    used both informationally and to determine which channel to use to
    forward an HTLC.
    
    The choice of next_outbound_htlc_limit_msat is somewhat arbitrary but
    matches the field used when determining which channel used to forward an
    HTLC. Any field should do since each field should be adjusted by the
    same amount relative to another FundingScope given the nature of the
    fields (i.e., inbound/outbound capacity, min/max HTLC limit).
    
    Using the minimum was chosen since an order for an HTLC to be sent over
    the channel, it must be possible for each funding scope -- both the
    confirmed one and any pending scopes, one of which may eventually
    confirm.

--- lightning/src/ln/channel.rs
+++ lightning/src/ln/channel.rs
@@ -1245,6 +1245,16 @@ impl<SP: Deref> Channel<SP> where
 		}
 	}
 
+	pub fn pending_funding(&self) -> &[FundingScope] {
+		match &self.phase {
+			ChannelPhase::Undefined => unreachable!(),
+			ChannelPhase::Funded(chan) => &chan.pending_funding,
+			ChannelPhase::UnfundedOutboundV1(_) => &[],
+			ChannelPhase::UnfundedInboundV1(_) => &[],
+			ChannelPhase::UnfundedV2(_) => &[],
+		}
+	}
+
 	pub fn unfunded_context_mut(&mut self) -> Option<&mut UnfundedChannelContext> {
 		match &mut self.phase {
 			ChannelPhase::Undefined => unreachable!(),
@@ -4162,6 +4172,20 @@ impl<SP: Deref> ChannelContext<SP> where SP::Target: SignerProvider {
 	/// if-we-removed-it-already-but-haven't-fully-resolved-they-can-still-send-an-inbound-HTLC
 	/// corner case properly.
 	pub fn get_available_balances<F: Deref>(
+		&self, funding: &FundingScope, pending_funding: &[FundingScope],
+		fee_estimator: &LowerBoundedFeeEstimator<F>,
+	) -> AvailableBalances
+	where
+		F::Target: FeeEstimator,
+	{
+		core::iter::once(funding)
+			.chain(pending_funding.iter())
+			.map(|funding| self.get_available_balances_for_scope(funding, fee_estimator))
+			.min_by_key(|balances| balances.next_outbound_htlc_limit_msat)
+			.expect("At least one FundingScope is always provided")
+	}
+
+	fn get_available_balances_for_scope<F: Deref>(
 		&self, funding: &FundingScope, fee_estimator: &LowerBoundedFeeEstimator<F>,
 	) -> AvailableBalances
 	where
@@ -4951,7 +4975,7 @@ pub(super) struct DualFundingChannelContext {
 // Counterparty designates channel data owned by the another channel participant entity.
 pub(super) struct FundedChannel<SP: Deref> where SP::Target: SignerProvider {
 	pub funding: FundingScope,
-	pending_funding: Vec<FundingScope>,
+	pub(super) pending_funding: Vec<FundingScope>,
 	commitment_signed_batch: BTreeMap<Txid, msgs::CommitmentSigned>,
 	pub context: ChannelContext<SP>,
 	pub interactive_tx_signing_session: Option<InteractiveTxSigningSession>,
@@ -8751,7 +8775,7 @@ impl<SP: Deref> FundedChannel<SP> where
 			return Err(ChannelError::Ignore("Cannot send 0-msat HTLC".to_owned()));
 		}
 
-		let available_balances = self.context.get_available_balances(&self.funding, fee_estimator);
+		let available_balances = self.get_available_balances(fee_estimator);
 		if amount_msat < available_balances.next_outbound_htlc_minimum_msat {
 			return Err(ChannelError::Ignore(format!("Cannot send less than our next-HTLC minimum - {} msat",
 				available_balances.next_outbound_htlc_minimum_msat)));
@@ -8821,6 +8845,15 @@ impl<SP: Deref> FundedChannel<SP> where
 		self.context.next_holder_htlc_id += 1;
 
 		Ok(Some(res))
+	}
+
+	pub fn get_available_balances<F: Deref>(
+		&self, fee_estimator: &LowerBoundedFeeEstimator<F>,
+	) -> AvailableBalances
+	where
+		F::Target: FeeEstimator,
+	{
+		self.context.get_available_balances(&self.funding, &self.pending_funding, fee_estimator)
 	}
 
 	fn build_commitment_no_status_check<L: Deref>(&mut self, logger: &L) -> ChannelMonitorUpdate where L::Target: Logger {
