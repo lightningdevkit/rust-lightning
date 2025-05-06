1704b53220ba959ba4004dd4de5dbaab7d5260d6
Jeffrey Czyz <jkczyz@gmail.com>
2025-05-06 15:37:36 -0500

    Add funding_txo to ChannelReady event
    A ChannelReady event is used for both channel establishment and splicing
    to indicate that the funding transaction is confirmed to an acceptable
    depth and thus the channel can be used with the funding. An upcoming
    SplicePending event will be emitted for each pending splice (i.e., both
    the initial splice attempt and any RBF attempts). Thus, when a
    ChannelReady event is emitted, the funding_txo must be included to
    differentiate between which ChannelPending -- which also contains the
    funding_txo -- that the event corresponds to.

--- lightning/src/ln/channelmanager.rs
+++ lightning/src/ln/channelmanager.rs
@@ -3350,6 +3350,7 @@ macro_rules! emit_initial_channel_ready_event {
 				channel_id: $channel.context.channel_id(),
 				user_channel_id: $channel.context.get_user_id(),
 				counterparty_node_id: $channel.context.get_counterparty_node_id(),
+				funding_txo: $channel.funding.get_funding_txo().map(|outpoint| outpoint.into_bitcoin_outpoint()),
 				channel_type: $channel.funding.get_channel_type().clone(),
 			}, None));
 			$channel.context.set_initial_channel_ready_event_emitted();
@@ -10057,6 +10058,7 @@ This indicates a bug inside LDK. Please report this error at https://github.com/
 							channel_id: chan.context.channel_id(),
 							user_channel_id: chan.context.get_user_id(),
 							counterparty_node_id: chan.context.get_counterparty_node_id(),
+							funding_txo: chan.funding.get_funding_txo().map(|outpoint| outpoint.into_bitcoin_outpoint()),
 							channel_type: chan.funding.get_channel_type().clone(),
 						}, None));
 					}
@@ -12134,6 +12136,7 @@ where
 												channel_id: funded_channel.context.channel_id(),
 												user_channel_id: funded_channel.context.get_user_id(),
 												counterparty_node_id: funded_channel.context.get_counterparty_node_id(),
+												funding_txo: funded_channel.funding.get_funding_txo().map(|outpoint| outpoint.into_bitcoin_outpoint()),
 												channel_type: funded_channel.funding.get_channel_type().clone(),
 											}, None));
 										}
