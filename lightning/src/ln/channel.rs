32f0f6bb034f4460f655598de0c9a249d0807dc4
Jeffrey Czyz <jkczyz@gmail.com>
2025-05-13 15:08:27 -0500

    Reset FundingScope::funding_tx_confirmation_height
    FundingScope::funding_tx_confirmation_height is reset as part of calling
    ChannelContext::check_funding_meets_minimum_depth via
    FundedChannel::check_get_channel_ready. This side effect requires using
    mutable references to self when otherwise it would not be needed.
    Instead of reseting funding_tx_confirmation_height there, do so when
    unconfirming the funding transaction.

--- lightning/src/ln/channel.rs
+++ lightning/src/ln/channel.rs
@@ -4974,7 +4974,7 @@ impl<SP: Deref> ChannelContext<SP> where SP::Target: SignerProvider {
 		self.get_initial_counterparty_commitment_signature(funding, logger)
 	}
 
-	fn check_funding_meets_minimum_depth(&self, funding: &mut FundingScope, height: u32) -> bool {
+	fn check_funding_meets_minimum_depth(&self, funding: &FundingScope, height: u32) -> bool {
 		let is_coinbase = funding
 			.funding_transaction
 			.as_ref()
@@ -4997,10 +4997,6 @@ impl<SP: Deref> ChannelContext<SP> where SP::Target: SignerProvider {
 		}
 
 		let funding_tx_confirmations = height as i64 - funding.funding_tx_confirmation_height as i64 + 1;
-		if funding_tx_confirmations <= 0 {
-			funding.funding_tx_confirmation_height = 0;
-		}
-
 		if funding_tx_confirmations < minimum_depth.unwrap_or(0) as i64 {
 			return false;
 		}
@@ -8221,7 +8217,7 @@ impl<SP: Deref> FundedChannel<SP> where
 		// Called:
 		//  * always when a new block/transactions are confirmed with the new height
 		//  * when funding is signed with a height of 0
-		if !self.context.check_funding_meets_minimum_depth(&mut self.funding, height) {
+		if !self.context.check_funding_meets_minimum_depth(&self.funding, height) {
 			return None;
 		}
 
@@ -8480,6 +8476,9 @@ impl<SP: Deref> FundedChannel<SP> where
 			// larger. If we don't know that time has moved forward, we can just set it to the last
 			// time we saw and it will be ignored.
 			let best_time = self.context.update_time_counter;
+
+			self.funding.funding_tx_confirmation_height = 0;
+
 			match self.do_best_block_updated(reorg_height, best_time, None::<(ChainHash, &&dyn NodeSigner, &UserConfig)>, logger) {
 				Ok((channel_ready, timed_out_htlcs, announcement_sigs)) => {
 					assert!(channel_ready.is_none(), "We can't generate a funding with 0 confirmations?");
