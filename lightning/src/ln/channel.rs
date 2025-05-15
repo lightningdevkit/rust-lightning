704569a0cef564f69949236151e2c0163dbc05ed
Jeffrey Czyz <jkczyz@gmail.com>
2025-05-15 16:48:35 -0500

    Move check_funding_meets_minimum_depth to FundedChannel
    This method is only applicable for FundedChannel, so it shouldn't be
    accessible from ChannelContext.

--- lightning/src/ln/channel.rs
+++ lightning/src/ln/channel.rs
@@ -5039,43 +5039,6 @@ impl<SP: Deref> ChannelContext<SP> where SP::Target: SignerProvider {
 		self.get_initial_counterparty_commitment_signature(funding, logger)
 	}
 
-	fn check_funding_meets_minimum_depth(&self, funding: &FundingScope, height: u32) -> bool {
-		let minimum_depth = self.minimum_depth
-			.expect("ChannelContext::minimum_depth should be set for FundedChannel");
-
-		// Zero-conf channels always meet the minimum depth.
-		if minimum_depth == 0 {
-			return true;
-		}
-
-		let is_coinbase = funding
-			.funding_transaction
-			.as_ref()
-			.map(|tx| tx.is_coinbase())
-			.unwrap_or(false);
-
-		let minimum_depth = {
-			// If the funding transaction is a coinbase transaction, we need to set the minimum
-			// depth to 100.
-			if is_coinbase && minimum_depth < COINBASE_MATURITY {
-				COINBASE_MATURITY
-			} else {
-				minimum_depth
-			}
-		};
-
-		if funding.funding_tx_confirmation_height == 0 {
-			return false;
-		}
-
-		let funding_tx_confirmations = height as i64 - funding.funding_tx_confirmation_height as i64 + 1;
-		if funding_tx_confirmations < minimum_depth as i64 {
-			return false;
-		}
-
-		return true;
-	}
-
 	fn check_for_funding_tx_confirmed(
 		&mut self, funding: &mut FundingScope, block_hash: &BlockHash, height: u32,
 		index_in_block: usize, tx: &mut ConfirmedTransaction,
@@ -8518,7 +8481,7 @@ impl<SP: Deref> FundedChannel<SP> where
 		// Called:
 		//  * always when a new block/transactions are confirmed with the new height
 		//  * when funding is signed with a height of 0
-		if !self.context.check_funding_meets_minimum_depth(&self.funding, height) {
+		if !self.check_funding_meets_minimum_depth(&self.funding, height) {
 			return None;
 		}
 
@@ -8591,7 +8554,7 @@ impl<SP: Deref> FundedChannel<SP> where
 	fn check_get_splice_locked(
 		&self, pending_splice: &PendingSplice, funding: &FundingScope, height: u32,
 	) -> Option<msgs::SpliceLocked> {
-		if !self.context.check_funding_meets_minimum_depth(funding, height) {
+		if !self.check_funding_meets_minimum_depth(funding, height) {
 			return None;
 		}
 
@@ -8612,6 +8575,43 @@ impl<SP: Deref> FundedChannel<SP> where
 				})
 			},
 		}
+	}
+
+	fn check_funding_meets_minimum_depth(&self, funding: &FundingScope, height: u32) -> bool {
+		let minimum_depth = self.context.minimum_depth
+			.expect("ChannelContext::minimum_depth should be set for FundedChannel");
+
+		// Zero-conf channels always meet the minimum depth.
+		if minimum_depth == 0 {
+			return true;
+		}
+
+		let is_coinbase = funding
+			.funding_transaction
+			.as_ref()
+			.map(|tx| tx.is_coinbase())
+			.unwrap_or(false);
+
+		let minimum_depth = {
+			// If the funding transaction is a coinbase transaction, we need to set the minimum
+			// depth to 100.
+			if is_coinbase && minimum_depth < COINBASE_MATURITY {
+				COINBASE_MATURITY
+			} else {
+				minimum_depth
+			}
+		};
+
+		if funding.funding_tx_confirmation_height == 0 {
+			return false;
+		}
+
+		let funding_tx_confirmations = height as i64 - funding.funding_tx_confirmation_height as i64 + 1;
+		if funding_tx_confirmations < minimum_depth as i64 {
+			return false;
+		}
+
+		return true;
 	}
 
 	#[cfg(splicing)]
