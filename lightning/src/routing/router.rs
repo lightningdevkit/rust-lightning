5da244eb8d06ce7b9e3688b54a6dca9e22163244
Elias Rohrer <dev@tnull.de>
2025-05-21 14:35:57 +0200

    Derive `Debug` for `RouteParametersConfig`
    .. as we'll reuse it in LDK Node, where we'll need `Debug`.

--- lightning/src/routing/router.rs
+++ lightning/src/routing/router.rs
@@ -1075,7 +1075,7 @@ impl PaymentParameters {
 }
 
 /// A struct for configuring parameters for routing the payment.
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 pub struct RouteParametersConfig {
 	/// The maximum total fees, in millisatoshi, that may accrue during route finding.
 	///
