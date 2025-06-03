1b095ea7d790db4d644b980dafbd4f3982b76900
Jeffrey Czyz <jkczyz@gmail.com>
2025-06-03 17:19:08 -0500

    f - add logging to transaction_unconfirmed

--- lightning/src/ln/channel.rs
+++ lightning/src/ln/channel.rs
@@ -8931,6 +8931,12 @@ impl<SP: Deref> FundedChannel<SP> where
 				#[cfg(splicing)]
 				if let Some(pending_splice) = &mut self.pending_splice {
 					if pending_splice.sent_funding_txid == Some(*txid) {
+						log_warn!(
+							logger,
+							"Unconfirming sent splice_locked txid {} for channel {}",
+							txid,
+							&self.context.channel_id,
+						);
 						pending_splice.sent_funding_txid = None;
 					}
 				}
