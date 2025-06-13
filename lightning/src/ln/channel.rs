f65c60bb1a5de03a8daf1f65cd9b39536f97ec27
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2025-06-13 14:29:54 -0400

    ln: add channel type awareness to get_dust_exposure_limiting_feerate
    Co-authored-by: Matt Corallo <git@bluematt.me>

--- lightning/src/ln/channel.rs
+++ lightning/src/ln/channel.rs
@@ -3831,13 +3831,20 @@ where
 		cmp::max(self.config.options.cltv_expiry_delta, MIN_CLTV_EXPIRY_DELTA)
 	}
 
+	/// Returns a maximum "sane" fee rate used to reason about our dust exposure.
+	/// Will be Some if the `channel_type`'s dust exposure depends on its commitment fee rate, and
+	/// None otherwise.
 	fn get_dust_exposure_limiting_feerate<F: Deref>(
-		&self, fee_estimator: &LowerBoundedFeeEstimator<F>,
+		&self, fee_estimator: &LowerBoundedFeeEstimator<F>, channel_type: &ChannelTypeFeatures,
 	) -> Option<u32>
 	where
 		F::Target: FeeEstimator,
 	{
-		Some(fee_estimator.bounded_sat_per_1000_weight(ConfirmationTarget::MaximumFeeEstimate))
+		if channel_type.supports_anchor_zero_fee_commitments() {
+			None
+		} else {
+			Some(fee_estimator.bounded_sat_per_1000_weight(ConfirmationTarget::MaximumFeeEstimate))
+		}
 	}
 
 	/// Returns the maximum configured dust exposure.
@@ -3974,7 +3981,9 @@ where
 			return Err(ChannelError::close("Remote side tried to send more than the total value of the channel".to_owned()));
 		}
 
-		let dust_exposure_limiting_feerate = self.get_dust_exposure_limiting_feerate(&fee_estimator);
+		let dust_exposure_limiting_feerate = self.get_dust_exposure_limiting_feerate(
+			&fee_estimator, funding.get_channel_type(),
+		);
 		let htlc_stats = self.get_pending_htlc_stats(funding, None, dust_exposure_limiting_feerate);
 		if htlc_stats.pending_inbound_htlcs + 1 > self.holder_max_accepted_htlcs as usize {
 			return Err(ChannelError::close(format!("Remote tried to push more than our max accepted HTLCs ({})", self.holder_max_accepted_htlcs)));
@@ -4058,7 +4067,9 @@ where
 		F::Target: FeeEstimator,
 	{
 		// Check that we won't be pushed over our dust exposure limit by the feerate increase.
-		let dust_exposure_limiting_feerate = self.get_dust_exposure_limiting_feerate(&fee_estimator);
+		let dust_exposure_limiting_feerate = self.get_dust_exposure_limiting_feerate(
+			&fee_estimator, funding.get_channel_type(),
+		);
 		let htlc_stats = self.get_pending_htlc_stats(funding, None, dust_exposure_limiting_feerate);
 		let max_dust_htlc_exposure_msat = self.get_max_dust_htlc_exposure_msat(dust_exposure_limiting_feerate);
 		if htlc_stats.on_holder_tx_dust_exposure_msat > max_dust_htlc_exposure_msat {
@@ -4193,7 +4204,9 @@ where
 		L::Target: Logger,
 	{
 		// Before proposing a feerate update, check that we can actually afford the new fee.
-		let dust_exposure_limiting_feerate = self.get_dust_exposure_limiting_feerate(&fee_estimator);
+		let dust_exposure_limiting_feerate = self.get_dust_exposure_limiting_feerate(
+			&fee_estimator, funding.get_channel_type(),
+		);
 		let htlc_stats = self.get_pending_htlc_stats(funding, Some(feerate_per_kw), dust_exposure_limiting_feerate);
 		let commitment_data = self.build_commitment_transaction(
 			funding, holder_commitment_point.transaction_number(),
@@ -4798,7 +4811,9 @@ where
 		// Note that we have to handle overflow due to the case mentioned in the docs in general
 		// here.
 
-		let dust_exposure_limiting_feerate = self.get_dust_exposure_limiting_feerate(&fee_estimator);
+		let dust_exposure_limiting_feerate = self.get_dust_exposure_limiting_feerate(
+			&fee_estimator, funding.get_channel_type(),
+		);
 		let htlc_stats = context.get_pending_htlc_stats(funding, None, dust_exposure_limiting_feerate);
 
 		let outbound_capacity_msat = funding.value_to_self_msat
@@ -8514,7 +8529,9 @@ where
 			return Err(LocalHTLCFailureReason::ChannelClosed)
 		}
 
-		let dust_exposure_limiting_feerate = self.context.get_dust_exposure_limiting_feerate(&fee_estimator);
+		let dust_exposure_limiting_feerate = self.context.get_dust_exposure_limiting_feerate(
+			&fee_estimator, self.funding.get_channel_type(),
+		);
 
 		core::iter::once(&self.funding)
 			.chain(self.pending_funding.iter())
