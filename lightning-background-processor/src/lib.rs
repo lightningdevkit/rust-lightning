82343935f91cf438d679605995c1af399b7e4c56
Elias Rohrer <dev@tnull.de>
2025-06-25 09:59:45 +0200

    Add copyright header to `lightning-background-processor/src/lib.rs`

--- lightning-background-processor/src/lib.rs
+++ lightning-background-processor/src/lib.rs
@@ -1,3 +1,12 @@
+// This file is Copyright its original authors, visible in version control
+// history.
+//
+// This file is licensed under the Apache License, Version 2.0 <LICENSE-APACHE
+// or http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
+// You may not use this file except in accordance with one or both of these
+// licenses.
+
 //! Utilities that take care of tasks that (1) need to happen periodically to keep Rust-Lightning
 //! running properly, and (2) either can or should be run in the background.
 #![cfg_attr(feature = "std", doc = "See docs for [`BackgroundProcessor`] for more details.")]
