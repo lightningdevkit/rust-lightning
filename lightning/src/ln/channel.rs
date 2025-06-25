bb91bb6434855526e551f4cfc4519a1074d2d1a9
Joost Jager <joost.jager@gmail.com>
2025-06-25 10:43:08 +0200

    Add AttributionData to UpdateFulfillHTLC message

--- lightning/src/ln/channel.rs
+++ lightning/src/ln/channel.rs
@@ -8385,6 +8385,7 @@ where
 							channel_id: self.context.channel_id(),
 							htlc_id: htlc.htlc_id,
 							payment_preimage: payment_preimage.clone(),
+							attribution_data: None,
 						});
 					},
 				}
