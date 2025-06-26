75a54f74d8bc334e63a2727378bebd726ef881f5
Matt Corallo <git@bluematt.me>
2025-06-26 11:18:03 +0000

    Move `Poly1305` fuzzing logic from `chacha20poly1305rfc.rs`
    Rather than skipping compilation of `poly1305.rs` when building for
    fuzzing and relying on `ChaCha20Poly1305` to do the fuzzing
    variants, implement an actual fuzz wrapper in `poly1305.rs`,
    keeping the same fuzz MAC structure that we already have.
    
    We also add a fuzzing implementation of `fixed_time_eq` which does
    a simple comparison, to allow the fuzzer to "see into" the
    comparison in some cases.
    
    Best reviewed with `-b`.

--- lightning/src/crypto/mod.rs
+++ lightning/src/crypto/mod.rs
@@ -1,9 +1,14 @@
 #[cfg(not(fuzzing))]
-use bitcoin::hashes::cmp::fixed_time_eq;
+pub(crate) use bitcoin::hashes::cmp::fixed_time_eq;
+
+#[cfg(fuzzing)]
+fn fixed_time_eq(a: &[u8], b: &[u8]) -> bool {
+	assert_eq!(a.len(), b.len());
+	a == b
+}
 
 pub(crate) mod chacha20;
 pub(crate) mod chacha20poly1305rfc;
-#[cfg(not(fuzzing))]
 pub(crate) mod poly1305;
 pub(crate) mod streams;
 pub(crate) mod utils;
