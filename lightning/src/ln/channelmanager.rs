9949a3357bce0cc62b92669c735138c98da2fc93
Elias Rohrer <dev@tnull.de>
2025-07-03 13:17:01 +0200

    Use `optionally_notify` in `process_pending_htlc_forwards`
    We move the code into the `optionally_notify` closure, but maintain the
    behavior for now. In the next step, we'll use this to make sure we only
    repersist when necessary.
    
    Best reviewed via `git diff --ignore-all-space`

--- lightning/src/ln/channelmanager.rs
+++ lightning/src/ln/channelmanager.rs
@@ -6356,8 +6356,14 @@ where
 	///
 	/// Will regularly be called by the background processor.
 	pub fn process_pending_htlc_forwards(&self) {
-		let _persistence_guard = PersistenceNotifierGuard::notify_on_drop(self);
+		let _persistence_guard = PersistenceNotifierGuard::optionally_notify(self, || {
+			self.internal_process_pending_htlc_forwards()
+		});
+	}
 
+	// Returns whether or not we need to re-persist.
+	fn internal_process_pending_htlc_forwards(&self) -> NotifyOption {
+		let should_persist = NotifyOption::DoPersist;
 		self.process_pending_update_add_htlcs();
 
 		let mut new_events = VecDeque::new();
@@ -6414,10 +6420,11 @@ where
 		self.check_free_holding_cells();
 
 		if new_events.is_empty() {
-			return;
+			return should_persist;
 		}
 		let mut events = self.pending_events.lock().unwrap();
 		events.append(&mut new_events);
+		should_persist
 	}
 
 	fn process_forward_htlcs(
