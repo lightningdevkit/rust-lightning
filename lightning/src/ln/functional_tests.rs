8383d2b8e1fe24617a9d57c63965a3c34cccdf14
Matt Corallo <git@bluematt.me>
2025-08-02 02:19:49 +0000

    Generate new `ReleasePaymentComplete` monitor updates
    `MonitorEvent`s aren't delivered to the `ChannelManager` in a
    durable fasion - if the `ChannelManager` fetches the pending
    `MonitorEvent`s, then the `ChannelMonitor` gets persisted (i.e. due
    to a block update) then the node crashes, prior to persisting the
    `ChannelManager` again, the `MonitorEvent` and its effects on the
    `ChannelManger` will be lost. This isn't likely in a sync persist
    environment, but in an async one this could be an issue.
    
    Note that this is only an issue for closed channels -
    `MonitorEvent`s only inform the `ChannelManager` that a channel is
    closed (which the `ChannelManager` will learn on startup or when it
    next tries to advance the channel state), that
    `ChannelMonitorUpdate` writes completed (which the `ChannelManager`
    will detect on startup), or that HTLCs resolved on-chain post
    closure. Of the three, only the last is problematic to lose prior
    to a reload.
    
    In previous commits we ensured that HTLC resolutions which came to
    `ChannelManager` via a `MonitorEvent` were replayed on startup if
    the `MonitorEvent` was lost. However, in cases where the
    `ChannelManager` was so stale that it didn't have the payment state
    for an HTLC at all, we only re-add it in cases where
    `ChannelMonitor::get_pending_or_resolved_outbound_htlcs` includes
    it.
    
    Because constantly re-adding a payment state and then failing it
    would generate lots of noise for users on startup (not to mention
    risk of confusing stale payment events for the latest state of a
    payment when the `PaymentId` has been reused to retry a payment).
    Thus, `get_pending_or_resolved_outbound_htlcs` does not include
    state for HTLCs which were resolved on chain with a preimage or
    HTLCs which were resolved on chain with a timeout after
    `ANTI_REORG_DELAY` confirmations.
    
    This critera matches the critera for generating a `MonitorEvent`,
    and works great under the assumption that `MonitorEvent`s are
    reliably delivered. However, if they are not, and our
    `ChannelManager` is lost or substantially old (or, in a future
    where we do not persist `ChannelManager` at all), we will not end
    up seeing payment resolution events for an HTLC.
    
    Instead, we really want to tell our `ChannelMonitor`s when the
    resolution of an HTLC is complete. Note that we don't particularly
    care about non-payment HTLCs, as there is no re-hydration of state
    to do there - `ChannelManager` load ignores forwarded HTLCs coming
    back from `get_pending_or_resolved_outbound_htlcs` as there's
    nothing to do - we always attempt to replay the success/failure and
    figure out if it mattered based on whether there was still an HTLC
    to claim/fail.
    
    Here we begin generating the new
    `ChannelMonitorUpdateStep::ReleasePaymentComplete` updates,
    updating functional tests for the new `ChannelMonitorUpdate`s where
    required.

--- lightning/src/ln/functional_tests.rs
+++ lightning/src/ln/functional_tests.rs
@@ -1528,7 +1528,8 @@ pub fn claim_htlc_outputs() {
 		// ANTI_REORG_DELAY confirmations.
 		mine_transaction(&nodes[1], accepted_claim);
 		connect_blocks(&nodes[1], ANTI_REORG_DELAY - 1);
-		expect_payment_failed!(nodes[1], payment_hash_2, false);
+		let conditions = PaymentFailedConditions::new().from_mon_update();
+		expect_payment_failed_conditions(&nodes[1], payment_hash_2, false, conditions);
 	}
 	get_announce_close_broadcast_events(&nodes, 0, 1);
 	assert_eq!(nodes[0].node.list_channels().len(), 0);
@@ -2015,6 +2016,7 @@ pub fn test_htlc_on_chain_success() {
 	check_closed_broadcast!(nodes[0], true);
 	check_added_monitors(&nodes[0], 1);
 	let events = nodes[0].node.get_and_clear_pending_events();
+	check_added_monitors(&nodes[0], 2);
 	assert_eq!(events.len(), 5);
 	let mut first_claimed = false;
 	for event in events {
@@ -2433,7 +2435,11 @@ fn do_test_commitment_revoked_fail_backward_exhaustive(
 	check_added_monitors(&nodes[1], 1);
 	connect_blocks(&nodes[1], ANTI_REORG_DELAY - 1);
 
+	check_added_monitors(&nodes[1], 0);
 	let events = nodes[1].node.get_and_clear_pending_events();
+	if deliver_bs_raa {
+		check_added_monitors(&nodes[1], 1);
+	}
 	assert_eq!(events.len(), if deliver_bs_raa { 3 + nodes.len() - 1 } else { 3 + nodes.len() });
 	assert!(events.iter().any(|ev| matches!(
 		ev,
@@ -4036,7 +4042,8 @@ pub fn test_static_spendable_outputs_timeout_tx() {
 	mine_transaction(&nodes[1], &node_txn[0]);
 	check_closed_event!(nodes[1], 1, ClosureReason::CommitmentTxConfirmed, [node_a_id], 100000);
 	connect_blocks(&nodes[1], ANTI_REORG_DELAY - 1);
-	expect_payment_failed!(nodes[1], our_payment_hash, false);
+	let conditions = PaymentFailedConditions::new().from_mon_update();
+	expect_payment_failed_conditions(&nodes[1], our_payment_hash, false, conditions);
 
 	let spend_txn = check_spendable_outputs!(nodes[1], node_cfgs[1].keys_manager);
 	assert_eq!(spend_txn.len(), 3); // SpendableOutput: remote_commitment_tx.to_remote, timeout_tx.output
@@ -5109,7 +5116,8 @@ pub fn test_dynamic_spendable_outputs_local_htlc_timeout_tx() {
 
 	mine_transaction(&nodes[0], &htlc_timeout);
 	connect_blocks(&nodes[0], BREAKDOWN_TIMEOUT as u32 - 1);
-	expect_payment_failed!(nodes[0], our_payment_hash, false);
+	let conditions = PaymentFailedConditions::new().from_mon_update();
+	expect_payment_failed_conditions(&nodes[0], our_payment_hash, false, conditions);
 
 	// Verify that A is able to spend its own HTLC-Timeout tx thanks to spendable output event given back by its ChannelMonitor
 	let spend_txn = check_spendable_outputs!(nodes[0], node_cfgs[0].keys_manager);
@@ -5229,7 +5237,8 @@ pub fn test_key_derivation_params() {
 
 	mine_transaction(&nodes[0], &htlc_timeout);
 	connect_blocks(&nodes[0], BREAKDOWN_TIMEOUT as u32 - 1);
-	expect_payment_failed!(nodes[0], our_payment_hash, false);
+	let conditions = PaymentFailedConditions::new().from_mon_update();
+	expect_payment_failed_conditions(&nodes[0], our_payment_hash, false, conditions);
 
 	// Verify that A is able to spend its own HTLC-Timeout tx thanks to spendable output event given back by its ChannelMonitor
 	let new_keys_manager = test_utils::TestKeysInterface::new(&seed, Network::Testnet);
@@ -6233,7 +6242,9 @@ fn do_test_failure_delay_dust_htlc_local_commitment(announce_latest: bool) {
 
 	assert_eq!(nodes[0].node.get_and_clear_pending_events().len(), 0);
 	connect_blocks(&nodes[0], ANTI_REORG_DELAY - 1);
+	check_added_monitors(&nodes[0], 0);
 	let events = nodes[0].node.get_and_clear_pending_events();
+	check_added_monitors(&nodes[0], 2);
 	// Only 2 PaymentPathFailed events should show up, over-dust HTLC has to be failed by timeout tx
 	assert_eq!(events.len(), 4);
 	let mut first_failed = false;
@@ -6302,12 +6313,14 @@ fn do_test_sweep_outbound_htlc_failure_update(revoked: bool, local: bool) {
 		// We fail dust-HTLC 1 by broadcast of local commitment tx
 		mine_transaction(&nodes[0], &as_commitment_tx[0]);
 		check_closed_event!(nodes[0], 1, ClosureReason::CommitmentTxConfirmed, [node_b_id], 100000);
-		connect_blocks(&nodes[0], ANTI_REORG_DELAY - 1);
-		expect_payment_failed!(nodes[0], dust_hash, false);
-
-		connect_blocks(&nodes[0], TEST_FINAL_CLTV + LATENCY_GRACE_PERIOD_BLOCKS - ANTI_REORG_DELAY);
 		check_closed_broadcast!(nodes[0], true);
 		check_added_monitors(&nodes[0], 1);
+		connect_blocks(&nodes[0], ANTI_REORG_DELAY - 1);
+		let conditions = PaymentFailedConditions::new().from_mon_update();
+		expect_payment_failed_conditions(&nodes[0], dust_hash, false, conditions);
+
+		connect_blocks(&nodes[0], TEST_FINAL_CLTV + LATENCY_GRACE_PERIOD_BLOCKS - ANTI_REORG_DELAY);
+		check_added_monitors(&nodes[0], 0);
 		assert_eq!(nodes[0].node.get_and_clear_pending_events().len(), 0);
 		timeout_tx.push(nodes[0].tx_broadcaster.txn_broadcasted.lock().unwrap()[0].clone());
 		assert_eq!(
@@ -6318,7 +6331,8 @@ fn do_test_sweep_outbound_htlc_failure_update(revoked: bool, local: bool) {
 		assert_eq!(nodes[0].node.get_and_clear_pending_events().len(), 0);
 		mine_transaction(&nodes[0], &timeout_tx[0]);
 		connect_blocks(&nodes[0], ANTI_REORG_DELAY - 1);
-		expect_payment_failed!(nodes[0], non_dust_hash, false);
+		let conditions = PaymentFailedConditions::new().from_mon_update();
+		expect_payment_failed_conditions(&nodes[0], non_dust_hash, false, conditions);
 	} else {
 		// We fail dust-HTLC 1 by broadcast of remote commitment tx. If revoked, fail also non-dust HTLC
 		mine_transaction(&nodes[0], &bs_commitment_tx[0]);
@@ -6339,7 +6353,8 @@ fn do_test_sweep_outbound_htlc_failure_update(revoked: bool, local: bool) {
 		check_spends!(timeout_tx[0], bs_commitment_tx[0]);
 		// For both a revoked or non-revoked commitment transaction, after ANTI_REORG_DELAY the
 		// dust HTLC should have been failed.
-		expect_payment_failed!(nodes[0], dust_hash, false);
+		let conditions = PaymentFailedConditions::new().from_mon_update();
+		expect_payment_failed_conditions(&nodes[0], dust_hash, false, conditions);
 
 		if !revoked {
 			assert_eq!(
@@ -6353,7 +6368,8 @@ fn do_test_sweep_outbound_htlc_failure_update(revoked: bool, local: bool) {
 		mine_transaction(&nodes[0], &timeout_tx[0]);
 		assert_eq!(nodes[0].node.get_and_clear_pending_events().len(), 0);
 		connect_blocks(&nodes[0], ANTI_REORG_DELAY - 1);
-		expect_payment_failed!(nodes[0], non_dust_hash, false);
+		let conditions = PaymentFailedConditions::new().from_mon_update();
+		expect_payment_failed_conditions(&nodes[0], non_dust_hash, false, conditions);
 	}
 }
 
@@ -8429,7 +8445,8 @@ pub fn test_htlc_no_detection() {
 		&create_dummy_block(nodes[0].best_block_hash(), 42, vec![htlc_timeout.clone()]),
 	);
 	connect_blocks(&nodes[0], ANTI_REORG_DELAY - 1);
-	expect_payment_failed!(nodes[0], our_payment_hash, false);
+	let conditions = PaymentFailedConditions::new().from_mon_update();
+	expect_payment_failed_conditions(&nodes[0], our_payment_hash, false, conditions);
 }
 
 fn do_test_onchain_htlc_settlement_after_close(
