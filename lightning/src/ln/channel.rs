73a6d978ff587763667650c2ceb139cfe38bfe67
Matt Corallo <git@bluematt.me>
2025-08-12 23:01:48 +0000

    Reject `splice_init`s when we aren't quiescent

--- lightning/src/ln/channel.rs
+++ lightning/src/ln/channel.rs
@@ -11110,6 +11110,10 @@ where
 		ES::Target: EntropySource,
 		L::Target: Logger,
 	{
+		if !self.context.channel_state.is_quiescent() {
+			return Err(ChannelError::WarnAndDisconnect("Quiescence needed to splice".to_owned()));
+		}
+
 		let our_funding_contribution = SignedAmount::from_sat(our_funding_contribution_satoshis);
 		let splice_funding = self.validate_splice_init(msg, our_funding_contribution)?;
 
