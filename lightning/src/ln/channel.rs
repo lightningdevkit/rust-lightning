6737bdef75ed3879d481018aca8a3e6aa95c6434
Valentine Wallace <vwallace@protonmail.com>
2025-08-29 15:57:21 -0400

    Send held_htlc_available with counterparty reply path
    As part of supporting sending payments as an often-offline sender, the sender
    needs to send held_htlc_available onion messages such that the reply path to
    the message terminates at their always-online channel counterparty that is
    holding the HTLC. That way when the recipient responds with release_held_htlc,
    the sender's counterparty will receive that message.
    
    After laying groundwork over some past commits, here we as an async sender send
    held_htlc_available messages using reply paths created by our always-online
    channel counterparty.

--- lightning/src/ln/channel.rs
+++ lightning/src/ln/channel.rs
@@ -80,6 +80,7 @@ use crate::ln::script::{self, ShutdownScript};
 use crate::ln::types::ChannelId;
 #[cfg(splicing)]
 use crate::ln::LN_MAX_MSG_LEN;
+use crate::offers::static_invoice::StaticInvoice;
 use crate::routing::gossip::NodeId;
 use crate::sign::ecdsa::EcdsaChannelSigner;
 use crate::sign::tx_builder::{HTLCAmountDirection, NextCommitmentStats, SpecTxBuilder, TxBuilder};
@@ -7992,10 +7993,25 @@ where
 	/// waiting on this revoke_and_ack. The generation of this new commitment_signed may also fail,
 	/// generating an appropriate error *after* the channel state has been updated based on the
 	/// revoke_and_ack message.
+	///
+	/// The static invoices will be used by us as an async sender to enqueue [`HeldHtlcAvailable`]
+	/// onion messages for the often-offline recipient, and the blinded reply paths the invoices are
+	/// paired with were created by our channel counterparty and will be used as reply paths for
+	/// corresponding [`ReleaseHeldHtlc`] messages.
+	///
+	/// [`HeldHtlcAvailable`]: crate::onion_message::async_payments::HeldHtlcAvailable
+	/// [`ReleaseHeldHtlc`]: crate::onion_message::async_payments::ReleaseHeldHtlc
 	pub fn revoke_and_ack<F: Deref, L: Deref>(
 		&mut self, msg: &msgs::RevokeAndACK, fee_estimator: &LowerBoundedFeeEstimator<F>,
 		logger: &L, hold_mon_update: bool,
-	) -> Result<(Vec<(HTLCSource, PaymentHash)>, Option<ChannelMonitorUpdate>), ChannelError>
+	) -> Result<
+		(
+			Vec<(HTLCSource, PaymentHash)>,
+			Vec<(StaticInvoice, BlindedMessagePath)>,
+			Option<ChannelMonitorUpdate>,
+		),
+		ChannelError,
+	>
 	where
 		F::Target: FeeEstimator,
 		L::Target: Logger,
@@ -8110,6 +8126,7 @@ where
 		let mut finalized_claimed_htlcs = Vec::new();
 		let mut update_fail_htlcs = Vec::new();
 		let mut update_fail_malformed_htlcs = Vec::new();
+		let mut static_invoices = Vec::new();
 		let mut require_commitment = false;
 		let mut value_to_self_msat_diff: i64 = 0;
 
@@ -8225,6 +8242,20 @@ where
 				}
 			}
 			for htlc in pending_outbound_htlcs.iter_mut() {
+				for (htlc_id, blinded_path) in &msg.release_htlc_message_paths {
+					if htlc.htlc_id != *htlc_id {
+						continue;
+					}
+					let static_invoice = match htlc.source.static_invoice() {
+						Some(inv) => inv,
+						None => {
+							// This is reachable but it means the counterparty is buggy and included a release
+							// path for an HTLC that we didn't originally flag as a hold_htlc.
+							continue;
+						},
+					};
+					static_invoices.push((static_invoice, blinded_path.clone()));
+				}
 				if let OutboundHTLCState::LocalAnnounced(_) = htlc.state {
 					log_trace!(
 						logger,
@@ -8292,9 +8323,9 @@ where
 					self.context
 						.blocked_monitor_updates
 						.push(PendingChannelMonitorUpdate { update: monitor_update });
-					return Ok(($htlcs_to_fail, None));
+					return Ok(($htlcs_to_fail, static_invoices, None));
 				} else {
-					return Ok(($htlcs_to_fail, Some(monitor_update)));
+					return Ok(($htlcs_to_fail, static_invoices, Some(monitor_update)));
 				}
 			};
 		}
