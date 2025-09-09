1671d7588ba3a0538d4aac41590e9c3ea1e25e7e
Leo Nash <hello@leonash.net>
2025-09-09 23:50:49 +0000

    Do not produce 0FC HTLC txs on `unsafe_get_latest_holder_commitment_txn`
    As in CSV anchor channels, HTLC transactions in 0FC channels require
    external funding before they can be finalized, so we choose to return
    the commitment transaction alone here.

--- lightning/src/chain/channelmonitor.rs
+++ lightning/src/chain/channelmonitor.rs
@@ -5131,9 +5131,22 @@ impl<Signer: EcdsaChannelSigner> ChannelMonitorImpl<Signer> {
 			self.funding.current_holder_commitment_tx.add_holder_sig(&redeem_script, sig)
 		};
 		let mut holder_transactions = vec![commitment_tx];
-		// When anchor outputs are present, the HTLC transactions are only final once the commitment
-		// transaction confirms due to the CSV 1 encumberance.
-		if self.channel_type_features().supports_anchors_zero_fee_htlc_tx() {
+
+		if self.channel_type_features().supports_anchors_zero_fee_htlc_tx()
+			|| self.channel_type_features().supports_anchor_zero_fee_commitments()
+		{
+			// HTLC transactions in these channels require external funding before finalized,
+			// so we return the commitment transaction alone here.
+			//
+			// In 0FC channels, we *could* use HTLC transactions to pay for fees on a
+			// 0FC commitment transaction to save the fixed transaction overhead
+			// (locktime + version), but we would still have to pay for fees using
+			// external UTXOs to avoid invalidating the counterparty HTLC signature.
+			// This is something we would consider in the future.
+			//
+			// Furthermore, we can't broadcast a HTLC claim transaction while the
+			// anchor claim transaction and its parent are still unconfirmed due to the
+			// current single-child restriction on TRUC transactions.
 			return holder_transactions;
 		}
 
