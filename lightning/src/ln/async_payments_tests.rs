9318a692fa8c0e97a00a1fc1b82bb42d93aa82c6
Valentine Wallace <vwallace@protonmail.com>
2025-09-10 12:12:53 -0400

    Use cached peers in OffersMessageFlow
    In the previous commit, we started caching the set of peers in the
    OffersMessageFlow that are used when creating blinded paths. Here we start
    using that cache and stop passing in the peers for every method.

--- lightning/src/ln/async_payments_tests.rs
+++ lightning/src/ln/async_payments_tests.rs
@@ -276,7 +276,6 @@ fn create_static_invoice_builder<'a>(
 			payment_secret,
 			relative_expiry_secs,
 			recipient.node.list_usable_channels(),
-			recipient.node.test_get_peers_for_blinded_path(),
 		)
 		.unwrap()
 }
