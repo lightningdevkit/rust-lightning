2f855cb94fdaadc5d5dd64059578476b640fe9f1
Martin Saposnic <martinsaposnic@gmail.com>
2025-09-26 11:54:09 -0300

    Queue holder commit once funding tx confirms
    Sets should_broadcast_commitment=true when funding confirms.
    
    Since we skip the initial broadcast when funding_seen_onchain is false,
    we need to queue it once funding actually hits the chain.
    
    Backport of be1955a2bdcc0c7409980849dc147a2c3d6a8d69

--- lightning/src/chain/channelmonitor.rs
+++ lightning/src/chain/channelmonitor.rs
@@ -5355,6 +5355,7 @@ impl<Signer: EcdsaChannelSigner> ChannelMonitorImpl<Signer> {
 		F::Target: FeeEstimator,
 		L::Target: Logger,
 	{
+		let funding_seen_before = self.funding_seen_onchain;
 		let txn_matched = self.filter_block(txdata);
 
 		if !self.funding_seen_onchain {
@@ -5389,6 +5390,11 @@ impl<Signer: EcdsaChannelSigner> ChannelMonitorImpl<Signer> {
 
 		let mut watch_outputs = Vec::new();
 		let mut claimable_outpoints = Vec::new();
+
+		if self.is_manual_broadcast && !funding_seen_before && self.funding_seen_onchain && self.holder_tx_signed
+		{
+			should_broadcast_commitment = true;
+		}
 		'tx_iter: for tx in &txn_matched {
 			let txid = tx.compute_txid();
 			log_trace!(logger, "Transaction {} confirmed in block {}", txid , block_hash);
