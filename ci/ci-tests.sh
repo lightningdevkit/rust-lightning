2d7a53f0ae7f55a800bfa704b8d7ac0e28b6c02e
Elias Rohrer <dev@tnull.de>
2025-10-20 03:01:30 +0200

    Drop `cfg(async_payments)` from tests
    We previously dropped `cfg(async_payments)` but didn't drop the actual
    `cargo test` call from our CI. Here we do that.

--- ci/ci-tests.sh
+++ ci/ci-tests.sh
@@ -141,8 +141,6 @@ fi
 echo -e "\n\nTest cfg-flag builds"
 RUSTFLAGS="--cfg=taproot" cargo test --verbose --color always -p lightning
 [ "$CI_MINIMIZE_DISK_USAGE" != "" ] && cargo clean
-RUSTFLAGS="--cfg=async_payments" cargo test --verbose --color always -p lightning
-[ "$CI_MINIMIZE_DISK_USAGE" != "" ] && cargo clean
 RUSTFLAGS="--cfg=simple_close" cargo test --verbose --color always -p lightning
 [ "$CI_MINIMIZE_DISK_USAGE" != "" ] && cargo clean
 RUSTFLAGS="--cfg=lsps1_service" cargo test --verbose --color always -p lightning-liquidity
