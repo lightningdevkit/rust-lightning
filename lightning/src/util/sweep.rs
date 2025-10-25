6440f14d72fa5c43efe2a8427bca89dd87f0afb2
Matt Corallo <git@bluematt.me>
2025-10-25 13:41:47 +0000

    Trivially replace `Box::pin` with `pin!` in a few places
    Now that our MSRV is above 1.68 we can use the `pin!` macro to
    avoid having to `Box` various futures, avoiding some allocations,
    especially in `lightning-net-tokio`, which happens in a tight loop.

--- lightning/src/util/sweep.rs
+++ lightning/src/util/sweep.rs
@@ -35,6 +35,7 @@ use bitcoin::{BlockHash, ScriptBuf, Transaction, Txid};
 
 use core::future::Future;
 use core::ops::Deref;
+use core::pin::pin;
 use core::sync::atomic::{AtomicBool, Ordering};
 use core::task;
 
@@ -930,7 +931,7 @@ where
 		&self, output_descriptors: Vec<SpendableOutputDescriptor>, channel_id: Option<ChannelId>,
 		exclude_static_outputs: bool, delay_until_height: Option<u32>,
 	) -> Result<(), ()> {
-		let mut fut = Box::pin(self.sweeper.track_spendable_outputs(
+		let mut fut = pin!(self.sweeper.track_spendable_outputs(
 			output_descriptors,
 			channel_id,
 			exclude_static_outputs,
@@ -961,7 +962,7 @@ where
 	/// Regenerates and broadcasts the spending transaction for any outputs that are pending. Wraps
 	/// [`OutputSweeper::regenerate_and_broadcast_spend_if_necessary`].
 	pub fn regenerate_and_broadcast_spend_if_necessary(&self) -> Result<(), ()> {
-		let mut fut = Box::pin(self.sweeper.regenerate_and_broadcast_spend_if_necessary());
+		let mut fut = pin!(self.sweeper.regenerate_and_broadcast_spend_if_necessary());
 		let mut waker = dummy_waker();
 		let mut ctx = task::Context::from_waker(&mut waker);
 		match fut.as_mut().poll(&mut ctx) {
