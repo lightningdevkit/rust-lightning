031a987bfaef91b62f173932260641ee72edbf69
Matt Corallo <git@bluematt.me>
2025-10-29 18:04:37 +0000

    Make `LSPS5ServiceEvent::SendWebhookNotification::headers` a Vec
    While HTTP headers should be a unique K->V mapping, returning three
    headers to a user in an event via a `HashMap` is substantially
    overkill (and also not trivial to do in bindings). Instead, we
    expose them as a `Vec`.

--- lightning-liquidity/src/lsps5/service.rs
+++ lightning-liquidity/src/lsps5/service.rs
@@ -629,12 +629,12 @@ where
 
 		let signature_hex = self.sign_notification(&notification, &timestamp)?;
 
-		let mut headers: HashMap<String, String> = [("Content-Type", "application/json")]
+		let mut headers: Vec<(String, String)> = [("Content-Type", "application/json")]
 			.into_iter()
 			.map(|(k, v)| (k.to_string(), v.to_string()))
 			.collect();
-		headers.insert("x-lsps5-timestamp".into(), timestamp.to_rfc3339());
-		headers.insert("x-lsps5-signature".into(), signature_hex);
+		headers.push(("x-lsps5-timestamp".into(), timestamp.to_rfc3339()));
+		headers.push(("x-lsps5-signature".into(), signature_hex));
 
 		event_queue_notifier.enqueue(LSPS5ServiceEvent::SendWebhookNotification {
 			counterparty_node_id,
