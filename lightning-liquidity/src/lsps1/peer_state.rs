114a5887b6d9bc70262c2b809a037d2cf79b6647
Elias Rohrer <dev@tnull.de>
2025-11-16 13:07:09 +0100

    Drop bogus channel state handling
    .. we will re-add a proper state machine in a later commit, but for now
    we can just drop all of this half-baked logic that doesn't actually do
    anything.

--- lightning-liquidity/src/lsps1/peer_state.rs
+++ lightning-liquidity/src/lsps1/peer_state.rs
@@ -14,9 +14,6 @@ use super::msgs::{LSPS1OrderId, LSPS1OrderParams, LSPS1PaymentInfo, LSPS1Request
 use crate::lsps0::ser::{LSPSDateTime, LSPSRequestId};
 use crate::prelude::HashMap;
 
-use lightning::ln::msgs::{ErrorAction, LightningError};
-use lightning::util::logger::Level;
-
 #[derive(Default)]
 pub(super) struct PeerState {
 	pub(super) outbound_channels_by_order_id: HashMap<LSPS1OrderId, OutboundCRChannel>,
@@ -31,31 +28,6 @@ impl PeerState {
 	}
 }
 
-struct ChannelStateError(String);
-
-impl From<ChannelStateError> for LightningError {
-	fn from(value: ChannelStateError) -> Self {
-		LightningError { err: value.0, action: ErrorAction::IgnoreAndLog(Level::Info) }
-	}
-}
-
-#[derive(PartialEq, Debug)]
-pub(super) enum OutboundRequestState {
-	OrderCreated { order_id: LSPS1OrderId },
-	WaitingPayment { order_id: LSPS1OrderId },
-}
-
-impl OutboundRequestState {
-	fn awaiting_payment(&self) -> Result<Self, ChannelStateError> {
-		match self {
-			OutboundRequestState::OrderCreated { order_id } => {
-				Ok(OutboundRequestState::WaitingPayment { order_id: order_id.clone() })
-			},
-			state => Err(ChannelStateError(format!("TODO. JIT Channel was in state: {:?}", state))),
-		}
-	}
-}
-
 pub(super) struct OutboundLSPS1Config {
 	pub(super) order: LSPS1OrderParams,
 	pub(super) created_at: LSPSDateTime,
@@ -63,22 +35,13 @@ pub(super) struct OutboundLSPS1Config {
 }
 
 pub(super) struct OutboundCRChannel {
-	pub(super) state: OutboundRequestState,
 	pub(super) config: OutboundLSPS1Config,
 }
 
 impl OutboundCRChannel {
 	pub(super) fn new(
-		order: LSPS1OrderParams, created_at: LSPSDateTime, order_id: LSPS1OrderId,
-		payment: LSPS1PaymentInfo,
+		order: LSPS1OrderParams, created_at: LSPSDateTime, payment: LSPS1PaymentInfo,
 	) -> Self {
-		Self {
-			state: OutboundRequestState::OrderCreated { order_id },
-			config: OutboundLSPS1Config { order, created_at, payment },
-		}
-	}
-	pub(super) fn awaiting_payment(&mut self) -> Result<(), LightningError> {
-		self.state = self.state.awaiting_payment()?;
-		Ok(())
+		Self { config: OutboundLSPS1Config { order, created_at, payment } }
 	}
 }
