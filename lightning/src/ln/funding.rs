f92406c5def956962094bdc2cb5d5856d6239367
Jeffrey Czyz <jkczyz@gmail.com>
2026-01-13 16:45:52 -0600

    Move FundingTxInput::sequence to Utxo
    A forthcoming commit will change CoinSelection to include FundingTxInput
    instead of Utxo, though the former will probably be renamed. This is so
    CoinSelectionSource can be used when funding a splice. Further updating
    WalletSource to use FundingTxInput is not desirable, however, as it
    would result in looking up each confirmed UTXOs previous transaction
    even if it is not selected. See Wallet's implementation of
    CoinSelectionSource, which delegates to WalletSource for listing all
    confirmed UTXOs.
    
    This commit moves FundingTxInput::sequence to Utxo, and thus the
    responsibility for setting it to WalletSource implementations. Doing so
    will allow Wallet's CoinSelectionSource implementation to delegate
    looking up previous transactions to WalletSource without having to
    explicitly set the sequence on any FundingTxInput.

--- lightning/src/ln/funding.rs
+++ lightning/src/ln/funding.rs
@@ -108,11 +108,6 @@ pub struct FundingTxInput {
 	/// [`TxOut`]: bitcoin::TxOut
 	pub(super) utxo: Utxo,
 
-	/// The sequence number to use in the [`TxIn`].
-	///
-	/// [`TxIn`]: bitcoin::TxIn
-	pub(super) sequence: Sequence,
-
 	/// The transaction containing the unspent [`TxOut`] referenced by [`utxo`].
 	///
 	/// [`TxOut`]: bitcoin::TxOut
@@ -122,7 +117,7 @@ pub struct FundingTxInput {
 
 impl_writeable_tlv_based!(FundingTxInput, {
 	(1, utxo, required),
-	(3, sequence, required),
+	(3, _sequence, (legacy, Sequence, |input: &FundingTxInput| Some(input.utxo.sequence))),
 	(5, prevtx, required),
 });
 
@@ -140,8 +135,8 @@ impl FundingTxInput {
 					.ok_or(())?
 					.clone(),
 				satisfaction_weight: EMPTY_SCRIPT_SIG_WEIGHT + witness_weight.to_wu(),
+				sequence: Sequence::ENABLE_RBF_NO_LOCKTIME,
 			},
-			sequence: Sequence::ENABLE_RBF_NO_LOCKTIME,
 			prevtx,
 		})
 	}
@@ -234,14 +229,14 @@ impl FundingTxInput {
 	///
 	/// [`TxIn`]: bitcoin::TxIn
 	pub fn sequence(&self) -> Sequence {
-		self.sequence
+		self.utxo.sequence
 	}
 
 	/// Sets the sequence number to use in the [`TxIn`].
 	///
 	/// [`TxIn`]: bitcoin::TxIn
 	pub fn set_sequence(&mut self, sequence: Sequence) {
-		self.sequence = sequence;
+		self.utxo.sequence = sequence;
 	}
 
 	/// Converts the [`FundingTxInput`] into a [`Utxo`] for coin selection.
