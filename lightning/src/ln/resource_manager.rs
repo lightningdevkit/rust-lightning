cbfc629f73da47a5035deceda07bf8e899e0d233
elnosh <elnosh@pm.me>
2026-02-04 14:00:26 -0500

    Add GeneralBucket to resource manager
    Resources available in the channel will be divided into general,
    congestion and protected resources. Here we implement the general
    bucket with basic denial of service protections.
    
    Co-authored-by: Carla Kirk-Cohen <kirkcohenc@gmail.com>

--- lightning/src/ln/resource_manager.rs
+++ lightning/src/ln/resource_manager.rs
@@ -11,6 +11,202 @@
 
 use core::time::Duration;
 
+use crate::{
+	crypto::chacha20::ChaCha20,
+	prelude::{hash_map::Entry, new_hash_map, HashMap, Vec},
+	sign::EntropySource,
+};
+
+/// The minimum number of slots required for the general bucket to function.
+const MIN_GENERAL_BUCKET_SLOTS: u16 = 5;
+
+struct GeneralBucket {
+	/// Our SCID
+	scid: u64,
+
+	total_slots: u16,
+	total_liquidity: u64,
+
+	/// The number of slots in the general bucket that each forwarding channel pair gets.
+	per_channel_slots: u8,
+	/// The liquidity amount of each slot in the general bucket that each forwarding channel pair
+	/// gets.
+	per_slot_msat: u64,
+
+	/// Tracks the occupancy of HTLC slots in the bucket where the index represents the slot
+	/// number and the optional value indicates which channel is currently using the slot.
+	slots_occupied: Vec<Option<u64>>,
+
+	/// SCID -> (slots assigned, salt)
+	/// Maps short channel IDs to the slots that the channel is allowed to use and the salt. The
+	/// salt is stored to deterministically generate the slots for each channel on restarts.
+	channels_slots: HashMap<u64, (Vec<u16>, [u8; 32])>,
+}
+
+impl GeneralBucket {
+	fn new(scid: u64, slots_allocated: u16, liquidity_allocated: u64) -> Result<Self, ()> {
+		if slots_allocated < MIN_GENERAL_BUCKET_SLOTS {
+			return Err(());
+		}
+
+		let per_channel_slots = u8::max(
+			MIN_GENERAL_BUCKET_SLOTS as u8,
+			u8::try_from((slots_allocated * MIN_GENERAL_BUCKET_SLOTS).div_ceil(100)).unwrap(),
+		);
+
+		let per_slot_msat = liquidity_allocated * per_channel_slots as u64 / slots_allocated as u64;
+		// This is a sanity check but based on the minimum channel size accepted by LDK and the
+		// max_accepted_htlcs limit of 483 we do not expect to hit this.
+		if per_slot_msat == 0 {
+			return Err(());
+		}
+		Ok(GeneralBucket {
+			scid,
+			total_slots: slots_allocated,
+			total_liquidity: liquidity_allocated,
+			per_channel_slots,
+			per_slot_msat,
+			slots_occupied: vec![None; slots_allocated as usize],
+			channels_slots: new_hash_map(),
+		})
+	}
+
+	/// Returns the available slots that could be used by the outgoing scid for the specified
+	/// htlc amount.
+	fn slots_for_amount<ES: EntropySource>(
+		&mut self, outgoing_scid: u64, htlc_amount_msat: u64, entropy_source: &ES,
+	) -> Result<Option<Vec<u16>>, ()> {
+		let slots_needed = u64::max(1, htlc_amount_msat.div_ceil(self.per_slot_msat));
+
+		let channel_entry = match self.channels_slots.entry(outgoing_scid) {
+			Entry::Occupied(e) => e.into_mut(),
+			Entry::Vacant(entry) => {
+				let salt = entropy_source.get_secure_random_bytes();
+				let slots = assign_slots_for_channel(
+					self.scid,
+					outgoing_scid,
+					salt,
+					self.per_channel_slots,
+					self.total_slots,
+				)?;
+				entry.insert((slots, salt))
+			},
+		};
+
+		let slots_to_use: Vec<u16> = channel_entry
+			.0
+			.iter()
+			.filter(|idx| match self.slots_occupied.get(**idx as usize) {
+				Some(is_occupied) => is_occupied.is_none(),
+				None => {
+					debug_assert!(false, "assigned slot {} is not present in slots_occupied", idx);
+					false
+				},
+			})
+			.take(slots_needed as usize)
+			.copied()
+			.collect();
+
+		if (slots_to_use.len() as u64) < slots_needed {
+			Ok(None)
+		} else {
+			Ok(Some(slots_to_use))
+		}
+	}
+
+	fn can_add_htlc<ES: EntropySource>(
+		&mut self, outgoing_scid: u64, htlc_amount_msat: u64, entropy_source: &ES,
+	) -> Result<bool, ()> {
+		Ok(self.slots_for_amount(outgoing_scid, htlc_amount_msat, entropy_source)?.is_some())
+	}
+
+	fn add_htlc<ES: EntropySource>(
+		&mut self, outgoing_scid: u64, htlc_amount_msat: u64, entropy_source: &ES,
+	) -> Result<Vec<u16>, ()> {
+		match self.slots_for_amount(outgoing_scid, htlc_amount_msat, entropy_source)? {
+			Some(slots) => {
+				for slot_idx in &slots {
+					debug_assert!(self.slots_occupied[*slot_idx as usize].is_none());
+					self.slots_occupied[*slot_idx as usize] = Some(outgoing_scid);
+				}
+				Ok(slots)
+			},
+			None => Err(()),
+		}
+	}
+
+	fn remove_htlc(&mut self, outgoing_scid: u64, htlc_amount_msat: u64) -> Result<(), ()> {
+		let channel_slots = &self.channels_slots.get(&outgoing_scid).ok_or(())?.0;
+		let slots_needed = u64::max(1, htlc_amount_msat.div_ceil(self.per_slot_msat));
+		let slots_used_by_channel = channel_slots
+			.iter()
+			.filter(|slot_idx| self.slots_occupied[**slot_idx as usize] == Some(outgoing_scid))
+			.count();
+
+		if slots_needed > slots_used_by_channel as u64 {
+			return Err(());
+		}
+
+		let mut released = 0;
+		for slot_idx in channel_slots {
+			if released == slots_needed {
+				break;
+			}
+			if self.slots_occupied[*slot_idx as usize] == Some(outgoing_scid) {
+				self.slots_occupied[*slot_idx as usize] = None;
+				released += 1;
+			}
+		}
+
+		Ok(())
+	}
+
+	fn remove_channel_slots(&mut self, outgoing_scid: u64) {
+		if let Some((slots, _)) = self.channels_slots.remove(&outgoing_scid) {
+			for slot_idx in slots {
+				if self.slots_occupied[slot_idx as usize] == Some(outgoing_scid) {
+					self.slots_occupied[slot_idx as usize] = None;
+				}
+			}
+		}
+	}
+}
+
+fn assign_slots_for_channel(
+	incoming_scid: u64, outgoing_scid: u64, salt: [u8; 32], per_channel_slots: u8, total_slots: u16,
+) -> Result<Vec<u16>, ()> {
+	debug_assert_ne!(incoming_scid, outgoing_scid);
+
+	let mut channel_slots = Vec::with_capacity(per_channel_slots.into());
+	let mut slots_assigned_counter = 0;
+
+	let mut nonce = [0u8; 12];
+	nonce[..4].copy_from_slice(&incoming_scid.to_be_bytes()[..4]);
+	nonce[4..].copy_from_slice(&outgoing_scid.to_be_bytes());
+	let mut prng = ChaCha20::new(&salt, &nonce);
+	let mut buf = [0u8; 4];
+
+	let max_attempts = per_channel_slots * 10;
+	for _ in 0..max_attempts {
+		if slots_assigned_counter == per_channel_slots {
+			break;
+		}
+
+		prng.process_in_place(&mut buf);
+		let slot_idx: u16 = (u32::from_le_bytes(buf) % total_slots as u32) as u16;
+		if !channel_slots.contains(&slot_idx) {
+			channel_slots.push(slot_idx);
+			slots_assigned_counter += 1;
+		}
+	}
+
+	if slots_assigned_counter < per_channel_slots {
+		return Err(());
+	}
+
+	Ok(channel_slots)
+}
+
 /// A weighted average that decays over a specified window.
 ///
 /// It enables tracking of historical behavior without storing individual data points.
@@ -107,10 +303,172 @@ mod tests {
 
 	use crate::{
 		crypto::chacha20::ChaCha20,
-		ln::resource_manager::{AggregatedWindowAverage, DecayingAverage},
+		ln::resource_manager::{
+			assign_slots_for_channel, AggregatedWindowAverage, DecayingAverage, GeneralBucket,
+		},
+		sign::EntropySource,
+		util::test_utils::TestKeysInterface,
 	};
+	use bitcoin::Network;
 
 	const WINDOW: Duration = Duration::from_secs(2016 * 10 * 60);
+
+	#[test]
+	fn test_general_bucket_channel_slots_count() {
+		struct TestCase {
+			general_slots: u16,
+			general_liquidity: u64,
+			expected_slots: u8,
+			expected_liquidity: u64,
+		}
+
+		// Test that it correctly assigns the number of slots based on total slots in general
+		// bucket
+		let cases = vec![
+			TestCase {
+				general_slots: 20,
+				general_liquidity: 100_000_000,
+				expected_slots: 5,
+				expected_liquidity: 25_000_000,
+			},
+			TestCase {
+				general_slots: 50,
+				general_liquidity: 100_000_000,
+				expected_slots: 5,
+				expected_liquidity: 10_000_000,
+			},
+			TestCase {
+				general_slots: 100,
+				general_liquidity: 100_000_000,
+				expected_slots: 5,
+				expected_liquidity: 5_000_000,
+			},
+			TestCase {
+				general_slots: 114,
+				general_liquidity: 300_000_000,
+				expected_slots: 6,
+				expected_liquidity: 15789473,
+			},
+			TestCase {
+				general_slots: 193,
+				general_liquidity: 100_000_000,
+				expected_slots: 10,
+				expected_liquidity: 5_181_347,
+			},
+		];
+
+		let scid = 21;
+		let entropy_source = TestKeysInterface::new(&[0; 32], Network::Testnet);
+		for case in cases {
+			let general_bucket =
+				GeneralBucket::new(0, case.general_slots, case.general_liquidity).unwrap();
+
+			assert_eq!(general_bucket.per_channel_slots, case.expected_slots);
+			assert_eq!(general_bucket.per_slot_msat, case.expected_liquidity);
+			assert!(general_bucket.slots_occupied.iter().all(|slot| slot.is_none()));
+
+			let salt = entropy_source.get_secure_random_bytes();
+			let slots = assign_slots_for_channel(
+				general_bucket.scid,
+				scid,
+				salt,
+				general_bucket.per_channel_slots,
+				general_bucket.total_slots,
+			)
+			.unwrap();
+			assert_eq!(slots.len(), case.expected_slots as usize);
+		}
+	}
+
+	#[test]
+	fn test_general_bucket_errors() {
+		// slots_allocated is below the minimum.
+		assert!(GeneralBucket::new(0, 4, 10_000).is_err());
+		assert!(GeneralBucket::new(0, 0, 10_000).is_err());
+
+		// per_slot_msat rounds to zero.
+		assert!(GeneralBucket::new(0, 100, 0).is_err());
+		assert!(GeneralBucket::new(0, 100, 19).is_err());
+	}
+
+	#[test]
+	fn test_general_bucket_add_htlc_over_max_liquidity() {
+		let entropy_source = TestKeysInterface::new(&[0; 32], Network::Testnet);
+		let mut general_bucket = GeneralBucket::new(0, 100, 10_000).unwrap();
+		debug_assert_eq!(general_bucket.per_channel_slots, 5);
+		debug_assert_eq!(general_bucket.per_slot_msat, 500);
+
+		let scid = 21;
+		let htlc_amount_over_max = 3000;
+		// General bucket will assign 5 slots of 500 per channel. Max 5 * 500 = 2500
+		// Adding an HTLC over the amount should return error.
+		let add_htlc_res = general_bucket.add_htlc(scid, htlc_amount_over_max, &entropy_source);
+		assert!(add_htlc_res.is_err());
+
+		// All slots for the channel should be unoccupied since adding the HTLC failed.
+		let slots = &general_bucket.channels_slots.get(&scid).unwrap().0;
+		assert!(slots
+			.iter()
+			.all(|slot_idx| general_bucket.slots_occupied[*slot_idx as usize].is_none()));
+	}
+
+	#[test]
+	fn test_general_bucket_add_htlc() {
+		let entropy_source = TestKeysInterface::new(&[0; 32], Network::Testnet);
+		// General bucket will assign 5 slots of 500 per channel. Max 5 * 500 = 2500
+		let mut general_bucket = GeneralBucket::new(0, 100, 10_000).unwrap();
+		debug_assert_eq!(general_bucket.per_channel_slots, 5);
+		debug_assert_eq!(general_bucket.per_slot_msat, 500);
+
+		let scid = 21;
+		// HTLC of 500 should take one slot
+		let add_htlc_res = general_bucket.add_htlc(scid, 500, &entropy_source);
+		assert!(add_htlc_res.is_ok());
+		let slots_occupied = add_htlc_res.unwrap();
+		assert_eq!(slots_occupied.len(), 1);
+
+		let slot_occupied = slots_occupied[0];
+		assert_eq!(general_bucket.slots_occupied[slot_occupied as usize], Some(scid));
+
+		// HTLC of 1200 should take 3 general slots
+		let add_htlc_res = general_bucket.add_htlc(scid, 1200, &entropy_source);
+		assert!(add_htlc_res.is_ok());
+		let slots_occupied = add_htlc_res.unwrap();
+		assert_eq!(slots_occupied.len(), 3);
+
+		for slot_occupied in slots_occupied.iter() {
+			assert_eq!(general_bucket.slots_occupied[*slot_occupied as usize], Some(scid));
+		}
+
+		// 4 slots have been taken. Trying to add HTLC that will take 2 or more slots should fail
+		// now.
+		assert!(general_bucket.add_htlc(scid, 501, &entropy_source).is_err());
+		let channel_slots = &general_bucket.channels_slots.get(&scid).unwrap().0;
+		let unoccupied_slots_for_channel: Vec<&u16> = channel_slots
+			.iter()
+			.filter(|slot_idx| general_bucket.slots_occupied[**slot_idx as usize].is_none())
+			.collect();
+		assert_eq!(unoccupied_slots_for_channel.len(), 1);
+	}
+
+	#[test]
+	fn test_general_bucket_remove_htlc() {
+		let entropy_source = TestKeysInterface::new(&[0; 32], Network::Testnet);
+		let mut general_bucket = GeneralBucket::new(0, 100, 10_000).unwrap();
+
+		let scid = 21;
+		let htlc_amount = 400;
+		let slots_occupied = general_bucket.add_htlc(scid, htlc_amount, &entropy_source).unwrap();
+		assert_eq!(slots_occupied.len(), 1);
+		let slot_occupied = slots_occupied[0];
+		assert_eq!(general_bucket.slots_occupied[slot_occupied as usize], Some(scid));
+
+		// Trying to remove HTLC over number of slots previously used should result in a error
+		assert!(general_bucket.remove_htlc(scid, htlc_amount + 400).is_err());
+		assert!(general_bucket.remove_htlc(scid, htlc_amount).is_ok());
+
+		assert!(general_bucket.slots_occupied[slot_occupied as usize].is_none());
+	}
 
 	#[test]
 	fn test_decaying_average_bounds() {
