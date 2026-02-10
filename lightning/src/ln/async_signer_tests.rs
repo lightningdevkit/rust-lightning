db2a7eb716babf3645bf89f10e9bfd70a0b67eb5
elnosh <elnosh@pm.me>
2026-02-09 20:22:01 -0500

    Update changelog and remove manually_accept references

--- lightning/src/ln/async_signer_tests.rs
+++ lightning/src/ln/async_signer_tests.rs
@@ -372,11 +372,9 @@ fn test_funding_signed_0conf() {
 
 fn do_test_funding_signed_0conf(signer_ops: Vec<SignerOp>) {
 	// Simulate acquiring the signature for `funding_signed` asynchronously for a zero-conf channel.
-	let mut manually_accept_config = test_default_channel_config();
-
 	let chanmon_cfgs = create_chanmon_cfgs(2);
 	let node_cfgs = create_node_cfgs(2, &chanmon_cfgs);
-	let node_chanmgrs = create_node_chanmgrs(2, &node_cfgs, &[None, Some(manually_accept_config)]);
+	let node_chanmgrs = create_node_chanmgrs(2, &node_cfgs, &[None, None]);
 	let nodes = create_network(2, &node_cfgs, &node_chanmgrs);
 	let node_a_id = nodes[0].node.get_our_node_id();
 	let node_b_id = nodes[1].node.get_our_node_id();
