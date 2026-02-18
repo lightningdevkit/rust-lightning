9d0739983bf6287a2faa671bc773ac7ccadfab26
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2026-02-18 15:27:28 +0200

    ln: handle trampoline payments in finalize_claims

--- lightning/src/ln/channelmanager.rs
+++ lightning/src/ln/channelmanager.rs
@@ -10415,6 +10415,8 @@ This indicates a bug inside LDK. Please report this error at https://github.com/
 				});
 
 				Some((source, hold_times))
+			} else if let HTLCSource::TrampolineForward { .. } = source {
+				Some((source, Vec::new()))
 			} else {
 				None
 			}
