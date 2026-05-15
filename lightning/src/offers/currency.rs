14603e39b10decaece7e149fa22c4aea108be763
shaavan <shaavan.github@gmail.com>
2026-05-15 22:06:24 +0530

    [feat] Introduce the CurrencyConversion trait
    Add a `CurrencyConversion` trait for resolving currency-denominated amounts
    into millisatoshis.
    
    LDK cannot supply exchange rates itself, so applications provide this
    conversion logic as the foundation for fiat-denominated offer support.

---
+++ lightning/src/offers/currency.rs
@@ -0,0 +1,151 @@
+// This file is Copyright its original authors, visible in version control
+// history.
+//
+// This file is licensed under the Apache License, Version 2.0 <LICENSE-APACHE
+// or http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
+// You may not use this file except in accordance with one or both of these
+// licenses.
+
+//! Data structures and encoding for currency conversion support.
+
+use crate::offers::offer::CurrencyCode;
+#[allow(unused_imports)]
+use crate::prelude::*;
+
+use core::cmp::Ordering;
+use core::num::NonZeroU64;
+use core::ops::Deref;
+
+/// An exchange rate represented as `msats / minor_units`.
+///
+/// For example:
+///
+/// `ExchangeRate { msats: 123, minor_units: 1_000_000 }`
+///
+/// represents `123 millisatoshis per 1,000,000 minor currency units`.
+///
+/// Minor units are the smallest unit of an ISO 4217 currency.
+///
+/// For example:
+/// - USD (exponent 2) uses cents (0.01 USD)
+/// - JPY (exponent 0) uses yen
+#[derive(Clone, Copy, Debug)]
+pub struct ExchangeRate {
+	/// The millisatoshi numerator of the exchange-rate ratio.
+	pub msats: u64,
+
+	/// The fiat minor-unit denominator of the exchange-rate ratio.
+	pub minor_units: NonZeroU64,
+}
+
+impl ExchangeRate {
+	/// Creates a new exchange rate represented as `msats / minor_units`.
+	///
+	/// Returns an error if `minor_units` is zero.
+	pub fn new(msats: u64, minor_units: u64) -> Result<Self, ()> {
+		Ok(Self { msats, minor_units: NonZeroU64::new(minor_units).ok_or(())? })
+	}
+
+	/// Converts the given fiat minor-unit amount to millisatoshis using this
+	/// exchange rate.
+	///
+	/// The conversion is calculated as:
+	///
+	/// `fiat_minor_units * self.msats / self.minor_units`
+	///
+	/// Returns an error if the calculation overflows.
+	pub(crate) fn convert_to_msats(&self, fiat_minor_units: u64) -> Result<u64, ()> {
+		u128::from(fiat_minor_units)
+			.checked_mul(u128::from(self.msats))
+			.and_then(|amount| amount.checked_div(u128::from(self.minor_units.get())))
+			.and_then(|amount| amount.try_into().ok())
+			.ok_or(())
+	}
+}
+
+impl PartialEq for ExchangeRate {
+	fn eq(&self, other: &Self) -> bool {
+		self.cmp(other) == Ordering::Equal
+	}
+}
+
+impl Eq for ExchangeRate {}
+
+impl PartialOrd for ExchangeRate {
+	fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+		Some(self.cmp(other))
+	}
+}
+
+impl Ord for ExchangeRate {
+	fn cmp(&self, other: &Self) -> Ordering {
+		let lhs = u128::from(self.msats) * u128::from(other.minor_units.get());
+		let rhs = u128::from(other.msats) * u128::from(self.minor_units.get());
+
+		lhs.cmp(&rhs)
+	}
+}
+
+/// A range of accepted exchange rates.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct ExchangeRange {
+	/// The minimum accepted exchange rate.
+	minimum: ExchangeRate,
+
+	/// The maximum accepted exchange rate.
+	maximum: ExchangeRate,
+}
+
+impl ExchangeRange {
+	/// Constructs a new range of accepted exchange rates.
+	///
+	/// Returns `Err(())` if `minimum` exceeds `maximum`.
+	pub fn new(minimum: ExchangeRate, maximum: ExchangeRate) -> Result<Self, ()> {
+		if minimum > maximum {
+			return Err(());
+		}
+
+		Ok(Self { minimum, maximum })
+	}
+
+	/// Returns the minimum accepted exchange rate.
+	pub fn minimum(&self) -> ExchangeRate {
+		self.minimum
+	}
+
+	/// Returns the maximum accepted exchange rate.
+	pub fn maximum(&self) -> ExchangeRate {
+		self.maximum
+	}
+}
+
+/// A trait for retrieving fiat-to-bitcoin conversion ranges.
+///
+/// The returned range defines the minimum and maximum accepted exchange
+/// rates for converting fiat minor units into millisatoshis.
+///
+/// Exchange rates are represented as:
+///
+/// `msats / minor_units`
+pub trait CurrencyConversion {
+	/// Returns the accepted conversion range for the given currency.
+	fn conversion_range(&self, currency: CurrencyCode) -> Result<ExchangeRange, ()>;
+}
+
+impl<T: CurrencyConversion + ?Sized, CC: Deref<Target = T>> CurrencyConversion for CC {
+	fn conversion_range(&self, currency: CurrencyCode) -> Result<ExchangeRange, ()> {
+		self.deref().conversion_range(currency)
+	}
+}
+
+/// A [`CurrencyConversion`] implementation that does not support
+/// any fiat currency conversions.
+#[derive(Clone, Copy, Debug)]
+pub struct NullCurrencyConversion;
+
+impl CurrencyConversion for NullCurrencyConversion {
+	fn conversion_range(&self, _currency: CurrencyCode) -> Result<ExchangeRange, ()> {
+		Err(())
+	}
+}
